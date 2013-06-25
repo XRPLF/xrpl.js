2a340d09a418cb424b78c107d021d8dc2fe7c8f8
wltsmrz <chlavois@gmail.com>
2013-06-26 03:40:11 +0900

    Typo

--- src/js/ripple/remote.js
+++ src/js/ripple/remote.js
@@ -1282,8 +1282,8 @@ Remote.prototype.request_ripple_path_find = function (src_account, dst_account, 
   request.message.destination_account = UInt160.json_rewrite(dst_account);
   request.message.destination_amount  = Amount.json_rewrite(dst_amount);
 
-  if (source_currencies) {
-    request.message.source_currencies   = source_currencies.map(function (ci) {
+  if (src_currencies) {
+    request.message.source_currencies   = src_currencies.map(function (ci) {
       var ci_new  = {};
 
       if ('issuer' in ci)
