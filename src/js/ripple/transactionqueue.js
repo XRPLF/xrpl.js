0794f7b629b1ed3e518efad7c37c52c6cadcc34f
wltsmrz <wltsmrz@gmail.com>
2013-09-03 13:52:26 -0700

    Update transaction queue

--- src/js/ripple/transactionqueue.js
+++ src/js/ripple/transactionqueue.js
@@ -1,22 +1,16 @@
 
 function TransactionQueue() {
+  var self = this;
+
   this._queue  = [ ];
+
+  Object.defineProperty(this, '_length', {
+    get: function() { return self._queue.length }
+  });
 }
 
 TransactionQueue.prototype.length = function() {
   return this._queue.length;
-};
-
-TransactionQueue.prototype.push = function(o) {
-  return this._queue.push(o);
-};
-
-TransactionQueue.prototype.hasHash = function(hash) {
-  return this.indexOf('hash', hash) !== -1;
-};
-
-TransactionQueue.prototype.hasSequence = function(sequence) {
-  return this.indexOf('sequence', sequence) !== -1;
 };
 
 TransactionQueue.prototype.indexOf = function(prop, val) {
@@ -30,12 +24,20 @@ TransactionQueue.prototype.indexOf = function(prop, val) {
   return index;
 };
 
+TransactionQueue.prototype.hasHash = function(hash) {
+  return this.indexOf('hash', hash) !== -1;
+};
+
+TransactionQueue.prototype.get = function(prop, val) {
+  var index = this.indexOf(prop, val);
+  return index > -1 ? this._queue[index] : false;
+};
+
 TransactionQueue.prototype.removeSequence = function(sequence) {
   var result = [ ];
   for (var i=0, tx; tx=this._queue[i]; i++) {
     if (!tx.tx_json) continue;
-    if (tx.tx_json.Sequence !== sequence) 
-      result.push(tx);
+    if (tx.tx_json.Sequence !== sequence) result.push(tx);
   }
   this._queue = result;
 };
@@ -44,16 +46,17 @@ TransactionQueue.prototype.removeHash = function(hash) {
   var result = [ ];
   for (var i=0, tx; tx=this._queue[i]; i++) {
     if (!tx.tx_json) continue;
-    if (tx.hash !== hash)
-      result.push(tx);
+    if (tx.hash !== hash) result.push(tx);
   }
   this._queue = result;
 };
 
-TransactionQueue.prototype.forEach = function(fn) {
-  for (var i=0, tx; tx=this._queue[i]; i++) {
-    fn(tx, i);
-  }
+TransactionQueue.prototype.forEach = function() {
+  Array.prototype.forEach.apply(this._queue, arguments);
+};
+
+TransactionQueue.prototype.push = function() {
+  Array.prototype.push.apply(this._queue, arguments);
 };
 
 exports.TransactionQueue = TransactionQueue;
