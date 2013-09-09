2e7fcb0e76f285739c2e582fb2887b13cdc2b2be
wltsmrz <wltsmrz@gmail.com>
2013-09-09 14:48:20 -0700

    Cleanup

--- src/js/ripple/base.js
+++ src/js/ripple/base.js
@@ -1,4 +1,3 @@
-
 var sjcl    = require('../../../build/sjcl');
 var utils   = require('./utils');
 var jsbn    = require('./jsbn');
@@ -9,81 +8,78 @@ var nbi        = jsbn.nbi;
 
 var Base = {};
 
-var alphabets	= Base.alphabets = {
-  'ripple'  : "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz",
-  'tipple'  : "RPShNAF39wBUDnEGHJKLM4pQrsT7VWXYZ2bcdeCg65jkm8ofqi1tuvaxyz",
-  'bitcoin' : "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
+var alphabets = Base.alphabets = {
+  ripple  :  "rpshnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCg65jkm8oFqi1tuvAxyz",
+  tipple  :  "RPShNAF39wBUDnEGHJKLM4pQrsT7VWXYZ2bcdeCg65jkm8ofqi1tuvaxyz",
+  bitcoin :  "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
 };
 
 extend(Base, {
-  'VER_NONE'              : 1,
-  'VER_NODE_PUBLIC'       : 28,
-  'VER_NODE_PRIVATE'      : 32,
-  'VER_ACCOUNT_ID'        : 0,
-  'VER_ACCOUNT_PUBLIC'    : 35,
-  'VER_ACCOUNT_PRIVATE'   : 34,
-  'VER_FAMILY_GENERATOR'  : 41,
-  'VER_FAMILY_SEED'       : 33
+  VER_NONE              : 1,
+  VER_NODE_PUBLIC       : 28,
+  VER_NODE_PRIVATE      : 32,
+  VER_ACCOUNT_ID        : 0,
+  VER_ACCOUNT_PUBLIC    : 35,
+  VER_ACCOUNT_PRIVATE   : 34,
+  VER_FAMILY_GENERATOR  : 41,
+  VER_FAMILY_SEED       : 33
 });
 
-var sha256  = function (bytes) {
+function sha256(bytes) {
   return sjcl.codec.bytes.fromBits(sjcl.hash.sha256.hash(sjcl.codec.bytes.toBits(bytes)));
 };
 
-var sha256hash = function (bytes) {
+function sha256hash(bytes) {
   return sha256(sha256(bytes));
 };
 
 // --> input: big-endian array of bytes.
 // <-- string at least as long as input.
 Base.encode = function (input, alpha) {
-  var alphabet	= alphabets[alpha || 'ripple'];
-  var bi_base	= new BigInteger(String(alphabet.length));
-  var bi_q	= nbi();
-  var bi_r	= nbi();
-  var bi_value	= new BigInteger(input);
-  var buffer	= [];
+  var alphabet = alphabets[alpha || 'ripple'];
+  var bi_base  = new BigInteger(String(alphabet.length));
+  var bi_q     = nbi();
+  var bi_r     = nbi();
+  var bi_value = new BigInteger(input);
+  var buffer   = [];
 
-  while (bi_value.compareTo(BigInteger.ZERO) > 0)
-  {
+  while (bi_value.compareTo(BigInteger.ZERO) > 0) {
     bi_value.divRemTo(bi_base, bi_q, bi_r);
     bi_q.copyTo(bi_value);
-
     buffer.push(alphabet[bi_r.intValue()]);
   }
 
-  var i;
-
-  for (i = 0; i != input.length && !input[i]; i += 1) {
+  for (var i=0; i !== input.length && !input[i]; i += 1) {
     buffer.push(alphabet[0]);
   }
 
-  return buffer.reverse().join("");
+  return buffer.reverse().join('');
 };
 
 // --> input: String
 // <-- array of bytes or undefined.
 Base.decode = function (input, alpha) {
-  if ("string" !== typeof input) return undefined;
+  if (typeof input !== 'string') {
+    return void(0);
+  }
 
-  var alphabet	= alphabets[alpha || 'ripple'];
-  var bi_base	= new BigInteger(String(alphabet.length));
-  var bi_value	= nbi();
+  var alphabet = alphabets[alpha || 'ripple'];
+  var bi_base  = new BigInteger(String(alphabet.length));
+  var bi_value = nbi();
   var i;
 
   for (i = 0; i != input.length && input[i] === alphabet[0]; i += 1)
-    ;
+  ;
 
-  for (; i != input.length; i += 1) {
-    var	v = alphabet.indexOf(input[i]);
+  for (; i !== input.length; i += 1) {
+    var v = alphabet.indexOf(input[i]);
 
-    if (v < 0)
-      return undefined;
+    if (v < 0) {
+      return void(0);
+    }
 
     var r = nbi();
-
     r.fromInt(v);
-
     bi_value  = bi_value.multiply(bi_base).add(r);
   }
 
@@ -93,69 +89,83 @@ Base.decode = function (input, alpha) {
   var bytes =  bi_value.toByteArray().map(function (b) { return b ? b < 0 ? 256+b : b : 0; });
   var extra = 0;
 
-  while (extra != bytes.length && !bytes[extra])
+  while (extra != bytes.length && !bytes[extra]) {
     extra += 1;
+  }
 
-  if (extra)
+  if (extra) {
     bytes = bytes.slice(extra);
+  }
 
   var zeros = 0;
 
-  while (zeros !== input.length && input[zeros] === alphabet[0])
+  while (zeros !== input.length && input[zeros] === alphabet[0]) {
     zeros += 1;
+  }
 
   return [].concat(utils.arraySet(zeros, 0), bytes);
 };
 
 Base.verify_checksum = function (bytes) {
-  var computed	= sha256hash(bytes.slice(0, -4)).slice(0, 4);
-  var checksum	= bytes.slice(-4);
+  var computed = sha256hash(bytes.slice(0, -4)).slice(0, 4);
+  var checksum = bytes.slice(-4);
+  var result = true;
 
-  for (var i = 0; i < 4; i++)
-    if (computed[i] !== checksum[i])
-      return false;
+  for (var i=0; i<4; i++) {
+    if (computed[i] !== checksum[i]) {
+      result = false;
+      break;
+    }
+  }
 
-  return true;
+  return result;
 };
 
 // --> input: Array
 // <-- String
 Base.encode_check = function (version, input, alphabet) {
-  var buffer  = [].concat(version, input);
-  var check   = sha256(sha256(buffer)).slice(0, 4);
+  var buffer = [].concat(version, input);
+  var check  = sha256(sha256(buffer)).slice(0, 4);
 
   return Base.encode([].concat(buffer, check), alphabet);
-}
+};
 
 // --> input : String
 // <-- NaN || BigInteger
 Base.decode_check = function (version, input, alphabet) {
   var buffer = Base.decode(input, alphabet);
 
-  if (!buffer || buffer.length < 5)
+  if (!buffer || buffer.length < 5) {
     return NaN;
-
-  // Single valid version
-  if ("number" === typeof version && buffer[0] !== version)
-    return NaN;
-
-  // Multiple allowed versions
-  if ("object" === typeof version && Array.isArray(version)) {
-    var match = false;
-    for (var i = 0, l = version.length; i < l; i++) {
-      match |= version[i] === buffer[0];
-    }
-    if (!match) return NaN;
   }
 
-  if (!Base.verify_checksum(buffer))
+  // Single valid version
+  if (typeof version && buffer[0] !== version === 'number') {
     return NaN;
+  }
+
+  // Multiple allowed versions
+  if (typeof version && Array.isArray(version) === 'object') {
+    var match = false;
+
+    for (var i=0, l=version.length; i<l; i++) {
+      match |= version[i] === buffer[0];
+    }
+
+    if (!match) {
+      return NaN;
+    }
+  }
+
+  if (!Base.verify_checksum(buffer)) {
+    return NaN;
+  }
 
   // We'll use the version byte to add a leading zero, this ensures JSBN doesn't
   // intrepret the value as a negative number
   buffer[0] = 0;
 
   return new BigInteger(buffer.slice(0, -4), 256);
-}
+}:
 
 exports.Base = Base;
