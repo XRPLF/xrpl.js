63307770766dec27fb8ecbee6b5f25ccae832f38
wltsmrz <wltsmrz@gmail.com>
2013-09-09 14:59:19 -0700

    Cleanup

--- src/js/ripple/currency.js
+++ src/js/ripple/currency.js
@@ -4,7 +4,7 @@
 //
 
 // XXX Internal form should be UInt160.
-var Currency = function () {
+function Currency() {
   // Internal form: 0 = XRP. 3 letter-code.
   // XXX Internal should be 0 or hex with three letter annotation when valid.
 
@@ -14,7 +14,7 @@ var Currency = function () {
   // XXX Should support hex, C++ doesn't currently allow it.
 
   this._value  = NaN;
-}
+};
 
 // Given "USD" return the json.
 Currency.json_rewrite = function (j) {
@@ -22,19 +22,11 @@ Currency.json_rewrite = function (j) {
 };
 
 Currency.from_json = function (j) {
-  if (j instanceof Currency) {
-    return j.clone();
-  } else {
-    return new Currency().parse_json(j);
-  }
+  return j instanceof Currency ? j.clone() : new Currency().parse_json(j);
 };
 
 Currency.from_bytes = function (j) {
-  if (j instanceof Currency) {
-    return j.clone();
-  } else {
-    return new Currency().parse_bytes(j);
-  }
+  return j instanceof Currency ? j.clone() : new Currency().parse_bytes(j);
 };
 
 Currency.is_valid = function (j) {
@@ -48,20 +40,20 @@ Currency.prototype.clone = function() {
 // Returns copy.
 Currency.prototype.copyTo = function (d) {
   d._value = this._value;
-
   return d;
 };
 
 Currency.prototype.equals = function (d) {
-  return ('string' !== typeof this._value && isNaN(this._value))
-    || ('string' !== typeof d._value && isNaN(d._value)) ? false : this._value === d._value;
+  var equals = (typeof this._value !== 'string' && isNaN(this._value))
+    || (typeof d._value !== 'string' && isNaN(d._value));
+  return equals ? false: this._value === d._value;
 };
 
 // this._value = NaN on error.
 Currency.prototype.parse_json = function (j) {
   if (j instanceof Currency) {
     this._value = j;
-  } else if ('string' === typeof j) {
+  } else if (typeof j === 'string') {
     if (j === "" || j === "0" || j === "XRP") {
       // XRP is never allowed as a Currency object
       this._value = 0;
@@ -70,10 +62,10 @@ Currency.prototype.parse_json = function (j) {
     } else {
       this._value = NaN;
     }
-  } else if ('number' === typeof j) {
+  } else if (typeof j === 'number' && !isNaN(j)) {
     // XXX This is a hack
     this._value = j;
-  } else if ('string' != typeof j || 3 !== j.length) {
+  } else if (typeof j !== 'string'|| j.length !== 3) {
     this._value = NaN;
   } else {
     this._value = j;
@@ -83,15 +75,19 @@ Currency.prototype.parse_json = function (j) {
 };
 
 Currency.prototype.parse_bytes = function (byte_array) {
-  if (Array.isArray(byte_array) && byte_array.length == 20) {
+  if (Array.isArray(byte_array) && byte_array.length === 20) {
     var result;
     // is it 0 everywhere except 12, 13, 14?
     var isZeroExceptInStandardPositions = true;
+
     for (var i=0; i<20; i++) {
       isZeroExceptInStandardPositions = isZeroExceptInStandardPositions && (i===12 || i===13 || i===14 || byte_array[0]===0)
     }
+
     if (isZeroExceptInStandardPositions) {
-      var currencyCode = String.fromCharCode(byte_array[12]) + String.fromCharCode(byte_array[13]) + String.fromCharCode(byte_array[14]);
+      var currencyCode = String.fromCharCode(byte_array[12])
+      + String.fromCharCode(byte_array[13])
+      + String.fromCharCode(byte_array[14]);
       if (/^[A-Z0-9]{3}$/.test(currencyCode) && currencyCode !== "XRP" ) {
         this._value = currencyCode;
       } else if (currencyCode === "\0\0\0") {
@@ -114,7 +110,7 @@ Currency.prototype.is_native = function () {
 };
 
 Currency.prototype.is_valid = function () {
-  return 'string' === typeof this._value || !isNaN(this._value);
+  return typeof this._value === 'string' || !isNaN(this._value);
 };
 
 Currency.prototype.to_json = function () {
