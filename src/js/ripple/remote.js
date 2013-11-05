43319267adc8d415b6db06a345cd5768164267c3
Vahe Hovhannisyan <vhpoet@gmail.com>
2013-11-04 16:52:27 -0800

    Add all available flags to Remote.flags.

--- src/js/ripple/remote.js
+++ src/js/ripple/remote.js
@@ -203,11 +203,29 @@ util.inherits(Remote, EventEmitter);
 
 // Flags for ledger entries. In support of account_root().
 Remote.flags = {
+  // Account Root
   account_root : {
-    PasswordSpent:   0x00010000,
-    RequireDestTag:  0x00020000,
-    RequireAuth:     0x00040000,
-    DisallowXRP:     0x00080000
+    PasswordSpent:      0x00010000, // True, if password set fee is spent.
+    RequireDestTag:     0x00020000, // True, to require a DestinationTag for payments.
+    RequireAuth:        0x00040000, // True, to require a authorization to hold IOUs.
+    DisallowXRP:        0x00080000, // True, to disallow sending XRP.
+    DisableMaster:      0x00100000  // True, force regular key.
+  },
+
+  // Offer
+  offer: {
+    Passive:            0x00010000,
+    Sell:               0x00020000  // True, offer was placed as a sell.
+  },
+
+  // Ripple State
+  state: {
+    LowReserve:         0x00010000, // True, if entry counts toward reserve.
+    HighReserve:        0x00020000,
+    LowAuth:            0x00040000,
+    HighAuth:           0x00080000,
+    LowNoRipple:        0x00100000,
+    HighNoRipple:       0x00200000
   }
 };
 
