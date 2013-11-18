1fa196037e62c10a74f0d5fb1683270b201bd8a2
wltsmrz <wltsmrz@gmail.com>
2013-11-18 13:08:05 -0800

    Fix direct requirements for tests

--- test/testutils.js
+++ test/testutils.js
@@ -1,3 +1,4 @@
+var ripple = require('../src/js/ripple');
 
 exports.get_config = get_config;
 
@@ -9,16 +10,22 @@ function get_config() {
     config = require('./config-example');
   }
   return load_config(config);
-}
+};
 
 exports.load_config = load_config;
 
 function load_config(config) {
   return load_module('config').load(config);
-}
+};
 
 exports.load_module = load_module;
 
 function load_module(name) {
-  return require((process.env.RIPPLE_LIB_COV ? '../src-cov/js/ripple/' : '../src/js/ripple/') + name);
-}
+  if (process.env.RIPPLE_LIB_COV) {
+    return require('../src-cov/js/ripple/' + name)
+  } else if (!ripple.hasOwnProperty(name)) {
+    return require('../src/js/ripple/' + name);
+  } else {
+    return require('../src/js/ripple')[name];
+  }
+};
