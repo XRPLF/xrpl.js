2fdec4767db44d1b1be845e6d66608b0d919e82e
MatthewPhinney <matt.phinney@ripple.com>
2014-12-17 17:14:09 -0800

    [FIX] add logic to ripple lib to allow queries by ripple address (RT-2961)

--- src/js/ripple/authinfo.js
+++ src/js/ripple/authinfo.js
@@ -12,6 +12,47 @@ AuthInfo._getUser = function(url, callback) {
   superagent.get(url, callback);
 };
 
+/**
+ * Get auth info for a given address
+ *
+ * @param {string}    domain - Domain which hosts the user's info
+ * @param {string}    address - Address of user who's info we are retreiving
+ * @param {function}  fn - Callback function
+ */
+
+AuthInfo.getAddress = function(domain, address, callback) {
+  var self = this;
+  
+  function getRippleTxt(callback) {
+    self._getRippleTxt(domain, function(err, txt) {
+      if (err) {
+        return callback(err);
+      }
+
+      if (!txt.authinfo_url) {
+        return callback(new Error('Authentication is not supported on ' + domain));
+      }
+
+      var url = Array.isArray(txt.authinfo_url) ? txt.authinfo_url[0] : txt.authinfo_url;
+
+      url += '?domain=' + domain + '&username=' + address;
+
+      callback(null, url);
+    });
+  };
+
+  function getUserAddress(url, callback) {
+    self._getUser(url, function(err, res) {
+      if (err || res.error) {
+        callback(new Error('Authentication info server unreachable'));
+      } else {
+        callback(null, res.body);
+      }
+    });
+  };
+
+  async.waterfall([ getRippleTxt, getUserAddress ], callback);
+};
 
 /**
  * Get auth info for a given username
