85eee0e651c3713b938565844875318d7c999cd9
Nicholas Dudfield <ndudfield@gmail.com>
2015-06-17 19:58:42 +0700

    Initial import

---
+++ packages/ripple-address-codec/index.js
@@ -0,0 +1,30 @@
+var hashjs = require('hash.js');
+var apiFactory = require('coin-address-codec');
+
+var VER_NODE_PUBLIC = 28;
+var VER_NODE_PRIVATE = 32;
+var VER_ACCOUNT_ID = 0;
+var VER_FAMILY_SEED = 33;
+
+function sha256(bytes) {
+  return hashjs.sha256().update(bytes).digest();
+}
+
+var api = apiFactory({sha256: sha256, defaultAlphabet: 'ripple'});
+
+function addVersion(name, version) {
+  function add(symbol, func) {
+    api[symbol+name] = function(string) {
+      return api[symbol](string, {version: version});
+    }
+  }
+  add('decode')
+  add('encode')
+}
+
+addVersion('Seed', VER_FAMILY_SEED);
+addVersion('AccountID', VER_ACCOUNT_ID);
+addVersion('NodePublic', VER_NODE_PUBLIC);
+addVersion('NodePrivate', VER_NODE_PRIVATE);
+
+module.exports = api;
