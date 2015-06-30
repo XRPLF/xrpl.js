99e076b7ddad72e098e149b3c06b83b7ab6b5880
Chris Clark <clark@ripple.com>
2015-06-30 15:21:28 -0700

    Sort orderbook orders by price

--- src/api/ledger/orderbook.js
+++ src/api/ledger/orderbook.js
@@ -16,7 +16,7 @@ function getBookOffers(remote, account, ledgerVersion, limit,
     ledger: ledgerVersion || 'validated',
     limit: limit,
     taker: account
-  }), composeAsync((data) => data.offers.map(parseOrderbookOrder), callback));
+  }), composeAsync(data => data.offers, callback));
 }
 
 function isSameIssue(a, b) {
@@ -28,10 +28,18 @@ function orderFilter(issue, direction, order) {
     && order.specification.direction === direction;
 }
 
-function formatBidsAndAsks(orderbook, orders) {
+function formatBidsAndAsks(orderbook, offers) {
   // the "base" currency is the currency that you are buying or selling
   // the "counter" is the currency that the "base" is priced in
   // a "bid"/"ask" is an order to buy/sell the base, respectively
+  // for bids: takerGets = totalPrice = counter, takerPays = quantity = base
+  // for asks: takerGets = quantity = base, takerPays = totalPrice = counter
+  // quality = takerPays / takerGets; price = totalPrice / quantity
+  // for bids: lowest quality => lowest quantity/totalPrice => highest price
+  // for asks: lowest quality => lowest totalPrice/quantity => lowest price
+  // for both bids and asks, lowest quality is closest to mid-market
+  // we sort the orders so that earlier orders are closer to mid-market
+  const orders = _.sortBy(offers, 'quality').map(parseOrderbookOrder);
   const bids = orders.filter(_.partial(orderFilter, orderbook.base, 'buy'));
   const asks = orders.filter(_.partial(orderFilter, orderbook.base, 'sell'));
   return {bids, asks};
@@ -44,9 +52,9 @@ function getOrderBook(account, orderbook, options, callback) {
 
   const getter = _.partial(getBookOffers, this.remote, account,
     options.ledgerVersion, options.limit);
-  const getOrders = _.partial(getter, orderbook.base, orderbook.counter);
-  const getReverseOrders = _.partial(getter, orderbook.counter, orderbook.base);
-  async.parallel([getOrders, getReverseOrders],
+  const getOffers = _.partial(getter, orderbook.base, orderbook.counter);
+  const getReverseOffers = _.partial(getter, orderbook.counter, orderbook.base);
+  async.parallel([getOffers, getReverseOffers],
     composeAsync((data) => formatBidsAndAsks(orderbook, _.flatten(data)),
                  callback));
 }
