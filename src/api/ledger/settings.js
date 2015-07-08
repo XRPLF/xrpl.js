fa89c4dee82c24c6e78401fce5850d58181ba2fc
Chris Clark <clark@ripple.com>
2015-07-08 14:32:21 -0700

    Add ledgerVersion option to getSettings

--- src/api/ledger/settings.js
+++ src/api/ledger/settings.js
@@ -4,6 +4,7 @@ const utils = require('./utils');
 const flags = utils.common.core.Remote.flags.account_root;
 const validate = utils.common.validate;
 const parseFields = require('./parse/fields');
+const composeAsync = utils.common.composeAsync;
 
 const AccountFlags = {
   passwordSpent: flags.PasswordSpent,
@@ -26,19 +27,24 @@ function parseFlags(value) {
   return settings;
 }
 
-function getSettings(account, callback) {
-  validate.address(account);
+function formatSettings(response) {
+  const data = response.account_data;
+  const parsedFlags = parseFlags(data.Flags);
+  const parsedFields = parseFields(data);
+  return _.assign({}, parsedFlags, parsedFields);
+}
 
-  this.remote.requestAccountInfo({account: account}, function(error, info) {
-    if (error) {
-      return callback(error);
-    }
-    const data = info.account_data;
-    const parsedFlags = parseFlags(data.Flags);
-    const parsedFields = parseFields(data);
-    const settings = _.assign({}, parsedFlags, parsedFields);
-    callback(null, settings);
-  });
+function getSettings(account, options, callback) {
+  validate.address(account);
+  validate.options(options);
+
+  const request = {
+    account: account,
+    ledger: options.ledgerVersion
+  };
+
+  this.remote.requestAccountInfo(request,
+    composeAsync(formatSettings, callback));
 }
 
 module.exports = utils.wrapCatch(getSettings);
