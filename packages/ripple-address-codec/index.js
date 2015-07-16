f9c51b05a66a23caa508952e1af16bbd2b47d37c
Nicholas Dudfield <ndudfield@gmail.com>
2015-07-16 19:33:59 +0800

    Add Ed25519 seeds and tests

--- packages/ripple-address-codec/index.js
+++ packages/ripple-address-codec/index.js
@@ -1,10 +1,22 @@
 var hashjs = require('hash.js');
 var apiFactory = require('x-address-codec');
 
-var VER_NODE_PUBLIC = 28;
-var VER_NODE_PRIVATE = 32;
-var VER_ACCOUNT_ID = 0;
-var VER_FAMILY_SEED = 33;
+var NODE_PUBLIC = 28;
+var NODE_PRIVATE = 32;
+var ACCOUNT_ID = 0;
+var FAMILY_SEED = 33;
+var ED25519_SEED = [0x01, 0xE1, 0x4B];
+
+var VERSIONS = {
+  EdSeed : {
+    expectedLength: 16,
+    version: ED25519_SEED
+  },
+  AccountID: {version: ACCOUNT_ID },
+  NodePublic: {version: NODE_PUBLIC },
+  NodePrivate: {version: NODE_PRIVATE },
+  K256Seed : {version: FAMILY_SEED }
+};
 
 function sha256(bytes) {
   return hashjs.sha256().update(bytes).digest();
@@ -12,19 +24,25 @@ function sha256(bytes) {
 
 var api = apiFactory({sha256: sha256, defaultAlphabet: 'ripple'});
 
-function addVersion(name, version) {
-  function add(operation, func) {
+function addVersion(name, args) {
+  function add(operation) {
     api[operation + name] = function(string) {
-      return api[operation](string, {version: version});
+      return api[operation](string, args);
     }
   }
-  add('decode');
   add('encode');
+  add('decode');
 }
 
-addVersion('Seed', VER_FAMILY_SEED);
-addVersion('AccountID', VER_ACCOUNT_ID);
-addVersion('NodePublic', VER_NODE_PUBLIC);
-addVersion('NodePrivate', VER_NODE_PRIVATE);
+api.decodeSeed = function(seed) {
+  var args = {versions: [ED25519_SEED, FAMILY_SEED], expectedLength: 16};
+  var decoded = api.decode(seed, args);
+  decoded.type = decoded.versionIx === 0 ? 'EdSeed' : 'K256Seed';
+  return decoded;
+};
+
+for (var k in VERSIONS) {
+  addVersion(k, VERSIONS[k]);
+}
 
 module.exports = api;
