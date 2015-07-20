b477eb238bd94ad729fc8e7b1aa3cac458c31cd3
Alan Cohen <alan@ripple.com>
2015-07-20 13:09:58 -0700

    Add flow-bin to dev dependencies and ignore it in flowconfig
    Add flow to api/server/server.js
    
    Add flow check to src/api: index, accountinfo, balances, orderbook..
    
    orders, settings, transaction, transactions
    
    Add flow typecheck to api/ledger/pathfind
    
    Use eslint plugin flowtype to allow flow type annotations
    
    Babel-eslint emits errors when using the type keyword. This plugin works
    around the issue by stripping flow annotations before linting.
    
    Source: https://www.npmjs.com/package/eslint-plugin-flowtype
    
    Add flow to ledger/parse/account-order, trustline, orderbook-order

--- src/api/ledger/parse/amount.js
+++ src/api/ledger/parse/amount.js
@@ -2,10 +2,11 @@
 'use strict';
 const utils = require('./utils');
 
-/*:: type Amount = string | {currency: string, issuer: string, value: string} */
-/*:: type XRPAmount = {currency: string, value: string} */
-/*:: type IOUAmount = {currency: string, value: string, counterparty: string} */
-/*:: type Output = XRPAmount | IOUAmount */
+type Amount = string | {currency: string, issuer: string, value: string}
+type XRPAmount = {currency: string, value: string}
+type IOUAmount = {currency: string, value: string, counterparty: string}
+type Output = XRPAmount | IOUAmount
+
 function parseAmount(amount: Amount): Output {
   if (typeof amount === 'string') {
     return {
