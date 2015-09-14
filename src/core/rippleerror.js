bfe590d96d2ca80f1ac3c149e91a084d821e259f
Ivan Tivonenko <darkdarkdragon@gmail.com>
2015-09-14 22:38:15 +0300

    copy message from Error instance into RippleError

--- src/core/rippleerror.js
+++ src/core/rippleerror.js
@@ -1,23 +1,28 @@
-var util   = require('util');
-var extend = require('extend');
+const util = require('util');
+const _ = require('lodash');
 
-function RippleError(code, message) {
-  switch (typeof code) {
-    case 'object':
-      extend(this, code);
-      break;
+function RippleError(code?: any, message?: string) {
+  if (code instanceof Error) {
+    this.result = code;
+    this.result_message = code.message;
+  } else {
+    switch (typeof code) {
+      case 'object':
+        _.extend(this, code);
+        break;
 
-    case 'string':
-      this.result = code;
-      this.result_message = message;
-      break;
+      case 'string':
+        this.result = code;
+        this.result_message = message;
+        break;
+    }
   }
 
   this.engine_result = this.result = (this.result || this.engine_result || this.error || 'Error');
   this.engine_result_message = this.result_message = (this.result_message || this.engine_result_message || this.error_message || 'Error');
-  this.result_message = this.message = (this.result_message);
+  this.message = this.result_message;
 
-  var stack;
+  let stack;
 
   if (!!Error.captureStackTrace) {
     Error.captureStackTrace(this, code || this);
