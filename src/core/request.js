31e9793f9a2aed77d54a97199a057a1de4394007
Ivan Tivonenko <darkdarkdragon@gmail.com>
2015-09-27 22:22:28 +0300

    Handle case when request was re-submitted due to reconnection

--- src/core/request.js
+++ src/core/request.js
@@ -30,6 +30,7 @@ function Request(remote, command) {
   this.reconnectTimeout = 1000 * 3;
   this.successEvent = 'success';
   this.errorEvent = 'error';
+  this.reconnectionsCount = 0;
   this.message = {
     command: command,
     id: undefined
@@ -66,6 +67,7 @@ Request.prototype.request = function(servers, callback_) {
   }
 
   function onReconnect() {
+    this.reconnectionsCount += 1;
     doRequest();
   }
 
