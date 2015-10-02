ab9ced8344ee6b73f3e2218437f9365214981e81
Nicholas Dudfield <ndudfield@gmail.com>
2015-10-02 10:49:37 +0700

    Add encodeForSigning

--- packages/ripple-binary-codec/src/index.js
+++ packages/ripple-binary-codec/src/index.js
@@ -2,14 +2,28 @@
 
 const assert = require('assert');
 const coreTypes = require('@niq/ripple-core-types');
-const {binary: {bytesToHex, binaryToJSON, serializeObject}} = coreTypes;
+const {binary: {bytesToHex,
+                signingData,
+                multiSigningData,
+                binaryToJSON,
+                serializeObject}} = coreTypes;
 
 exports.decode = function(binary) {
-  assert(typeof binary === 'string');
+  assert(typeof binary === 'string', 'binary must be a hex string');
   return binaryToJSON(binary);
 };
 
 exports.encode = function(json) {
   assert(typeof json === 'object');
   return bytesToHex(serializeObject(json));
+};
+
+exports.encodeForSigning = function(json) {
+  assert(typeof json === 'object');
+  return bytesToHex(signingData(json));
+};
+
+exports.encodeForMultisigning = function(json, signer) {
+  assert(typeof json === 'object');
+  return bytesToHex(multiSigningData(json, signer));
 };
