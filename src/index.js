f722514ecf998fdfb80a153d1612fcff728c8f8e
Chris Clark <clark@ripple.com>
2015-10-30 15:22:03 -0700

    Fix samples

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@
 // Enable core-js polyfills. This allows use of ES6/7 extensions listed here:
 // https://github.com/zloirock/core-js/blob/fb0890f32dabe8d4d88a4350d1b268446127132e/shim.js#L1-L103
 /* eslint-enable max-len */
-require('babel-core/polyfill');
+
+// In node.js env, polyfill might be already loaded (from any npm package),
+// that's why we do this check.
+if (!global._babelPolyfill) {
+  require('babel-core/polyfill');
+}
 
 const _ = require('lodash');
 const EventEmitter = require('events').EventEmitter;
