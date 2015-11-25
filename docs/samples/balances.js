5d5cf868a2ddb1b1cd40e4a4f0a782d0066c2055
Chris Clark <clark@ripple.com>
2015-11-25 10:26:30 -0800

    Fix samples

--- docs/samples/balances.js
+++ docs/samples/balances.js
@@ -1,7 +1,7 @@
 'use strict';
 const RippleAPI = require('../../src').RippleAPI; // require('ripple-lib')
 
-const api = new RippleAPI({servers: ['wss://s1.ripple.com:443']});
+const api = new RippleAPI({server: 'wss://s1.ripple.com:443'});
 const address = 'r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV';
 
 api.connect().then(() => {
