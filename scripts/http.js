ae877c6026e7a5362617d88c34cba17e933627f6
Alan Cohen <alan@ripple.com>
2015-12-09 17:00:57 -0800

    Remove Windows line endings

--- scripts/http.js
+++ scripts/http.js
@@ -4,13 +4,11 @@ const createHTTPServer = require('../src/index').createHTTPServer;
 const port = 5990;
 const serverUrl = 'wss://s1.ripple.com';
 
-
 function main() {
   const server = createHTTPServer({server: serverUrl}, port);
   server.start().then(() => {
     console.log('Server started on port ' + String(port));
   });
 }
-
 
 main();
