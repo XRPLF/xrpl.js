016e82ab5d1116d19b8aa8c61c2af983c9547daf
wilsonianb <bwilson@ripple.com>
2017-03-30 19:01:58 -0700

    Simplify EscrowCreate parameters

--- src/ledger/parse/escrow-creation.js
+++ src/ledger/parse/escrow-creation.js
@@ -5,34 +5,18 @@ const assert = require('assert')
 const utils = require('./utils')
 const parseAmount = require('./amount')
 
-function removeGenericCounterparty(amount, address) {
-  return amount.counterparty === address ?
-    _.omit(amount, 'counterparty') : amount
-}
-
 function parseEscrowCreation(tx: Object): Object {
   assert(tx.TransactionType === 'EscrowCreate')
 
-  const source = {
-    address: tx.Account,
-    maxAmount: removeGenericCounterparty(
-      parseAmount(tx.SendMax || tx.Amount), tx.Account),
-    tag: tx.SourceTag
-  }
-
-  const destination = {
-    address: tx.Destination,
-    amount: removeGenericCounterparty(parseAmount(tx.Amount), tx.Destination),
-    tag: tx.DestinationTag
-  }
-
   return utils.removeUndefined({
-    source: utils.removeUndefined(source),
-    destination: utils.removeUndefined(destination),
+    amount: parseAmount(tx.Amount).value,
+    destination: tx.Destination,
     memos: utils.parseMemos(tx),
     condition: tx.Condition,
     allowCancelAfter: utils.parseTimestamp(tx.CancelAfter),
-    allowExecuteAfter: utils.parseTimestamp(tx.FinishAfter)
+    allowExecuteAfter: utils.parseTimestamp(tx.FinishAfter),
+    sourceTag: tx.SourceTag,
+    destinationTag: tx.DestinationTag
   })
 }
 
--- src/transaction/escrow-creation.js
+++ src/transaction/escrow-creation.js
@@ -2,18 +2,20 @@
 'use strict' // eslint-disable-line strict
 const _ = require('lodash')
 const utils = require('./utils')
-const {validate, iso8601ToRippleTime, toRippledAmount} = utils.common
+const {validate, iso8601ToRippleTime, xrpToDrops} = utils.common
 const ValidationError = utils.common.errors.ValidationError
 import type {Instructions, Prepare} from './types.js'
 import type {Adjustment, MaxAdjustment, Memo} from '../common/types.js'
 
 type EscrowCreation = {
-  source: MaxAdjustment,
-  destination: Adjustment,
+  amount: string,
+  destination: string,
   memos?: Array<Memo>,
   condition?: string,
   allowCancelAfter?: string,
-  allowExecuteAfter?: string
+  allowExecuteAfter?: string,
+  sourceTag?: number,
+  destinationTag?: number
 }
 
 function createEscrowCreationTransaction(account: string,
@@ -22,13 +24,10 @@ function createEscrowCreationTransaction(account: string,
   const txJSON: Object = {
     TransactionType: 'EscrowCreate',
     Account: account,
-    Destination: payment.destination.address,
-    Amount: toRippledAmount(payment.destination.amount)
+    Destination: payment.destination,
+    Amount: xrpToDrops(payment.amount),
   }
 
-  if (txJSON.Amount.currency !== undefined) {
-    throw new ValidationError('"Amount" currency must be XRP')
-  }
   if (payment.condition !== undefined) {
     txJSON.Condition = payment.condition
   }
@@ -38,11 +37,11 @@ function createEscrowCreationTransaction(account: string,
   if (payment.allowExecuteAfter !== undefined) {
     txJSON.FinishAfter = iso8601ToRippleTime(payment.allowExecuteAfter)
   }
-  if (payment.source.tag !== undefined) {
-    txJSON.SourceTag = payment.source.tag
+  if (payment.sourceTag !== undefined) {
+    txJSON.SourceTag = payment.sourceTag
   }
-  if (payment.destination.tag !== undefined) {
-    txJSON.DestinationTag = payment.destination.tag
+  if (payment.destinationTag !== undefined) {
+    txJSON.DestinationTag = payment.destinationTag
   }
   if (payment.memos !== undefined) {
     txJSON.Memos = _.map(payment.memos, utils.convertMemo)
