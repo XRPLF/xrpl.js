db2d7bfaa2b486162f4f54ff8c46becd5444fb16
Fred K. Schott <fschott@google.com>
2017-12-04 11:41:27 -0800

    add docs for custom node typing

--- custom_typings/node.d.ts
+++ custom_typings/node.d.ts
@@ -1,3 +1,7 @@
+/**
+ * This is an extension of Node's `process` object to include the browser
+ * property, which is added by webpack.
+ */
 interface AmbiguousProcess extends NodeJS.Process {
   browser?: true
 }
