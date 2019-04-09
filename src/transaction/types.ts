6469d68a7ae7dc683e82852d633e1a1ff4c384a0
Daniel Chan <daniel.chan@HKMAC0082.local>
2019-04-09 16:32:37 +0800

    add deliveredAmount as optional field for type Outcome

--- src/transaction/types.ts
+++ src/transaction/types.ts
@@ -82,6 +82,11 @@ export type Outcome = {
     }[]
   },
   orderbookChanges: object,
+  deliveredAmount?: {
+    currency: string;
+    value: string;
+    counterparty?: string;
+  };
   timestamp?: string
 }
 
