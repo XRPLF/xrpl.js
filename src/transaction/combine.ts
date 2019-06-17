2069d2e6429f712b7773f9dd759280ee75f5b27f
Sohail Salehi <info@soolan.com>
2019-06-17 17:13:53 +0800

    refactor combine.ts
    - replace lodash methods with native js
    - simplify the code
    - add a new method for transaction validation

--- src/transaction/combine.ts
+++ src/transaction/combine.ts
@@ -1,39 +1,27 @@
-import * as _ from 'lodash'
 import binary = require('ripple-binary-codec')
 import * as utils from './utils'
-import BigNumber from 'bignumber.js'
-import {decodeAddress} from 'ripple-address-codec'
 import {validate} from '../common'
 import {computeBinaryTransactionHash} from 'ripple-hashes'
 
-function addressToBigNumber(address) {
-  const hex = (Buffer.from(decodeAddress(address))).toString('hex')
-  return new BigNumber(hex, 16)
-}
-
-function compareSigners(a, b) {
-  return addressToBigNumber(a.Signer.Account)
-    .comparedTo(addressToBigNumber(b.Signer.Account))
+function hashArrayToObjectArray( signedTransactions : Array<string> ): Array {
+    validate.combine({signedTransactions})
+    const transactions: any[] = signedTransactions.map(binary.decode)
+    const tx = transactions[0]
+    delete tx.Signers
+    transactions.forEach( _tx => {
+        delete _tx.Signers;
+        if ( JSON.stringify(tx) !== JSON.stringify(_tx)) {
+            throw new utils.common.errors.ValidationError('txJSON is not the same for all signedTransactions')
+        }
+    })
+    return transactions
 }
 
 function combine(signedTransactions: Array<string>): object {
-  validate.combine({signedTransactions})
-
-  // TODO: signedTransactions is an array of strings in the documentation, but
-  // tests and this code handle it as an array of objects. Fix!
-  const txs: any[] = _.map(signedTransactions, binary.decode)
-  const tx = _.omit(txs[0], 'Signers')
-  if (!_.every(txs, _tx => _.isEqual(tx, _.omit(_tx, 'Signers')))) {
-    throw new utils.common.errors.ValidationError(
-      'txJSON is not the same for all signedTransactions')
-  }
-  const unsortedSigners = _.reduce(txs, (accumulator, _tx) =>
-    accumulator.concat(_tx.Signers || []), [])
-  const signers = unsortedSigners.sort(compareSigners)
-  const signedTx = _.assign({}, tx, {Signers: signers})
-  const signedTransaction = binary.encode(signedTx)
-  const id = computeBinaryTransactionHash(signedTransaction)
-  return {signedTransaction, id}
+    const txs = hashArrayToObjectArray(signedTransactions)
+    const signedTransaction = binary.encode(txs[0])
+    const id = computeBinaryTransactionHash(signedTransaction)
+    return {signedTransaction, id}
 }
 
 export default combine
