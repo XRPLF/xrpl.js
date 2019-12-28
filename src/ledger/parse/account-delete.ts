260bb06c0d0d5aac37e317264243ba4e376ccece
Elliot Lee <github.public@intelliot.com>
2019-12-28 11:35:17 -0800

    Update FormattedAccountDelete type

--- src/ledger/parse/account-delete.ts
+++ src/ledger/parse/account-delete.ts
@@ -11,6 +11,10 @@ export type FormattedAccountDelete = {
   // for the recipient of the deleted account's leftover XRP. NB: Ensure that the hosted recipient is
   // able to account for AccountDelete transactions; if not, your balance may not be properly credited.
   destinationTag?: number
+
+  // X-address of an account to receive any leftover XRP after deleting the sending account.
+  // Must be a funded account in the ledger, and must not be the sending account.
+  destinationXAddress: string
 }
 
 function parseAccountDelete(tx: any): FormattedAccountDelete {
