adaa75d180532cbe711d995752634f872501e7b0
Hans Bergren <hansbergren@gmail.com>
2020-02-05 13:19:47 -0500

    Update Mocha and NYC test configuration (#102)
    - Add TSNode to get proper coverage on TS files
    - Tell Mocha to look for tests in subdirectories
    - Configure NYC to look for coverage on all src files
    - Move `source-map-support` to being a dev dependency
    - Ensure NYC keeps us at 100% code coverage

--- packages/ripple-keypairs/.mocharc.js
+++ packages/ripple-keypairs/.mocharc.js
@@ -3,6 +3,13 @@ module.exports = {
   slow: 500,
   timeout: 5000,
 
+  // Required to get proper coverage on TypeScript files
+  // transpile-only is required if we use custom types
+  require: ['ts-node/register/transpile-only', 'source-map-support/register'],
+
+  // Look for tests in subdirectories
+  recursive: true,
+
   // Check for global variable leaks
-  "check-leaks": true,
+  'check-leaks': true,
 }
