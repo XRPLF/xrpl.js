3fb50f549ea1fb1b787ca22ba0bf96213ea92ca4
Nathan Nichols <natenichols@cox.net>
2021-07-22 12:20:45 -0500

    run integration tests with puppeteer

--- test/integration/integration-test.ts
+++ test/integration/integration-test.ts
@@ -12,9 +12,11 @@ import {isValidSecret} from 'ripple-api/common/utils'
 const TIMEOUT = 20000
 const INTERVAL = 1000 // how long to wait between checks for validated ledger
 
-const HOST = process.env.HOST ?? "127.0.0.1"
+const HOST = process.env.HOST ?? "0.0.0.0"
 const PORT = process.env.PORT ?? "6006"
 const serverUrl = `ws://${HOST}:${PORT}`
+
+console.log(serverUrl)
 
 function acceptLedger(api) {
   return api.connection.request({command: 'ledger_accept'})
