f1f5b7f8029ae10930122e40f910fd1cd6c86396
Nathan Nichols <natenichols@cox.net>
2021-08-04 12:22:43 -0500

    initial work on ts prepare functions

---
+++ snippets/src/v2-prepare.ts
@@ -0,0 +1,149 @@
+import { Connection } from "../../src/common";
+import { XrpLedgerTransaction, OfferCreateTransaction } from "./v2-transactions";
+
+interface AccountInfoRequest {
+    command: "account_info";
+    id?: string | number;
+    account: string;
+    ledger_hash?: string;
+    ledger_index?: string;
+    queue?: boolean;
+    signer_lists?: boolean;
+    strict?: boolean
+}
+
+interface AccountInfoResponse {
+    id?: string | number;
+    status: "success" | "error"
+    result: {
+        account_data: {
+            Account: string;
+            Sequence: number
+        }
+    }
+}
+
+interface ServerInfoRequest {
+    id?: string | number
+    command: "server_info"
+}
+
+interface ServerInfoResponse {
+    id?: string | number
+    status: "success" | "error"
+    result: {
+        info: {
+            validated_ledger: {
+                base_fee_xrp: number
+            }
+            load_factor: number
+        }
+    }
+}
+
+class RippleAPI {
+    private connection: Connection;
+
+    public request(r: AccountInfoRequest): Promise<AccountInfoResponse>
+    public request(r: ServerInfoRequest): Promise<ServerInfoResponse>
+    public request<Request, Response>(r: Request): Promise<Response> 
+    public request<Request, Response>(r: Request): Promise<Response> {
+        return this.connection.request(r); 
+    }
+
+    //sugar
+    getNextSequence = getNextSequence
+    getFee = getFee
+    autofill = autofill
+
+    prepare = prepare
+}
+
+async function getNextSequence(this: RippleAPI, account: string): Promise<number> {
+    const accountInfo = await this.request({
+        command: 'account_info',
+        account,
+    })
+
+    return accountInfo?.result?.account_data?.Sequence
+}
+
+async function getFee(this: RippleAPI): Promise<number> {
+    const serverInfo = await this.request({command: 'server_info'})
+
+    const base = serverInfo?.result?.info?.validated_ledger?.base_fee_xrp
+    const load_factor = serverInfo?.result?.info?.load_factor
+
+    return base * load_factor
+}
+
+async function autofill<T extends XrpLedgerTransaction>(
+    this: RippleAPI,
+    tx: T
+): Promise<T> {
+    const sequence = await this.getNextSequence(tx.Account)
+    const fee = await this.getFee()
+
+    tx.Sequence = sequence
+    tx.Fee = String(fee)
+
+    return tx
+}
+
+async function prepareOfferCreate(
+    api: RippleAPI,
+    tx: OfferCreateTransaction
+): Promise<OfferCreateTransaction> {
+    if (tx.TakerGets === undefined)
+        throw new Error("missing TakerGets")
+
+    if (tx.TakerPays === undefined)
+        throw new Error("missingTakerGets")
+
+    const filled = await api.autofill(tx);
+
+    return filled
+}
+
+function prepare<T extends XrpLedgerTransaction>(
+    this: RippleAPI,
+    transaction: T
+): Promise<T> {
+    let prepared: T | undefined
+
+    switch (transaction.TransactionType) {
+        default:
+            throw new Error ("Unknown TransactionType")
+        case "OfferCreate": 
+            return prepareOfferCreate(this, transaction)
+        case "Payment":
+            console.log(transaction)
+    }
+
+    return prepared
+}
+
+
+async function signAndSubmit() {
+    const api = new RippleAPI()
+
+    const tx: OfferCreateTransaction = {
+        Account: "FOO",
+        TransactionType: "OfferCreate",
+        TakerGets: "10",
+        TakerPays: "100", 
+    }
+
+    const offerCreate = prepareOfferCreate(api, tx)
+
+    const prepared = await api.prepare(tx)
+}
+
+const func2 = <A extends string>(a: A): A => {
+    //stuff
+    return `foo`  // Error!
+    //stuff
+}
+
+
+signAndSubmit()
---
+++ snippets/src/v2-transactions.ts
@@ -0,0 +1,197 @@
+import { tx_json } from "../../src/common/validate"
+
+export interface Memo {
+  MemoData?: string;
+  MemoType?: string;
+  MemoFormat?: string;
+}
+
+interface BaseTransaction {
+  Account: string;
+  TransactionType: string;
+  Fee?: string;
+  Sequence?: number;
+  AccountTxnID?: string;
+  Flags?: number;
+  LastLedgerSequence?: number;
+  Memos?: Array<{ Memo: Memo }>;
+  Signers?: Array<string>;
+  SourceTag?: number;
+  SigningPublicKey?: string;
+  TicketSequence?: number;
+  TxnSignature?: string;
+}
+
+export interface AccountSetTransaction extends BaseTransaction {
+  TransactionType: "AccountSet";
+  ClearFlag?: number;
+  Domain?: string;
+  EmailHash?: string;
+  MessageKey?: string;
+  SetFlag?: number;
+  TransferRate?: number;
+  TickSize?: number;
+}
+
+export interface AccountDeleteTransaction extends BaseTransaction {
+  TransactionType: "AccountDelete";
+  Destination: string;
+  DestinationTag?: number;
+}
+
+export interface CheckCancelTransaction extends BaseTransaction {
+  TransactionType: "CheckCancel";
+  CheckID: string;
+}
+
+export interface CheckCashTransaction extends BaseTransaction {
+  TransactionType: "CheckCash";
+  CheckID: string;
+  Amount: string;
+  DeliverMin: string;
+}
+
+export interface CheckCreateTransaction extends BaseTransaction {
+  TransactionType: "CheckCreate";
+  Destination: string;
+  SendMax: string;
+  DestinationTag?: number;
+  Expiration?: number;
+  InvoiceID?: string;
+}
+
+export interface DepositPreauthTransaction extends BaseTransaction {
+  TransactionType: "DepositPreauth";
+  Authorize?: string;
+  Unauthorize?: string;
+}
+
+export interface EscrowCancelTransaction extends BaseTransaction {
+  TransactionType: "EscrowCancel";
+  Owner: string;
+  OfferSequence: number;
+}
+
+export interface EscrowCreateTransaction extends BaseTransaction {
+  TransactionType: "EscrowCreate";
+  Amount: string;
+  Destination: string;
+  CancelAfter?: number;
+  FinishAfter?: number;
+  Condition?: string;
+  DestinationTag?: number;
+}
+
+export interface EscrowFinishTransaction extends BaseTransaction {
+  TransactionType: "EscrowFinish";
+  Owner: string;
+  OfferSequence: number;
+  Condition?: string;
+  Fulfillment?: string;
+}
+
+export interface OfferCancelTransaction extends BaseTransaction {
+  TransactionType: "OfferCancel";
+  OfferSequence?: number;
+}
+
+export interface OfferCreateTransaction extends BaseTransaction {
+  TransactionType: "OfferCreate";
+  Expiration?: number;
+  OfferSequence?: number;
+  TakerGets: string;
+  TakerPays: string;
+}
+
+interface Path {
+  account?: string;
+  currency?: string;
+  issuer?: string;
+}
+
+export interface PaymentTransaction extends BaseTransaction {
+  TransactionType: "Payment";
+  Amount: string;
+  Destination: string;
+  DestinationTag?: number;
+  InvoiceID?: number;
+  Paths: Array<Path>;
+  SendMax: string;
+  DeliverMin: string;
+}
+
+export interface PaymentChannelClaimTransaction extends BaseTransaction {
+  TransactionType: "PaymentChannelClaim";
+  Channel: string;
+  Balance?: string;
+  Amount?: string;
+  Signature?: string;
+  PublicKey?: string;
+}
+
+export interface PaymentChannelCreateTransaction extends BaseTransaction {
+  TransactionType: "PaymentChannelCreate";
+  Amount: string;
+  Destination: string;
+  SettleDelay: number;
+  PublicKey: string;
+  CancelAfter?: number;
+  DestinationTag?: number;
+}
+
+export interface PaymentChannelFundTransaction extends BaseTransaction {
+  TransactionType: "PaymentChannelFund";
+  Channel: string;
+  Amount: string;
+  Expiration?: number;
+}
+
+export interface SetRegularKeyTransaction extends BaseTransaction {
+  TransactionType: "SetRegularKey";
+  RegularKey?: string;
+}
+
+interface SignerList {
+  SignerEntry: {
+    Account: string;
+    SignerWeight: number;
+  };
+}
+
+export interface SignerListSetTransaction extends BaseTransaction {
+  TransactionType: "SignerListSet";
+  SignerQuorum: number;
+  SignerEntries?: Array<SignerList>;
+}
+
+export interface TicketCreateTransaction extends BaseTransaction {
+  TransactionType: "TicketCreate";
+  TicketCount: number;
+}
+
+export interface TrustSetTransaction extends BaseTransaction {
+  TransactionType: "TrustSet";
+  LimitAmount: string;
+  QualityIn?: number;
+  QualityOut?: number;
+}
+
+export type XrpLedgerTransaction = AccountSetTransaction
+  | AccountDeleteTransaction
+  | CheckCancelTransaction
+  | CheckCashTransaction
+  | CheckCreateTransaction
+  | DepositPreauthTransaction
+  | EscrowCancelTransaction
+  | EscrowCreateTransaction
+  | EscrowFinishTransaction
+  | OfferCancelTransaction
+  | OfferCreateTransaction
+  | PaymentTransaction
+  | PaymentChannelClaimTransaction
+  | PaymentChannelCreateTransaction
+  | PaymentChannelFundTransaction
+  | SetRegularKeyTransaction
+  | SignerListSetTransaction
+  | TicketCreateTransaction
+  | TrustSetTransaction
