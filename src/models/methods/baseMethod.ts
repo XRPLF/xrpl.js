dba5e635be92d235d9942fe05988402e62324dab
Mayukha Vadari <mvadari@ripple.com>
2021-08-09 14:36:48 -0700

    Defines TypeScript types for rippled account method requests & responses (#1498)
    * account_channels
    
    * account_currencies
    
    * account_info
    
    * account_lines
    
    * account_objects
    
    * account_offers
    
    * account_tx
    
    * gateway_balances
    
    * no ripple check
    
    * respond to comments
    
    * export methods
    
    * fix typos
    
    * respond to comments
    
    * edit BaseResponse to be more specific

---
+++ src/models/methods/baseMethod.ts
@@ -0,0 +1,26 @@
+import { Response } from ".";
+
+export interface BaseRequest {
+    id: number | string
+    command: string
+    api_version?: number
+}
+
+interface Warning {
+    id: number
+    message: string
+    details?: {[key: string]: string}
+}
+
+export interface BaseResponse {
+    id: number | string
+    status: "success" | "error" | string
+    type: "response" | string
+    result: any
+    warning?: "load"
+    warnings?: Warning[]
+    forwarded?: boolean
+    error?: string
+    request?: Response
+    api_version?: number
+}
