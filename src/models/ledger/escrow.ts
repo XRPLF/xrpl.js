7573e41eb5b5e45b130917621c9bfc8621178327
Mayukha Vadari <mvadari@ripple.com>
2021-08-09 09:54:56 -0700

    Defines TypeScript types for all ledger objects (#1499)
    * offer
    
    * accountroot
    
    * amendments
    
    * check
    
    * deposit preauth
    
    * directory node
    
    * escrow
    
    * fee settings
    
    * ledger hashes
    
    * negative unl
    
    * pay channel
    
    * ripple state
    
    * signer list
    
    * ticket
    
    * export
    
    * respond to comments
    
    * rename files to camelCase

---
+++ src/models/ledger/escrow.ts
@@ -0,0 +1,18 @@
+import { BaseLedgerEntry } from "./baseLedgerEntry";
+
+export interface Escrow extends BaseLedgerEntry {
+    LedgerEntryType: 'Escrow'
+    Account: string
+    Destination: string
+    Amount: string
+    Condition?: string
+    CancelAfter?: number
+    FinishAfter?: number
+    Flags: number
+    SourceTag?: number
+    DestinationTag?: number
+    OwnerNode: string
+    DestinationNode?: string
+    PreviousTxnID: string
+    PreviousTxnLgrSeq: number
+  }
