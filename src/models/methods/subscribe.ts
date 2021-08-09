d644d00c845a5b25fc0e15c1aad64122fc76c147
Mayukha Vadari <mvadari@ripple.com>
2021-08-09 11:31:54 -0700

    streams

--- src/models/methods/subscribe.ts
+++ src/models/methods/subscribe.ts
@@ -1,7 +1,8 @@
 import { BaseRequest, BaseResponse } from './baseMethod'
-import { Currency, LedgerIndex } from "../common"
+import { Currency } from "../common"
+import { TransactionMetadata } from '../common/transaction'
 
-type Stream = "consensus" | "ledger" | "manifests" | "peer_status" | "transactions" | "transactions_proposed" | "server" | "validations"
+type StreamType = "consensus" | "ledger" | "manifests" | "peer_status" | "transactions" | "transactions_proposed" | "server" | "validations"
 
 interface Book {
     taker_gets: Currency
@@ -13,7 +14,7 @@ interface Book {
   
 export interface SubscribeRequest extends BaseRequest {
     command: "subscribe"
-    streams?: Stream[]
+    streams?: StreamType[]
     accounts?: string[]
     accounts_proposed?: string[]
     books?: Book[]
@@ -25,3 +26,84 @@ export interface SubscribeRequest extends BaseRequest {
 export interface SubscribeResponse extends BaseResponse {
     result: any
 }
+
+interface BaseStream {
+    type: string
+}
+
+export interface LedgerStream extends BaseStream {
+    type: "ledgerClosed"
+    fee_base: number
+    fee_ref: number
+    ledger_hash: string
+    ledger_index: number
+    ledger_time: number
+    reserve_base: number
+    reserve_inc: number
+    txn_count: number
+    validated_ledgers?: string
+}
+
+export interface ValidationStream extends BaseStream {
+    type: "validationReceived"
+    amendments?: string[]
+    base_fee?: number
+    flags: number
+    full: boolean
+    ledger_hash: string
+    ledger_index: string
+    load_fee?: number
+    master_key?: string
+    reserve_base?: number
+    reserve_inc?: number
+    signature: string
+    signing_time: number
+    validation_public_key: string
+}
+
+export interface TransactionStream extends BaseStream {
+    status: string
+    type: "transaction"
+    engine_result: string
+    engine_result_code: number
+    engine_result_message: string
+    ledger_current_index?: number
+    ledger_hash?: string
+    ledger_index?: number
+    meta: TransactionMetadata
+    transaction: any // TODO: replace when we have types for transactions
+    validated: boolean
+}
+
+export interface PeerStatusStream extends BaseStream {
+    type: "peerStatusChange"
+    action: "CLOSING_LEDGER" | "ACCEPTED_LEDGER" | "SWITCHED_LEDGER" | "LOST_SYNC"
+    date: number
+    ledger_hash?: string
+    ledger_index?: number
+    ledger_index_max?: number
+    ledger_index_min?: number
+}
+
+export interface OrderBookStream extends BaseStream {
+    status: string
+    type: "transaction"
+    engine_result: string
+    engine_result_code: number
+    engine_result_message: string
+    ledger_current_index?: number
+    ledger_hash?: string
+    ledger_index?: number
+    meta: TransactionMetadata
+    transaction: any // TODO: replace when we have types for transactions
+    // TODO: transactions for this object have a special case for OfferCreate
+    // https://xrpl.org/subscribe.html#order-book-streams
+    validated: boolean
+}
+
+export interface ConsensusStream extends BaseStream {
+    type: "consensusPhase"
+    consensus: "open" | "establish" | "accepted"
+}
+
+export type Stream = LedgerStream | ValidationStream | TransactionStream | PeerStatusStream | OrderBookStream | ConsensusStream
