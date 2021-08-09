cb5f6fc49c5f4f5a5fd9e8307b2e5766f84c61a7
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-08-09 18:40:39 -0400

    AccountSet Model

---
+++ src/models/transactions/accountSet.ts
@@ -0,0 +1,71 @@
+import { ValidationError } from "../../common/errors";
+import { CommonFields, verifyCommonFields } from "./common";
+
+enum AccountSetFlagEnum {
+    asfRequireDest = 1,
+    asfRequireAuth = 2,
+    asfDisallowXRP = 3,
+    asfDisableMaster = 4,
+    asfAccountTxnID = 5,
+    asfNoFreeze = 6,
+    asfGlobalFreeze = 7,
+    asfDefaultRipple = 8,
+    asfDepositAuth = 9,
+}
+
+export interface AccountSet extends CommonFields {
+    TransactionType: "AccountSet";
+    ClearFlag?: number;
+    Domain?: string;
+    EmailHash?: string;
+    MessageKey?: string;
+    SetFlag?: AccountSetFlagEnum;
+    TransferRate?: number;
+    TickSize?: number;
+    WalletLocator?: string;
+    WalletSize?: number;
+}
+
+/**
+ * Verify the form and type of an AccountSet at runtime.
+ * 
+ * @param tx - An AccountSet Transaction
+ * @returns - Void.
+ * @throws - When the AccountSet is Malformed.
+ */
+ export function verifyAccountSet(tx: AccountSet): void {
+    verifyCommonFields(tx)
+
+    if (tx.ClearFlag !== undefined){
+        if (typeof tx.ClearFlag !== 'number')
+            throw new ValidationError("AccountSet: invalid ClearFlag")
+        if (0 > tx.ClearFlag || tx.ClearFlag > 9)
+            throw new ValidationError("AccountSet: invalid ClearFlag")
+    }
+
+    if (tx.Domain !== undefined && typeof tx.Domain !== 'string')
+        throw new ValidationError("AccountSet: invalid Domain")
+
+    if (tx.EmailHash !== undefined && typeof tx.EmailHash !== 'string')
+        throw new ValidationError("AccountSet: invalid EmailHash")
+        
+    if (tx.MessageKey !== undefined && typeof tx.MessageKey !== 'string')
+        throw new ValidationError("AccountSet: invalid MessageKey")
+    
+    if (tx.SetFlag !== undefined){
+        if (typeof tx.SetFlag !== 'number')
+            throw new ValidationError("AccountSet: invalid SetFlag")
+        if (0 > tx.SetFlag || tx.SetFlag > 9)
+            throw new ValidationError("AccountSet: invalid SetFlag")
+    }
+
+    if (tx.TransferRate !== undefined && typeof tx.TransferRate !== 'number')
+        throw new ValidationError("AccountSet: invalid TransferRate")
+    
+    if (tx.TickSize !== undefined){
+        if (typeof tx.TickSize !== 'number')
+            throw new ValidationError("AccountSet: invalid TickSize")
+        if (tx.TickSize !== 0 && (3 > tx.TickSize || tx.TickSize > 15))
+            throw new ValidationError("AccountSet: invalid TickSize")
+    }
+}
--- test/verify-test.ts
+++ test/verify-test.ts
@@ -1,6 +1,7 @@
 import { ValidationError } from 'ripple-api/common/errors'
 import { verifyCommonFields } from './../src/models/transactions/common'
 import { verifyOfferCreate } from './../src/models/transactions/offerCreate'
+import { verifyAccountSet } from './../src/models/transactions/accountSet'
 import { assert } from 'chai'
 
 /**
@@ -404,4 +405,154 @@ describe('Transaction Verification', function () {
             "OfferCreate: invalid TakerGets"
         )
     })
+
+    it (`verifies valid AccountSet`, () => {
+        const account = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            Domain : "6578616D706C652E636F6D",
+            SetFlag : 5,
+            MessageKey : "03AB40A0490F9B7ED8DF29D246BF2D6269820A0EE7742ACDD457BEA7C7D0931EDB"
+        } as any
+            
+        assert.doesNotThrow(() => verifyAccountSet(account))
+    })
+
+    it (`throws w/ invalid SetFlag`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            Domain : "6578616D706C652E636F6D",
+            SetFlag : 12,
+            MessageKey : "03AB40A0490F9B7ED8DF29D246BF2D6269820A0EE7742ACDD457BEA7C7D0931EDB"
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid SetFlag"
+        )
+    })
+
+    it (`throws w/ invalid SetFlag`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            Domain : "6578616D706C652E636F6D",
+            SetFlag : 'abc',
+            MessageKey : "03AB40A0490F9B7ED8DF29D246BF2D6269820A0EE7742ACDD457BEA7C7D0931EDB"
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid SetFlag"
+        )
+    })
+
+    it (`throws w/ invalid ClearFlag`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            Domain : "6578616D706C652E636F6D",
+            ClearFlag : 12,
+            MessageKey : "03AB40A0490F9B7ED8DF29D246BF2D6269820A0EE7742ACDD457BEA7C7D0931EDB"
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid ClearFlag"
+        )
+    })
+
+    it (`throws w/ invalid Domain`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            Domain : 6578616,
+            MessageKey : "03AB40A0490F9B7ED8DF29D246BF2D6269820A0EE7742ACDD457BEA7C7D0931EDB"
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid Domain"
+        )
+    })
+
+    it (`throws w/ invalid EmailHash`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            EmailHash : 657861645678909876543456789876543
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid EmailHash"
+        )
+    })
+
+    it (`throws w/ invalid MessageKey`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            MessageKey : 65786165678908765456789567890678
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid MessageKey"
+        )
+    })
+
+    it (`throws w/ invalid TransferRate`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            TransferRate : "1000000001"
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid TransferRate"
+        )
+    })
+
+    it (`throws w/ invalid TickSize`, () => {
+        const offer = {
+            TransactionType : "AccountSet",
+            Account : "rf1BiGeXwwQoi8Z2ueFYTEXSwuJYfV2Jpn",
+            Fee : "12",
+            Sequence : 5,
+            TickSize : 20
+        } as any
+
+        assert.throws(
+            () => verifyAccountSet(offer),
+            ValidationError,
+            "AccountSet: invalid TickSize"
+        )
+    })
+
 })
