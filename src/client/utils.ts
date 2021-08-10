b59de194b23f5990691115043ba8d36ccb38ab65
Mayukha Vadari <mvadari@ripple.com>
2021-08-10 15:32:36 -0700

    move api/server into clients folder

---
+++ src/client/utils.ts
@@ -0,0 +1,17 @@
+import * as common from '../common'
+
+
+function formatLedgerClose(ledgerClose: any): object {
+  return {
+    baseFeeXRP: common.dropsToXrp(ledgerClose.fee_base),
+    ledgerHash: ledgerClose.ledger_hash,
+    ledgerVersion: ledgerClose.ledger_index,
+    ledgerTimestamp: common.rippleTimeToISO8601(ledgerClose.ledger_time),
+    reserveBaseXRP: common.dropsToXrp(ledgerClose.reserve_base),
+    reserveIncrementXRP: common.dropsToXrp(ledgerClose.reserve_inc),
+    transactionCount: ledgerClose.txn_count,
+    validatedLedgerVersions: ledgerClose.validated_ledgers
+  }
+}
+
+export {formatLedgerClose}
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './api'
+export * from './client/api'
 
 export * from './transaction/types'
 
