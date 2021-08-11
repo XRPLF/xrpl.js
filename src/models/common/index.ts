a4207a552b44f1adb9041b87e96dd802a67140a0
Nathan Nichols <natenichols@cox.net>
2021-08-11 17:37:10 -0500

    refactor: Add common transaction fields Typescript definition (#1507)
    refactor: Add common transaction fields Typescript definition (#1507)

--- src/models/common/index.ts
+++ src/models/common/index.ts
@@ -19,6 +19,18 @@ export interface IssuedCurrencyAmount extends IssuedCurrency {
 
 export type Amount = IssuedCurrencyAmount | string
 
+export interface Signer {
+  Account: string;
+  TxnSignature: string;
+  SigningPubKey: string;
+}
+
+export interface Memo {
+  MemoData?: string;
+  MemoType?: string;
+  MemoFormat?: string;
+}
+
 export type StreamType = "consensus" | "ledger" | "manifests" | "peer_status" | "transactions" | "transactions_proposed" | "server" | "validations"
 
 interface PathStep {
