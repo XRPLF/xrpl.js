af328a09dbe9887563fcffb2a48dfd49ddc482c7
Mayukha Vadari <mvadari@ripple.com>
2021-08-11 13:42:04 -0700

    remove everything

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,53 +1,21 @@
 import {EventEmitter} from 'events'
 import {
   errors,
-  validate,
-  xrpToDrops,
-  dropsToXrp,
-  rippleTimeToISO8601,
-  iso8601ToRippleTime,
-  txFlags
 } from '../common'
 import { Connection, ConnectionUserOptions } from './connection'
 import {
   formatLedgerClose
 } from '../client/utils'
-import getTransaction from '../ledger/transaction'
-import getTransactions from '../ledger/transactions'
-import getTrustlines from '../ledger/trustlines'
-import getBalances from '../ledger/balances'
-import getBalanceSheet from '../ledger/balance-sheet'
-import getPaths from '../ledger/pathfind'
-import getOrders from '../ledger/orders'
-import {getOrderbook, formatBidsAndAsks} from '../ledger/orderbook'
-import {getSettings, parseAccountFlags} from '../ledger/settings'
-import getAccountInfo from '../ledger/accountinfo'
-import getAccountObjects from '../ledger/accountobjects'
-import getPaymentChannel from '../ledger/payment-channel'
-import preparePayment from '../transaction/payment'
-import prepareTrustline from '../transaction/trustline'
-import prepareOrder from '../transaction/order'
-import prepareOrderCancellation from '../transaction/ordercancellation'
-import prepareEscrowCreation from '../transaction/escrow-creation'
-import prepareEscrowExecution from '../transaction/escrow-execution'
-import prepareEscrowCancellation from '../transaction/escrow-cancellation'
-import preparePaymentChannelCreate from '../transaction/payment-channel-create'
-import preparePaymentChannelFund from '../transaction/payment-channel-fund'
-import preparePaymentChannelClaim from '../transaction/payment-channel-claim'
-import prepareCheckCreate from '../transaction/check-create'
-import prepareCheckCancel from '../transaction/check-cancel'
-import prepareCheckCash from '../transaction/check-cash'
-import prepareSettings from '../transaction/settings'
-import prepareTicketCreate from '../transaction/ticket'
-import {sign} from '../transaction/sign'
-import combine from '../transaction/combine'
-import submit from '../transaction/submit'
-import { generateAddress, generateXAddress } from '../offline/utils'
-import {deriveKeypair, deriveAddress, deriveXAddress} from '../offline/derive'
-import computeLedgerHash from '../offline/ledgerhash'
-import signPaymentChannelClaim from '../offline/sign-payment-channel-claim'
-import verifyPaymentChannelClaim from '../offline/verify-payment-channel-claim'
-import getLedger from '../ledger/ledger'
+// import {sign} from '../transaction/sign'
+// import combine from '../transaction/combine'
+// import submit from '../transaction/submit'
+// import { generateAddress, generateXAddress } from '../offline/utils'
+// import {deriveKeypair, deriveAddress, deriveXAddress} from '../offline/derive'
+// import computeLedgerHash from '../offline/ledgerhash'
+// import signPaymentChannelClaim from '../offline/sign-payment-channel-claim'
+// import verifyPaymentChannelClaim from '../offline/verify-payment-channel-claim'
+// import getLedger from '../ledger/ledger'
+
 import {
   Request,
   Response,
@@ -119,44 +87,8 @@ import {
   RandomRequest,
   RandomResponse
 } from '../models/methods'
-
-import RangeSet from './rangeset'
-import * as ledgerUtils from '../ledger/utils'
-import * as transactionUtils from '../transaction/utils'
-import * as schemaValidator from '../common/schema-validator'
-import {getServerInfo, getFee} from '../common/serverinfo'
 import {ensureClassicAddress} from '../common'
 import {clamp} from '../ledger/utils'
-import {TransactionJSON, Instructions, Prepare} from '../transaction/types'
-import {
-  classicAddressToXAddress,
-  xAddressToClassicAddress,
-  isValidXAddress,
-  isValidClassicAddress,
-  encodeSeed,
-  decodeSeed,
-  encodeAccountID,
-  decodeAccountID,
-  encodeNodePublic,
-  decodeNodePublic,
-  encodeAccountPublic,
-  decodeAccountPublic,
-  encodeXAddress,
-  decodeXAddress
-} from 'ripple-address-codec'
-import {
-  computeBinaryTransactionHash,
-  computeTransactionHash,
-  computeBinaryTransactionSigningHash,
-  computeAccountLedgerObjectID,
-  computeSignerListLedgerObjectID,
-  computeOrderID,
-  computeTrustlineHash,
-  computeTransactionTreeHash,
-  computeStateTreeHash,
-  computeEscrowHash,
-  computePaymentChannelHash
-} from '../common/hashes'
 
 export interface ClientOptions extends ConnectionUserOptions {
   server?: string
@@ -206,26 +138,26 @@ type MarkerResponse = AccountChannelsResponse
                     | LedgerDataResponse
 
 class Client extends EventEmitter {
-  _feeCushion: number
-  _maxFeeXRP: string
+  // _feeCushion: number
+  // _maxFeeXRP: string
 
   // New in > 0.21.0
   // non-validated ledger versions are allowed, and passed to rippled as-is.
   connection: Connection
 
-  // these are exposed only for use by unit tests; they are not part of the client.
-  static _PRIVATE = {
-    validate,
-    RangeSet,
-    ledgerUtils,
-    schemaValidator
-  }
+  // // these are exposed only for use by unit tests; they are not part of the client.
+  // static _PRIVATE = {
+  //   validate,
+  //   RangeSet,
+  //   ledgerUtils,
+  //   schemaValidator
+  // }
 
   constructor(options: ClientOptions = {}) {
     super()
-    validate.apiOptions(options)
-    this._feeCushion = options.feeCushion || 1.2
-    this._maxFeeXRP = options.maxFeeXRP || '2'
+    // validate.apiOptions(options)
+    // this._feeCushion = options.feeCushion || 1.2
+    // this._maxFeeXRP = options.maxFeeXRP || '2'
     const serverURL = options.server
     if (serverURL != null) {
       this.connection = new Connection(serverURL, options)
@@ -325,29 +257,6 @@ class Client extends EventEmitter {
   }
 
   /**
-   * Prepare a transaction.
-   *
-   * You can later submit the transaction with a `submit` request.
-   */
-  async prepareTransaction(
-    txJSON: TransactionJSON,
-    instructions: Instructions = {}
-  ): Promise<Prepare> {
-    return transactionUtils.prepareTransaction(txJSON, this, instructions)
-  }
-
-  /**
-   * Convert a string to hex.
-   *
-   * This can be used to generate `MemoData`, `MemoType`, and `MemoFormat`.
-   *
-   * @param string string to convert to hex
-   */
-  convertStringToHex(string: string): string {
-    return transactionUtils.convertStringToHex(string)
-  }
-
-  /**
    * Makes multiple paged requests to the client to return a given number of
    * resources. _requestAll() will make multiple requests until the `limit`
    * number of resources is reached (if no `limit` is provided, a single request
@@ -405,10 +314,6 @@ class Client extends EventEmitter {
     return results
   }
 
-  // @deprecated Use X-addresses instead & Invoke from top-level package instead
-  generateAddress = generateAddress
-  generateXAddress = generateXAddress // @deprecated Invoke from top-level package instead
-
   isConnected(): boolean {
     return this.connection.isConnected()
   }
@@ -422,152 +327,9 @@ class Client extends EventEmitter {
     // this method returns nothing. SO we await but don't return any result.
     await this.connection.disconnect()
   }
-
-  getLedgerVersion(): Promise<number> {
-    return this.connection.getLedgerVersion()
-  }
-
-  getServerInfo = getServerInfo
-  getFee = getFee
-
-  getTransaction = getTransaction
-  getTransactions = getTransactions
-  getTrustlines = getTrustlines
-  getBalances = getBalances
-  getBalanceSheet = getBalanceSheet
-  getPaths = getPaths
-  getOrderbook = getOrderbook
-  getOrders = getOrders
-  getSettings = getSettings
-  getAccountInfo = getAccountInfo
-  getAccountObjects = getAccountObjects
-  getPaymentChannel = getPaymentChannel
-  getLedger = getLedger
-  parseAccountFlags = parseAccountFlags
-
-  preparePayment = preparePayment
-  prepareTrustline = prepareTrustline
-  prepareOrder = prepareOrder
-  prepareOrderCancellation = prepareOrderCancellation
-  prepareEscrowCreation = prepareEscrowCreation
-  prepareEscrowExecution = prepareEscrowExecution
-  prepareEscrowCancellation = prepareEscrowCancellation
-  preparePaymentChannelCreate = preparePaymentChannelCreate
-  preparePaymentChannelFund = preparePaymentChannelFund
-  preparePaymentChannelClaim = preparePaymentChannelClaim
-  prepareCheckCreate = prepareCheckCreate
-  prepareCheckCash = prepareCheckCash
-  prepareCheckCancel = prepareCheckCancel
-  prepareTicketCreate = prepareTicketCreate
-  prepareSettings = prepareSettings
-  sign = sign
-  combine = combine
-
-  submit = submit // @deprecated Use client.request({command: 'submit', tx_blob: signedTransaction }) instead
-
-  deriveKeypair = deriveKeypair // @deprecated Invoke from top-level package instead
-  deriveAddress = deriveAddress // @deprecated Invoke from top-level package instead
-  computeLedgerHash = computeLedgerHash // @deprecated Invoke from top-level package instead
-  signPaymentChannelClaim = signPaymentChannelClaim // @deprecated Invoke from top-level package instead
-  verifyPaymentChannelClaim = verifyPaymentChannelClaim // @deprecated Invoke from top-level package instead
-  errors = errors
-
-  static deriveXAddress = deriveXAddress
-
-  // Client.deriveClassicAddress (static) is a new name for client.deriveAddress
-  static deriveClassicAddress = deriveAddress
-
-  static formatBidsAndAsks = formatBidsAndAsks
-
-  /**
-   * Static methods to expose ripple-address-codec methods
-   */
-   static classicAddressToXAddress = classicAddressToXAddress
-   static xAddressToClassicAddress = xAddressToClassicAddress
-   static isValidXAddress = isValidXAddress
-   static isValidClassicAddress = isValidClassicAddress
-   static encodeSeed = encodeSeed
-   static decodeSeed = decodeSeed
-   static encodeAccountID = encodeAccountID
-   static decodeAccountID = decodeAccountID
-   static encodeNodePublic = encodeNodePublic
-   static decodeNodePublic = decodeNodePublic
-   static encodeAccountPublic = encodeAccountPublic
-   static decodeAccountPublic = decodeAccountPublic
-   static encodeXAddress = encodeXAddress
-   static decodeXAddress = decodeXAddress
-
-  /**
-   * Static methods that replace functionality from the now-deprecated ripple-hashes library
-   */
-  // Compute the hash of a binary transaction blob.
-  // @deprecated Invoke from top-level package instead
-  static computeBinaryTransactionHash = computeBinaryTransactionHash // (txBlobHex: string): string
-  // Compute the hash of a transaction in txJSON format.
-  // @deprecated Invoke from top-level package instead
-  static computeTransactionHash = computeTransactionHash // (txJSON: any): string
-  // @deprecated Invoke from top-level package instead
-  static computeBinaryTransactionSigningHash = computeBinaryTransactionSigningHash // (txBlobHex: string): string
-  // Compute the hash of an account, given the account's classic address (starting with `r`).
-  // @deprecated Invoke from top-level package instead
-  static computeAccountLedgerObjectID = computeAccountLedgerObjectID // (address: string): string
-  // Compute the hash (ID) of an account's SignerList.
-  // @deprecated Invoke from top-level package instead
-  static computeSignerListLedgerObjectID = computeSignerListLedgerObjectID // (address: string): string
-  // Compute the hash of an order, given the owner's classic address (starting with `r`) and the account sequence number of the `OfferCreate` order transaction.
-  // @deprecated Invoke from top-level package instead
-  static computeOrderID = computeOrderID // (address: string, sequence: number): string
-  // Compute the hash of a trustline, given the two parties' classic addresses (starting with `r`) and the currency code.
-  // @deprecated Invoke from top-level package instead
-  static computeTrustlineHash = computeTrustlineHash // (address1: string, address2: string, currency: string): string
-  // @deprecated Invoke from top-level package instead
-  static computeTransactionTreeHash = computeTransactionTreeHash // (transactions: any[]): string
-  // @deprecated Invoke from top-level package instead
-  static computeStateTreeHash = computeStateTreeHash // (entries: any[]): string
-  // Compute the hash of a ledger.
-  // @deprecated Invoke from top-level package instead
-  static computeLedgerHash = computeLedgerHash // (ledgerHeader): string
-  // Compute the hash of an escrow, given the owner's classic address (starting with `r`) and the account sequence number of the `EscrowCreate` escrow transaction.
-  // @deprecated Invoke from top-level package instead
-  static computeEscrowHash = computeEscrowHash // (address, sequence): string
-  // Compute the hash of a payment channel, given the owner's classic address (starting with `r`), the classic address of the destination, and the account sequence number of the `PaymentChannelCreate` payment channel transaction.
-  // @deprecated Invoke from top-level package instead
-  static computePaymentChannelHash = computePaymentChannelHash // (address, dstAddress, sequence): string
-
-  xrpToDrops = xrpToDrops // @deprecated Invoke from top-level package instead
-  dropsToXrp = dropsToXrp // @deprecated Invoke from top-level package instead
-  rippleTimeToISO8601 = rippleTimeToISO8601 // @deprecated Invoke from top-level package instead
-  iso8601ToRippleTime = iso8601ToRippleTime // @deprecated Invoke from top-level package instead
-  txFlags = txFlags
-
-  isValidAddress = schemaValidator.isValidAddress
-  isValidSecret = schemaValidator.isValidSecret
 }
 
 export {
   Client,
   Connection
-}
-
-export type {
-  AccountObjectsRequest,
-  AccountObjectsResponse,
-  AccountOffersRequest,
-  AccountOffersResponse,
-  AccountInfoRequest,
-  AccountInfoResponse,
-  AccountLinesRequest,
-  AccountLinesResponse,
-  BookOffersRequest,
-  BookOffersResponse,
-  GatewayBalancesRequest,
-  GatewayBalancesResponse,
-  // LedgerRequest,
-  // LedgerResponse,
-  // LedgerDataRequest,
-  // LedgerDataResponse,
-  // LedgerEntryRequest,
-  // LedgerEntryResponse,
-  // ServerInfoRequest,
-  // ServerInfoResponse
 }
