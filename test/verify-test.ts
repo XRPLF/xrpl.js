d38313239a48901ca0729629d893b77e097dde27
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-08-11 11:35:31 -0400

    feat: add tests

--- test/verify-test.ts
+++ test/verify-test.ts
@@ -1,6 +1,7 @@
 import { ValidationError } from 'ripple-api/common/errors'
 import { verifyCommonFields } from './../src/models/transactions/common'
 import { verifyOfferCreate } from './../src/models/transactions/offerCreate'
+import { verifyCheckCash } from './../src/models/transactions/checkCash'
 import { assert } from 'chai'
 
 /**
@@ -402,6 +403,64 @@ describe('Transaction Verification', function () {
             () => verifyOfferCreate(offer),
             ValidationError,
             "OfferCreate: invalid TakerGets"
+        )
+    })
+
+    it (`verifies valid CheckCash`, () => {
+        const validCheckCash = {
+            Account : "rfkE1aSy9G8Upk4JssnwBxhEv5p4mn2KTy",
+            TransactionType : "CheckCash",
+            Amount : "100000000",
+            CheckID : "838766BA2B995C00744175F69A1B11E32C3DBC40E64801A4056FCBD657F57334",
+            Fee : "12"
+        } as any
+        
+        assert.doesNotThrow(() => verifyCheckCash(validCheckCash))
+    })
+
+    it (`throws w/ invalid CheckID`, () => {
+        const invalidCheckID = {
+            Account : "rfkE1aSy9G8Upk4JssnwBxhEv5p4mn2KTy",
+            TransactionType : "CheckCash",
+            Amount : "100000000",
+            CheckID : 83876645678909854567890
+        } as any
+
+        assert.throws(
+            () => verifyCheckCash(invalidCheckID),
+            ValidationError,
+            "CheckCash: invalid CheckID"
+        )
+    })
+
+    it (`throws w/ invalid Amount`, () => {
+        const invalidAmount = {
+            Account : "rfkE1aSy9G8Upk4JssnwBxhEv5p4mn2KTy",
+            TransactionType : "CheckCash",
+            Amount : 100000000,
+            CheckID : "838766BA2B995C00744175F69A1B11E32C3DBC40E64801A4056FCBD657F57334"
+        } as any
+
+        assert.throws(
+            () => verifyCheckCash(invalidAmount),
+            ValidationError,
+            "CheckCash: invalid Amount"
+        )
+    })
+
+    it (`throws w/ invalid DeliverMin`, () => {
+        const invalidDeliverMin = {
+            Account : "rfkE1aSy9G8Upk4JssnwBxhEv5p4mn2KTy",
+            TransactionType : "CheckCash",
+            Amount : "100000000",
+            DeliverMin: 852156963,
+            CheckID : "838766BA2B995C00744175F69A1B11E32C3DBC40E64801A4056FCBD657F57334"
+        } as any
+
+        assert.throws(
+            () => verifyCheckCash(invalidDeliverMin),
+            ValidationError,
+            "CheckCash: invalid DeliverMin"
         )
     })
 })
