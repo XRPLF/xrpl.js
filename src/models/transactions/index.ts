61a8923e2a4c7c761fb207f27f0a67bd0e8832dd
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-08-16 17:31:17 -0400

    refactor: define typescript types for CheckCancel Transaction Model  (#1526)
    * offer
    
    * accountroot
    
    * amendments
    
    * check
    
    * deposit preauth
    
    * directory node
    
    * escrow
    
    * fee settings
    
    * ledger hashes
    
    * negative unl
    
    * pay channel
    
    * ripple state
    
    * signer list
    
    * ticket
    
    * export
    
    * account_channels
    
    * account_currencies
    
    * account_info
    
    * account_lines
    
    * account_objects
    
    * account_offers
    
    * account_tx
    
    * gateway_balances
    
    * no ripple check
    
    * respond to comments
    
    * rename files to camelCase
    
    * account_channels
    
    * account_currencies
    
    * account_info
    
    * account_lines
    
    * account_objects
    
    * account_offers
    
    * account_tx
    
    * gateway_balances
    
    * no ripple check
    
    * respond to comments
    
    * export methods
    
    * fix typos
    
    * refactor: Define type for CommonFields
    
    * refactor: Define OfferCreate transaction type
    
    * add a commented out SignedTransaction
    
    * add tests for verifyCommonFields
    
    * remove outdated files
    
    * refactor: Define OfferCreate transaction
    
    * define checkCancel model
    
    * add tests
    
    * fix: add tests and resolve conflicts
    
    * fix: resolve typos and seperate out test
    
    * fix: commenting issue
    
    * refactor: add checkCreate to tx/index
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>
    Co-authored-by: Nathan Nichols <natenichols@cox.net>

--- src/models/transactions/index.ts
+++ src/models/transactions/index.ts
@@ -1,3 +1,4 @@
 export * from './transaction'
 export * from './offerCreate'
+export * from './checkCancel'
 export * from './checkCreate'
---
+++ test/models/checkCancel.ts
@@ -0,0 +1,35 @@
+import { ValidationError } from 'ripple-api/common/errors'
+import { verifyCheckCancel} from './../../src/models/transactions/checkCancel'
+import { assert } from 'chai'
+
+/**
+ * CheckCancel Transaction Verification Testing
+ *
+ * Providing runtime verification testing for each specific transaction type
+ */
+describe('Transaction Verification', function () {
+
+    it (`verifies valid CheckCancel`, () => {
+        const validCheckCancel = {
+            Account : "rUn84CUYbNjRoTQ6mSW7BVJPSVJNLb1QLo",
+            TransactionType : "CheckCancel",
+            CheckID : "49647F0D748DC3FE26BDACBC57F251AADEFFF391403EC9BF87C97F67E9977FB0"
+        } as any
+
+        assert.doesNotThrow(() => verifyCheckCancel(validCheckCancel))
+    })
+
+    it (`throws w/ invalid CheckCancel`, () => {
+        const invalidCheckID = {
+            Account : "rUn84CUYbNjRoTQ6mSW7BVJPSVJNLb1QLo",
+            TransactionType : "CheckCancel",
+            CheckID : 496473456789876545678909876545678
+        } as any
+
+        assert.throws(
+            () => verifyCheckCancel(invalidCheckID),
+            ValidationError,
+            "CheckCancel: invalid CheckID"
+        )
+    })
+})
