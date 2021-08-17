ed54f148f9830def796ad190729868788763d101
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-08-17 16:03:13 -0400

    Update test/models/offerCancel.ts
    refactor: reuse code using beforeEach()
    
    Co-authored-by: Omar Khan <khancodegt@gmail.com>

--- test/models/offerCancel.ts
+++ test/models/offerCancel.ts
@@ -8,9 +8,10 @@ import { assert } from 'chai'
  * Providing runtime verification testing for each specific transaction type
  */
 describe('OfferCancel Transaction Verification', function () {
-
-    it (`verifies valid OfferCancel`, () => {
-        const offer = {
+   let offer
+   
+   beforeEach(() => {
+       offer = {
             Account: "rnKiczmiQkZFiDES8THYyLA2pQohC5C6EF",
             Fee: "10",
             LastLedgerSequence: 65477334,
@@ -21,9 +22,10 @@ describe('OfferCancel Transaction Verification', function () {
             TxnSignature: "30450221008C43BDCFC68B4793857CA47DF454C07E5B45D3F80E8E6785CAB9292...",
             date: "2021-08-06T21:04:11Z"
         } as any
-        
-        assert.doesNotThrow(() => verifyOfferCancel(offer))
-        
+   })
+
+    it (`verifies valid OfferCancel`, () => {        
+        assert.doesNotThrow(() => verifyOfferCancel(offer))        
     })
 
     it (`verifies valid OfferCancel with flags`, () => {
