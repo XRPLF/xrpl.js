8ff20540e4b0d13195a3139ccfee464acbb6e2b3
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-08-17 13:31:19 -0400

    refactor: define SignerListSet model and tests

---
+++ src/models/transactions/signerListSet.ts
@@ -0,0 +1,38 @@
+import { ValidationError } from "../../common/errors"
+import { SignerEntry } from "../common"
+import { BaseTransaction, verifyBaseTransaction } from "./common"
+
+export interface SignerListSet extends BaseTransaction {
+    TransactionType: "SignerListSet"
+    SignerQuorum: number
+    SignerEntries: SignerEntry[]
+}
+
+/**
+ * Verify the form and type of an SignerListSet at runtime.
+ * 
+ * @param tx - An SignerListSet Transaction
+ * @returns - Void.
+ * @throws - When the SignerListSet is Malformed.
+ */
+ export function verifySignerListSet(tx: SignerListSet): void {
+    verifyBaseTransaction(tx)
+
+    if (tx.SignerQuorum === undefined)
+        throw new ValidationError("SignerListSet: missing field SignerQuorum")
+
+    if (typeof tx.SignerQuorum !== 'number')
+        throw new ValidationError("SignerListSet: invalid SignerQuorum")
+
+    if (tx.SignerEntries === undefined)
+        throw new ValidationError("SignerListSet: missing field SignerEntries")
+
+    if (!Array.isArray(tx.SignerEntries))
+        throw new ValidationError("SignerListSet: invalid SignerEntries")
+
+    if (tx.SignerEntries.length === 0)
+        throw new ValidationError("SignerListSet: need atleast 1 member in SignerEntries")
+    
+    if (tx.SignerEntries.length > 8)
+        throw new ValidationError("SignerListSet: maximum of 8 members allowed in SignerEntries")
+}
