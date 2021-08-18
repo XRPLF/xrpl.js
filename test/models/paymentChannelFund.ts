928600fc1f4abcccb483d6e3f876d47174414af0
Nathan Nichols <natenichols@cox.net>
2021-08-18 16:02:55 -0500

    update import'

--- test/models/paymentChannelFund.ts
+++ test/models/paymentChannelFund.ts
@@ -1,4 +1,4 @@
-import { ValidationError } from 'ripple-api/common/errors'
+import { ValidationError } from 'xrpl-local/common/errors'
 import { verifyPaymentChannelFund } from './../../src/models/transactions/paymentChannelFund'
 import { assert } from 'chai'
 
