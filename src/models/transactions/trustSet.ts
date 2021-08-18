7c659d66780543d6e6149bd21a3fbbddebd0b678
Omar Khan <khancodegt@gmail.com>
2021-08-18 15:13:25 -0400

    add TrustSetFlags type and enum

--- src/models/transactions/trustSet.ts
+++ src/models/transactions/trustSet.ts
@@ -1,9 +1,26 @@
 import { Amount } from '../common'
-import { BaseTransaction } from './common'
+import { BaseTransaction, GlobalFlags } from './common'
+
+export enum TrustSetFlagsEnum {
+    tfSetfAuth = 0x00010000,
+    tfSetNoRipple = 0x00020000,
+    tfClearNoRipple = 0x00040000,
+    tfSetFreeze = 0x00100000,
+    tfClearFreeze = 0x00200000,
+}
+
+export interface TrustSetFlags extends GlobalFlags {
+    tfSetfAuth?: boolean
+    tfSetNoRipple?: boolean
+    tfClearNoRipple?: boolean
+    tfSetFreeze?: boolean
+    tfClearFreeze?: boolean
+}
 
 export interface TrustSet extends BaseTransaction {
     TransactionType: 'TrustSet'
     LimitAmount: Amount
     QualityIn?: number
     QualityOut?: number
+    Flags?: number | TrustSetFlags
 }
