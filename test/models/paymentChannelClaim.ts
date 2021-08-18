495493415d4890d5b8e2308fb4c1e41f09f5287b
Nathan Nichols <natenichols@cox.net>
2021-08-18 16:04:59 -0500

    update imports

--- test/models/paymentChannelClaim.ts
+++ test/models/paymentChannelClaim.ts
@@ -1,4 +1,4 @@
-import { ValidationError } from 'ripple-api/common/errors'
+import { ValidationError } from 'xrpl-local/common/errors'
 import { verifyPaymentChannelClaim } from './../../src/models/transactions/paymentChannelClaim'
 import { assert } from 'chai'
 
