a820224d0849ccad1439ea69a31a99576cf82ad0
Nathan Nichols <natenichols@cox.net>
2021-08-19 11:21:06 -0500

    refactor: Define PaymentChannelFund transaction model (#1535)
    refactor: Define PaymentChannelFund transaction model (#1535)

---
+++ src/models/transactions/paymentChannelFund.ts
@@ -0,0 +1,35 @@
+import { ValidationError } from "../../common/errors";
+import { BaseTransaction, verifyBaseTransaction } from "./common";
+
+export interface PaymentChannelFund extends BaseTransaction {
+    TransactionType: "PaymentChannelFund";
+    Channel: string;
+    Amount: string;
+    Expiration?: number;
+}
+
+/**
+ * Verify the form and type of an PaymentChannelFund at runtime.
+ * 
+ * @param tx - An PaymentChannelFund Transaction
+ * @returns - Void.
+ * @throws - When the PaymentChannelFund is Malformed.
+ */
+ export function verifyPaymentChannelFund(tx: PaymentChannelFund): void {
+    verifyBaseTransaction(tx)
+
+    if (tx.Channel === undefined)
+        throw new ValidationError("PaymentChannelFund: missing Channel")
+
+    if (typeof tx.Channel !== 'string')
+        throw new ValidationError("PaymentChannelFund: Channel must be a string")
+
+    if (tx.Amount === undefined)
+        throw new ValidationError("PaymentChannelFund: missing Amount")
+
+    if (typeof tx.Amount !== 'string')
+        throw new ValidationError("PaymentChannelFund: Amount must be a string")
+
+    if (tx.Expiration !== undefined && typeof tx.Expiration !== 'number')
+        throw new ValidationError("PaymentChannelFund: Expiration must be a number")
+}
