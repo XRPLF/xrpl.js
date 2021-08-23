455fdc7dd2fc6fd46ca96ecd11e53773dd137de2
Jackson Mills <jmills@ripple.com>
2021-08-23 15:12:05 -0700

    Rename verify-payment-channel-claim to camelCase

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,7 @@
 import { deriveKeypair, deriveAddress, deriveXAddress } from './derive'
 import computeLedgerHeaderHash from './ledgerHash'
 import signPaymentChannelClaim from './signPaymentChannelClaim'
-import verifyPaymentChannelClaim from './verify-payment-channel-claim'
+import verifyPaymentChannelClaim from './verifyPaymentChannelClaim'
 import {
     computeBinaryTransactionHash,
     computeTransactionHash,
