1195e3b192fc13a0508babd02488664e7c7268c2
Jackson Mills <jmills@ripple.com>
2021-08-24 09:58:25 -0700

    Improve readability of signPaymentChannelClaim

--- test/utils/signPaymentChannelClaim/index.ts
+++ test/utils/signPaymentChannelClaim/index.ts
@@ -1,17 +1,22 @@
 import {assertResultMatch} from '../../utils'
-import {channel, amount} from '../../fixtures/requests/sign-payment-channel-claim.json'
 import responses from '../../fixtures/responses'
 import signPaymentChannelClaim from '../../../src/utils/signPaymentChannelClaim'
 
+
+
 describe('signPaymentChannelClaim', function () {
   it('basic signature matches', () => {
-    const privateKey =
-      'ACCD3309DB14D1A4FC9B1DAE608031F4408C85C73EE05E035B7DC8B25840107A'
+    
+    const channel = "3E18C05AD40319B809520F1A136370C4075321B285217323396D6FD9EE1E9037"
+    const amount = ".00001"
+    const privateKey = 'ACCD3309DB14D1A4FC9B1DAE608031F4408C85C73EE05E035B7DC8B25840107A'
+    
     const result = signPaymentChannelClaim(
       channel,
       amount,
       privateKey
     )
+    
     assertResultMatch(
       result,
       responses.signPaymentChannelClaim,
