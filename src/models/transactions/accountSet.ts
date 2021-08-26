44f2cdc3777363388c8744d9925ef7487626d9ee
Nathan Nichols <natenichols@cox.net>
2021-08-26 11:17:36 -0500

    refactor: adjust vocabulary for transaciton flags

--- src/models/transactions/accountSet.ts
+++ src/models/transactions/accountSet.ts
@@ -1,69 +1,89 @@
-import { ValidationError } from "../../common/errors"
-import { BaseTransaction, verifyBaseTransaction } from "./common"
+import {ValidationError} from '../../common/errors'
+import {
+  BaseTransaction,
+  GlobalFlagsInterface,
+  verifyBaseTransaction
+} from './common'
 
-enum AccountSetFlagEnum {
-    asfRequireDest = 1,
-    asfRequireAuth = 2,
-    asfDisallowXRP = 3,
-    asfDisableMaster = 4,
-    asfAccountTxnID = 5,
-    asfNoFreeze = 6,
-    asfGlobalFreeze = 7,
-    asfDefaultRipple = 8,
-    asfDepositAuth = 9,
+export enum AccountSetFlags {
+  asfRequireDest = 1,
+  asfRequireAuth = 2,
+  asfDisallowXRP = 3,
+  asfDisableMaster = 4,
+  asfAccountTxnID = 5,
+  asfNoFreeze = 6,
+  asfGlobalFreeze = 7,
+  asfDefaultRipple = 8,
+  asfDepositAuth = 9,
+  tfRequireDestTag = 0x00010000,
+  tfOptionalDestTag = 0x00020000,
+  tfRequireAuth = 0x00040000,
+  tfOptionalAuth = 0x00080000,
+  tfDisallowXRP = 0x00100000,
+  tfAllowXRP = 0x00200000
+}
+
+export interface AccountSetFlagsInterface extends GlobalFlagsInterface {
+  tfRequireDestTag?: boolean
+  tfOptionalDestTag?: boolean
+  tfRequireAuth?: boolean
+  tfOptionalAuth?: boolean
+  tfDisallowXRP?: boolean
+  tfAllowXRP?: boolean
 }
 
 export interface AccountSet extends BaseTransaction {
-    TransactionType: "AccountSet"
-    ClearFlag?: number
-    Domain?: string
-    EmailHash?: string
-    MessageKey?: string
-    SetFlag?: AccountSetFlagEnum
-    TransferRate?: number
-    TickSize?: number
+  TransactionType: 'AccountSet'
+  Flags?: number | AccountSetFlagsInterface
+  ClearFlag?: number
+  Domain?: string
+  EmailHash?: string
+  MessageKey?: string
+  SetFlag?: AccountSetFlags
+  TransferRate?: number
+  TickSize?: number
 }
 
 /**
  * Verify the form and type of an AccountSet at runtime.
- * 
+ *
  * @param tx - An AccountSet Transaction
  * @returns - Void.
  * @throws - When the AccountSet is Malformed.
  */
- export function verifyAccountSet(tx: AccountSet): void {
-    verifyBaseTransaction(tx)
+export function verifyAccountSet(tx: AccountSet): void {
+  verifyBaseTransaction(tx)
 
-    if (tx.ClearFlag !== undefined){
-        if (typeof tx.ClearFlag !== 'number')
-            throw new ValidationError("AccountSet: invalid ClearFlag")
-        if (!Object.values(AccountSetFlagEnum).includes(tx.ClearFlag))
-            throw new ValidationError("AccountSet: invalid ClearFlag")
-    }
+  if (tx.ClearFlag !== undefined) {
+    if (typeof tx.ClearFlag !== 'number')
+      throw new ValidationError('AccountSet: invalid ClearFlag')
+    if (!Object.values(AccountSetFlags).includes(tx.ClearFlag))
+      throw new ValidationError('AccountSet: invalid ClearFlag')
+  }
 
-    if (tx.Domain !== undefined && typeof tx.Domain !== 'string')
-        throw new ValidationError("AccountSet: invalid Domain")
+  if (tx.Domain !== undefined && typeof tx.Domain !== 'string')
+    throw new ValidationError('AccountSet: invalid Domain')
 
-    if (tx.EmailHash !== undefined && typeof tx.EmailHash !== 'string')
-        throw new ValidationError("AccountSet: invalid EmailHash")
-        
-    if (tx.MessageKey !== undefined && typeof tx.MessageKey !== 'string')
-        throw new ValidationError("AccountSet: invalid MessageKey")
-    
-    if (tx.SetFlag !== undefined){
-        if (typeof tx.SetFlag !== 'number')
-            throw new ValidationError("AccountSet: invalid SetFlag")
-        if (!Object.values(AccountSetFlagEnum).includes(tx.SetFlag))
-            throw new ValidationError("AccountSet: invalid SetFlag")
-    }
+  if (tx.EmailHash !== undefined && typeof tx.EmailHash !== 'string')
+    throw new ValidationError('AccountSet: invalid EmailHash')
 
-    if (tx.TransferRate !== undefined && typeof tx.TransferRate !== 'number')
-        throw new ValidationError("AccountSet: invalid TransferRate")
-    
-    if (tx.TickSize !== undefined){
-        if (typeof tx.TickSize !== 'number')
-            throw new ValidationError("AccountSet: invalid TickSize")
-        if (tx.TickSize !== 0 && (3 > tx.TickSize || tx.TickSize > 15))
-            throw new ValidationError("AccountSet: invalid TickSize")
-    }
+  if (tx.MessageKey !== undefined && typeof tx.MessageKey !== 'string')
+    throw new ValidationError('AccountSet: invalid MessageKey')
+
+  if (tx.SetFlag !== undefined) {
+    if (typeof tx.SetFlag !== 'number')
+      throw new ValidationError('AccountSet: invalid SetFlag')
+    if (!Object.values(AccountSetFlags).includes(tx.SetFlag))
+      throw new ValidationError('AccountSet: invalid SetFlag')
+  }
+
+  if (tx.TransferRate !== undefined && typeof tx.TransferRate !== 'number')
+    throw new ValidationError('AccountSet: invalid TransferRate')
+
+  if (tx.TickSize !== undefined) {
+    if (typeof tx.TickSize !== 'number')
+      throw new ValidationError('AccountSet: invalid TickSize')
+    if (tx.TickSize !== 0 && (3 > tx.TickSize || tx.TickSize > 15))
+      throw new ValidationError('AccountSet: invalid TickSize')
+  }
 }
--- src/models/transactions/transaction.ts
+++ src/models/transactions/transaction.ts
@@ -1,46 +1,46 @@
-import Metadata from "../common/metadata";
-import { AccountDelete } from "./accountDelete";
-import { AccountSet } from "./accountSet";
-import { CheckCancel } from "./checkCancel";
-import { CheckCash } from "./checkCash";
-import { CheckCreate } from "./checkCreate";
-import { DepositPreauth } from "./depositPreauth"
-import { EscrowCreate } from "./escrowCreate"
-import { EscrowCancel } from './escrowCancel'
-import { EscrowFinish } from "./escrowFinish"
-import { OfferCancel } from "./offerCancel"
-import { OfferCreate } from "./offerCreate"
-import { PaymentTransaction } from "./paymentTransaction"
-import { PaymentChannelClaim } from './paymentChannelClaim'
-import { PaymentChannelCreate } from "./paymentChannelCreate"
-import { PaymentChannelFund } from "./paymentChannelFund"
-import { SetRegularKey } from "./setRegularKey";
-import { SignerListSet } from "./signerListSet"
-import { TicketCreate } from "./ticketCreate"
-import { TrustSet } from "./trustSet"
+import Metadata from '../common/metadata'
+import {AccountDelete} from './accountDelete'
+import {AccountSet} from './accountSet'
+import {CheckCancel} from './checkCancel'
+import {CheckCash} from './checkCash'
+import {CheckCreate} from './checkCreate'
+import {DepositPreauth} from './depositPreauth'
+import {EscrowCreate} from './escrowCreate'
+import {EscrowCancel} from './escrowCancel'
+import {EscrowFinish} from './escrowFinish'
+import {OfferCancel} from './offerCancel'
+import {OfferCreate} from './offerCreate'
+import {PaymentTransaction} from './payment'
+import {PaymentChannelClaim} from './paymentChannelClaim'
+import {PaymentChannelCreate} from './paymentChannelCreate'
+import {PaymentChannelFund} from './paymentChannelFund'
+import {SetRegularKey} from './setRegularKey'
+import {SignerListSet} from './signerListSet'
+import {TicketCreate} from './ticketCreate'
+import {TrustSet} from './trustSet'
 
 export type Transaction =
-       AccountDelete
-     | AccountSet
-     | CheckCancel
-     | CheckCash
-     | CheckCreate
-     | DepositPreauth
-     | EscrowCancel
-     | EscrowCreate
-     | EscrowFinish
-     | OfferCancel
-     | OfferCreate
-     | PaymentTransaction
-     | PaymentChannelClaim
-     | PaymentChannelCreate
-     | PaymentChannelFund
-     | SetRegularKey
-     | SignerListSet
-     | TicketCreate
-     | TrustSet
+  | AccountDelete
+  | AccountSet
+  | CheckCancel
+  | CheckCash
+  | CheckCreate
+  | DepositPreauth
+  | EscrowCancel
+  | EscrowCreate
+  | EscrowFinish
+  | OfferCancel
+  | OfferCreate
+  | PaymentTransaction
+  | PaymentChannelClaim
+  | PaymentChannelCreate
+  | PaymentChannelFund
+  | SetRegularKey
+  | SignerListSet
+  | TicketCreate
+  | TrustSet
 
 export interface TransactionAndMetadata {
-    transaction: Transaction
-    metadata: Metadata
+  transaction: Transaction
+  metadata: Metadata
 }
