e58396eedd1b65aeb088f3d8638ece2a22fbd86a
Jackson Mills <jmills@ripple.com>
2021-08-27 16:11:41 -0700

    Match rippled signer format and fix typing assumptions

--- src/ledger/signer.ts
+++ src/ledger/signer.ts
@@ -8,7 +8,6 @@ import { computeBinaryTransactionHash } from "../utils";
 import { flatMap } from "lodash";
 import { SignedTransaction } from "../common/types/objects";
 import { verifyBaseTransaction } from "../models/transactions/common";
-//import { encodeForSigningClaim } from 'ripple-binary-codec'
 import { sign as signWithKeypair, verify as verifySignature } from 'ripple-keypairs'
 
 function sign(wallet: Wallet, tx: Transaction): SignedTransaction {
@@ -72,13 +71,30 @@ function combine(signedTransactions: string[]): SignedTransaction {
   }
 }
 
+function getDecodedTransaction(txOrBlob: (Transaction | string)): Transaction {
+  if(typeof (txOrBlob) === "object") {
+      return txOrBlob as Transaction
+  } else {
+      return decode(txOrBlob) as unknown as Transaction
+  }
+}
+
+function getEncodedTransaction(txOrBlob: (Transaction | string)): string {
+  if(typeof txOrBlob === "object") {
+      return encode(JSON.parse(JSON.stringify(txOrBlob)))
+  } else {
+      return txOrBlob as string
+  }
+}
+
 function multisign(transactions: Transaction[] | string[]): SignedTransaction {
     
     if(transactions.length == 0) {
         throw new ValidationError("There were 0 transactions given to multisign")
     }
 
-    transactions.forEach(tx => {
+    transactions.forEach(txOrBlob => {
+        const tx = getDecodedTransaction(txOrBlob)
         verifyBaseTransaction(tx)
 
         if(tx.SigningPubKey !== "") {
@@ -90,75 +106,21 @@ function multisign(transactions: Transaction[] | string[]): SignedTransaction {
         }
     })
 
-    let encodedTransactions: string[];
-    if(typeof (transactions[0]) === "object") {
-        encodedTransactions = (transactions as Transaction[]).map( tx => {
-            return encode(JSON.parse(JSON.stringify(tx)))
-        })
-    } else {
-        encodedTransactions = transactions as string[]
-    }
+    const encodedTransactions: string[] = transactions.map(txOrBlob => getEncodedTransaction(txOrBlob))
     
     return combine(encodedTransactions)
 }
 
 function authorizeChannel(wallet: Wallet, channelId: string, amount: string): string {
-
-    //const amount64 = Number(amount).toString(16).padStart(16, '0')
-    //const paymentChannelClaimPrefix = '434c4d00'
-    //const encodedAuthorization = `${paymentChannelClaimPrefix}${channelId}${amount64}`
-    //wallet.signTransaction
-    //encodeAccountPublic(wallet.publicKey)
-    //wallet.signTransaction
     const signingData = encodeForSigningClaim({
       channel: channelId,
       amount: amount
     })
     console.log("Signing Data:")
     console.log(signingData)
-/*
-Amount xrpToDrops: 3045022100BA56768853586214099F6708D9A49FA4E4D21EAEBDEA309FDFA1FF5C28DBB3FE02203CA8984A2B562535E4F6263BF8604E5C8B0776D15E0AED51046A6F6ED8B75AE1
-Amount no xrp2Drp: 3045022100BA56768853586214099F6708D9A49FA4E4D21EAEBDEA309FDFA1FF5C28DBB3FE02203CA8984A2B562535E4F6263BF8604E5C8B0776D15E0AED51046A6F6ED8B75AE1    
-(Same!)
-
-const num = bigInt(String(claim.amount));
-    const prefix = HashPrefix.paymentChannelClaim;
-    const channel = coreTypes.Hash256.from(claim.channel).toBytes();
-    const amount = coreTypes.UInt64.from(num).toBytes();
-  
-    const bytesList = new BytesList();
-  
-    bytesList.put(prefix);
-    bytesList.put(channel);
-    bytesList.put(amount);*/
-    
     return signWithKeypair(signingData, wallet.privateKey)
-  
-
-    //return signPaymentChannelClaim(channelId, amount, wallet.privateKey)
 }
-/*
-    //channelId concat with the xrp amount in drops
 
-    let keyType;
-    if(wallet.privateKey.startsWith(0xED.toString())){
-        keyType = "ed25519"
-    } else {
-        keyType = "secp256k1"
-    }
-
-    const amount64 = Number(amount).toString(16).padStart(16, '0')
-    const paymentChannelClaimPrefix = '434c4d00'
-    const encodedAuthorization = `${paymentChannelClaimPrefix}${channelId}${amount64}`
-    //wallet.signTransaction
-    //encodeAccountPublic(wallet.publicKey)
-    wallet.signTransaction
-    
-
-    return null */
-//}
-
-// TODO: Implement verify
 function verify(tx: Transaction): void {
   //Verify tx.TxnSignature by using tx.SigningPubKey
   //TODO: Check whether we need to remove the TxnSignature and SigningPubKey fields before encoding
--- test/signerTest.ts
+++ test/signerTest.ts
@@ -1,6 +1,6 @@
 import { assert } from 'chai'
 //import { deriveKeypair } from 'ripple-keypairs/dist'
-import { sign, authorizeChannel } from 'xrpl-local/ledger/signer'
+import { sign, authorizeChannel, multisign } from 'xrpl-local/ledger/signer'
 import { SignedTransaction } from '../src/common/types/objects'
 import { Transaction } from '../src/models/transactions'
 import Wallet from '../src/Wallet'
@@ -41,7 +41,58 @@ describe('Signer tests', function () {
 
     it ('multi-signs successfully', () => {
         // TODO:
-        notImplemented()
+        const tx1: Transaction = {
+            Account: "rEuLyBCvcw4CFmzv8RepSiAoNgF8tTGJQC",
+            Fee: "30000",
+            Flags: 262144,
+            LimitAmount: {
+              currency: "USD",
+              issuer: "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh",
+              value: "100"
+            },
+            Sequence: 2,
+            Signers: [
+              {
+                Signer: {
+                  Account: "rsA2LpzuawewSBQXkiju3YQTMzW13pAAdW",
+                  SigningPubKey: "02B3EC4E5DD96029A647CFA20DA07FE1F85296505552CCAC114087E66B46BD77DF",
+                  TxnSignature: "30450221009C195DBBF7967E223D8626CA19CF02073667F2B22E206727BFE848FF42BEAC8A022048C323B0BED19A988BDBEFA974B6DE8AA9DCAE250AA82BBD1221787032A864E5"
+                }
+              }
+            ],
+            SigningPubKey: "",
+            TransactionType: "TrustSet",
+            //hash: "BD636194C48FD7A100DE4C972336534C8E710FD008C0F3CF7BC5BF34DAF3C3E6"
+          }
+
+        const tx2: Transaction = {
+            Account: "rEuLyBCvcw4CFmzv8RepSiAoNgF8tTGJQC",
+            Fee: "30000",
+            Flags: 262144,
+            LimitAmount: {
+              currency: "USD",
+              issuer: "rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh",
+              value: "100"
+            },
+            Sequence: 2,
+            Signers: [
+                {
+                    Signer: {
+                    Account: "rUpy3eEg8rqjqfUoLeBnZkscbKbFsKXC3v",
+                    TxnSignature: "30440220680BBD745004E9CFB6B13A137F505FB92298AD309071D16C7B982825188FD1AE022004200B1F7E4A6A84BB0E4FC09E1E3BA2B66EBD32F0E6D121A34BA3B04AD99BC1",
+                    SigningPubKey: "028FFB276505F9AC3F57E8D5242B386A597EF6C40A7999F37F1948636FD484E25B"
+                    }
+                }              
+            ],
+            SigningPubKey: "",
+            TransactionType: "TrustSet",
+            //hash: "BD636194C48FD7A100DE4C972336534C8E710FD008C0F3CF7BC5BF34DAF3C3E6"
+          }
+
+        const transactions: Transaction[] = new Array(tx1, tx2)
+
+        multisign(transactions)
+
     })
 
     it ('throws a validation error if multi-signing with no transactions', () => {
@@ -50,28 +101,29 @@ describe('Signer tests', function () {
     })
 
     it ('authorizeChannel succeeds', () => {
-        //const keypair = deriveKeypair('snoPBrXtMeMyMHUVTgbuqAfg1SUTb')
-        const keypair = {
-            publicKey: "02DEBD2E84F1FB9FB8A0E565F23B3109B43B5EB83DD615E1C59506C16D2EE5CDEF",
-            privateKey: "081C78F2789D35A2D501226BD099A71ED7719FDFC9ACA0257EC4246C022F960F"
-        }
-        console.log(keypair)
-        //const publicKey = '02DEBD2E84F1FB9FB8A0E565F23B3109B43B5EB83DD615E1C59506C16D2EE5CDEF'
-        //const secretKey = '081C78F2789D35A2D501226BD099A71ED7719FDFC9ACA0257EC4246C022F960F'
-        const wallet = new Wallet(keypair.publicKey, keypair.privateKey)
+        const wallet = Wallet.fromSeed("snGHNrPbHrdUcszeuDEigMdC1Lyyd")
         const channelId = '5DB01B7FFED6B67E6B0414DED11E051D2EE2B7619CE0EAA6286D67A3A4D5BDB3'
         const amount = '1000000'
 
         assert.equal(authorizeChannel(wallet, channelId, amount), 
-        '3045022100DB6CF7ADF76F55E4A564C986E58541B5805CA4FD44C338CB8A3CAF2F4FCBB1D50220795C8D3ED550ABA33F9D12BE4FD18899338331C590DB07B1DCB5E0493CC9C5C9')
+        '304402204E7052F33DDAFAAA55C9F5B132A5E50EE95B2CF68C0902F61DFE77299BC893740220353640B951DCD24371C16868B3F91B78D38B6F3FD1E826413CDF891FA8250AAC')
     })
 
-    it ('validate transaction succeeds for valid transaction', () => {
+    /*it('authorizeChannel succeeds with ed25519 seed'), () => {
+        const wallet = Wallet.fromSeed("sEdnGHNrPbHrdUcszeuDEigMdC1Lyyd")
+        const channelId = '5DB01B7FFED6B67E6B0414DED11E051D2EE2B7619CE0EAA6286D67A3A4D5BDB3'
+        const amount = '1000000'
+
+        assert.equal(authorizeChannel(wallet, channelId, amount), 
+        '304402207936D71315E0F2AC349A33F988111A6C6D92CD96347AF42BDB5C34EE61D4EC1F0220684CB495EA93DBF0B5A7EC003A1501BC87B2635AA592E5362D240A43DC1BC801')
+    }*/
+
+    it ('verify succeeds for valid transaction', () => {
         // TODO:
         notImplemented()
     })
 
-    it ('validate transaction throws for invalid transaction', () => {
+    it ('verify throws for invalid transaction', () => {
         // TODO:
         notImplemented() 
     })
