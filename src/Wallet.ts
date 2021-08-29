c351efcb0d371f3d0b8323dda3c85fd8cc2027b8
Omar Khan <khancodegt@gmail.com>
2021-08-29 12:44:33 -0400

    add getXAddress to Wallet (#1558)
    - add getXAddress to Wallet
    - refactor wallet tests

--- src/Wallet.ts
+++ src/Wallet.ts
@@ -1,13 +1,14 @@
-import { fromSeed } from "bip32";
-import { mnemonicToSeedSync } from "bip39";
-import { decode, encodeForSigning } from "ripple-binary-codec";
-import { deriveKeypair, generateSeed, verify } from "ripple-keypairs";
+import { fromSeed } from 'bip32';
+import { mnemonicToSeedSync } from 'bip39';
+import { classicAddressToXAddress } from 'ripple-address-codec'
+import { decode, encodeForSigning } from 'ripple-binary-codec';
+import { deriveAddress, deriveKeypair, generateSeed, verify } from 'ripple-keypairs';
 
-import ECDSA from "./common/ecdsa";
-import { ValidationError } from "./common/errors";
-import { SignedTransaction } from "./common/types/objects";
-import { signOffline } from "./transaction/sign";
-import { SignOptions } from "./transaction/types";
+import ECDSA from './common/ecdsa';
+import { ValidationError } from './common/errors';
+import { SignedTransaction } from './common/types/objects';
+import { signOffline } from './transaction/sign';
+import { SignOptions } from './transaction/types';
 
 /**
  * A utility for deriving a wallet composed of a keypair (publicKey/privateKey).
@@ -120,6 +121,20 @@ class Wallet {
     const messageHex: string = encodeForSigning(tx);
     const signature = tx.TxnSignature;
     return verify(messageHex, signature, this.publicKey);
+  }
+
+  /**
+   * Gets an X-address in Testnet/Mainnet format.
+   * @param {number} tag A tag to be included within the X-address.
+   * @param {boolean} test A boolean to indicate if X-address should be in Testnet (true) or Mainnet (false) format.
+   * @returns {string} An X-address.
+   */
+  getXAddress(tag: number, test: boolean = false): string {
+    return classicAddressToXAddress(
+      deriveAddress(this.publicKey),
+      tag,
+      test,
+    )
   }
 }
 
