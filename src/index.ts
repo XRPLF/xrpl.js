2d072a51ba2e432a79c9231a1669c312568fd2b9
Omar Khan <khancodegt@gmail.com>
2021-09-01 17:41:43 -0400

    Refactor generateFaucetWallet to return a Wallet (#1564)
    * add Wallet.generate() and 
    
    * return Wallet in generateFaucetWallet
    
    * refactor Wallet tests
    
    * rename wallet-generation.ts to generateFaucetWallet.ts
    
    * rename and move Wallet.ts to src/wallet/index.ts and update webpack config

--- src/index.ts
+++ src/index.ts
@@ -11,4 +11,4 @@ export * from "./utils";
 // Broadcast client is experimental
 export { BroadcastClient } from "./client/broadcastClient";
 
-export * from "./Wallet";
+export { default as Wallet } from "./wallet";
