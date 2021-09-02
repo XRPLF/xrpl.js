523daddf59a2b63a515f1b5d1c9e4ec2b551a711
Mayukha Vadari <mvadari@ripple.com>
2021-09-02 12:26:06 -0500

    lint setupClient

--- test/setupClient.ts
+++ test/setupClient.ts
@@ -1,9 +1,14 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types -- Necessary for testcase setup */
+/* eslint-disable no-param-reassign -- Necessary for testcase setup */
 import { Client, BroadcastClient } from 'xrpl-local'
 
-import { createMockRippled } from './mockRippled'
+import createMockRippled from './mockRippled'
 import { getFreePort } from './testUtils'
 
-async function setupMockRippledConnection(testcase, port) {
+async function setupMockRippledConnection(
+  testcase,
+  port: number,
+): Promise<void> {
   return new Promise<void>((resolve, reject) => {
     testcase.mockRippled = createMockRippled(port)
     testcase._mockedServerPort = port
@@ -12,44 +17,53 @@ async function setupMockRippledConnection(testcase, port) {
   })
 }
 
-async function setupMockRippledConnectionForBroadcast(testcase, ports) {
+async function setupMockRippledConnectionForBroadcast(
+  testcase,
+  ports: number[],
+): Promise<void> {
   return new Promise<void>((resolve, reject) => {
     const servers = ports.map((port) => `ws://localhost:${port}`)
-    testcase.mocks = ports.map((port) => createMockRippled(port))
+    // eslint-disable-next-line @typescript-eslint/promise-function-async -- Not an async function
+    testcase.mocks = ports.map(function (port) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return -- Correct type here
+      return createMockRippled(port)
+    })
     testcase.client = new BroadcastClient(servers)
     testcase.client.connect().then(resolve).catch(reject)
   })
 }
 
-async function setup(this: any) {
+async function setupClient(this: unknown): Promise<void> {
   return getFreePort().then(async (port) => {
     return setupMockRippledConnection(this, port)
   })
 }
 
-async function setupBroadcast(this: any) {
+async function setupBroadcast(this: unknown): Promise<void> {
   return Promise.all([getFreePort(), getFreePort()]).then(async (ports) => {
     return setupMockRippledConnectionForBroadcast(this, ports)
   })
 }
 
-function teardown(this: any, done) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any -- Needed for teardown
+async function teardownClient(this: any, done: () => void): Promise<void> {
   this.client
     .disconnect()
     .then(() => {
+      // eslint-disable-next-line no-negated-condition -- easier to understand with negation
       if (this.mockRippled != null) {
         this.mockRippled.close()
       } else {
-        this.mocks.forEach((mock) => mock.close())
+        this.mocks.forEach((mock: { close: () => void }) => mock.close())
       }
       setImmediate(done)
     })
     .catch(done)
 }
 
-export default {
-  setup,
-  teardown,
+export {
+  setupClient as setup,
+  teardownClient as teardown,
   setupBroadcast,
   createMockRippled,
 }
