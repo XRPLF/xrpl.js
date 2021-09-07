1308adb9fdea9de97ab9d66882bd31425c2394b4
Jackson Mills <jmills@ripple.com>
2021-09-07 16:46:26 -0700

    Revert prettier for js files

--- src/transaction/sign.ts
+++ src/transaction/sign.ts
@@ -1,20 +1,20 @@
-import BigNumber from "bignumber.js";
-import _ from "lodash";
-import binaryCodec from "ripple-binary-codec";
-import keypairs from "ripple-keypairs";
+import BigNumber from 'bignumber.js'
+import _ from 'lodash'
+import binaryCodec from 'ripple-binary-codec'
+import keypairs from 'ripple-keypairs'
 
-import type { Client, Wallet } from "..";
-import { ValidationError } from "../common/errors";
-import { xrpToDrops } from "../utils";
-import { computeBinaryTransactionHash } from "../utils/hashes";
+import type { Client, Wallet } from '..'
+import { ValidationError } from '../common/errors'
+import { xrpToDrops } from '../utils'
+import { computeBinaryTransactionHash } from '../utils/hashes'
 
-import { SignOptions, KeyPair, TransactionJSON } from "./types";
+import { SignOptions, KeyPair, TransactionJSON } from './types'
 
 function computeSignature(tx: object, privateKey: string, signAs?: string) {
   const signingData = signAs
     ? binaryCodec.encodeForMultisigning(tx, signAs)
-    : binaryCodec.encodeForSigning(tx);
-  return keypairs.sign(signingData, privateKey);
+    : binaryCodec.encodeForSigning(tx)
+  return keypairs.sign(signingData, privateKey)
 }
 
 function signWithKeypair(
@@ -22,23 +22,23 @@ function signWithKeypair(
   txJSON: string,
   keypair: KeyPair,
   options: SignOptions = {
-    signAs: "",
-  }
+    signAs: '',
+  },
 ): { signedTransaction: string; id: string } {
-  const tx = JSON.parse(txJSON);
+  const tx = JSON.parse(txJSON)
   if (tx.TxnSignature || tx.Signers) {
     throw new ValidationError(
-      'txJSON must not contain "TxnSignature" or "Signers" properties'
-    );
+      'txJSON must not contain "TxnSignature" or "Signers" properties',
+    )
   }
 
   if (client != null) {
-    checkFee(client, tx.Fee);
+    checkFee(client, tx.Fee)
   }
 
-  const txToSignAndEncode = { ...tx };
+  const txToSignAndEncode = { ...tx }
 
-  txToSignAndEncode.SigningPubKey = options.signAs ? "" : keypair.publicKey;
+  txToSignAndEncode.SigningPubKey = options.signAs ? '' : keypair.publicKey
 
   if (options.signAs) {
     const signer = {
@@ -47,22 +47,22 @@ function signWithKeypair(
       TxnSignature: computeSignature(
         txToSignAndEncode,
         keypair.privateKey,
-        options.signAs
+        options.signAs,
       ),
-    };
-    txToSignAndEncode.Signers = [{ Signer: signer }];
+    }
+    txToSignAndEncode.Signers = [{ Signer: signer }]
   } else {
     txToSignAndEncode.TxnSignature = computeSignature(
       txToSignAndEncode,
-      keypair.privateKey
-    );
+      keypair.privateKey,
+    )
   }
-  const serialized = binaryCodec.encode(txToSignAndEncode);
-  checkTxSerialization(serialized, tx);
+  const serialized = binaryCodec.encode(txToSignAndEncode)
+  checkTxSerialization(serialized, tx)
   return {
     signedTransaction: serialized,
     id: computeBinaryTransactionHash(serialized),
-  };
+  }
 }
 
 /**
@@ -74,48 +74,48 @@ function signWithKeypair(
  * @returns An object containing the differences between the two objects.
  */
 function objectDiff(a: object, b: object): object {
-  const diffs = {};
+  const diffs = {}
 
   // Compare two items and push non-matches to object
   const compare = function (i1: any, i2: any, k: string): void {
-    const type1 = Object.prototype.toString.call(i1);
-    const type2 = Object.prototype.toString.call(i2);
-    if (type2 === "[object Undefined]") {
-      diffs[k] = null; // Indicate that the item has been removed
-      return;
+    const type1 = Object.prototype.toString.call(i1)
+    const type2 = Object.prototype.toString.call(i2)
+    if (type2 === '[object Undefined]') {
+      diffs[k] = null // Indicate that the item has been removed
+      return
     }
     if (type1 !== type2) {
-      diffs[k] = i2; // Indicate that the item has changed types
-      return;
+      diffs[k] = i2 // Indicate that the item has changed types
+      return
     }
-    if (type1 === "[object Object]") {
-      const objDiff = objectDiff(i1, i2);
+    if (type1 === '[object Object]') {
+      const objDiff = objectDiff(i1, i2)
       if (Object.keys(objDiff).length > 0) {
-        diffs[k] = objDiff;
+        diffs[k] = objDiff
       }
-      return;
+      return
     }
-    if (type1 === "[object Array]") {
+    if (type1 === '[object Array]') {
       if (!_.isEqual(i1, i2)) {
-        diffs[k] = i2; // If arrays do not match, add second item to diffs
+        diffs[k] = i2 // If arrays do not match, add second item to diffs
       }
-      return;
+      return
     }
-    if (type1 === "[object Function]") {
+    if (type1 === '[object Function]') {
       if (i1.toString() !== i2.toString()) {
-        diffs[k] = i2; // If functions differ, add second one to diffs
+        diffs[k] = i2 // If functions differ, add second one to diffs
       }
-      return;
+      return
     }
     if (i1 !== i2) {
-      diffs[k] = i2;
+      diffs[k] = i2
     }
-  };
+  }
 
   // Check items in first object
   for (const key in a) {
     if (a.hasOwnProperty(key)) {
-      compare(a[key], b[key], key);
+      compare(a[key], b[key], key)
     }
   }
 
@@ -123,12 +123,12 @@ function objectDiff(a: object, b: object): object {
   for (const key in b) {
     if (b.hasOwnProperty(key)) {
       if (!a[key] && a[key] !== b[key]) {
-        diffs[key] = b[key];
+        diffs[key] = b[key]
       }
     }
   }
 
-  return diffs;
+  return diffs
 }
 
 /**
@@ -142,55 +142,55 @@ function objectDiff(a: object, b: object): object {
  */
 function checkTxSerialization(serialized: string, tx: TransactionJSON): void {
   // Decode the serialized transaction:
-  const decoded = binaryCodec.decode(serialized);
+  const decoded = binaryCodec.decode(serialized)
 
   // ...And ensure it is equal to the original tx, except:
   // - It must have a TxnSignature or Signers (multisign).
   if (!decoded.TxnSignature && !decoded.Signers) {
     throw new ValidationError(
-      "Serialized transaction must have a TxnSignature or Signers property"
-    );
+      'Serialized transaction must have a TxnSignature or Signers property',
+    )
   }
   // - We know that the original tx did not have TxnSignature, so we should delete it:
-  delete decoded.TxnSignature;
+  delete decoded.TxnSignature
   // - We know that the original tx did not have Signers, so if it exists, we should delete it:
-  delete decoded.Signers;
+  delete decoded.Signers
 
   // - If SigningPubKey was not in the original tx, then we should delete it.
   //   But if it was in the original tx, then we should ensure that it has not been changed.
   if (!tx.SigningPubKey) {
-    delete decoded.SigningPubKey;
+    delete decoded.SigningPubKey
   }
 
   // - Memos have exclusively hex data which should ignore case.
   //   Since decode goes to upper case, we set all tx memos to be uppercase for the comparison.
   tx.Memos?.map((memo) => {
     if (memo.Memo.MemoData) {
-      memo.Memo.MemoData = memo.Memo.MemoData.toUpperCase();
+      memo.Memo.MemoData = memo.Memo.MemoData.toUpperCase()
     }
 
     if (memo.Memo.MemoType) {
-      memo.Memo.MemoType = memo.Memo.MemoType.toUpperCase();
+      memo.Memo.MemoType = memo.Memo.MemoType.toUpperCase()
     }
 
     if (memo.Memo.MemoFormat) {
-      memo.Memo.MemoFormat = memo.Memo.MemoFormat.toUpperCase();
+      memo.Memo.MemoFormat = memo.Memo.MemoFormat.toUpperCase()
     }
 
-    return memo;
-  });
+    return memo
+  })
 
   if (!_.isEqual(decoded, tx)) {
     const data = {
       decoded,
       tx,
       diff: objectDiff(tx, decoded),
-    };
+    }
     const error = new ValidationError(
-      "Serialized transaction does not match original txJSON. See `error.data`",
-      data
-    );
-    throw error;
+      'Serialized transaction does not match original txJSON. See `error.data`',
+      data,
+    )
+    throw error
   }
 }
 
@@ -205,13 +205,13 @@ function checkTxSerialization(serialized: string, tx: TransactionJSON): void {
  * @returns This method does not return a value, but throws an error if the check fails.
  */
 function checkFee(client: Client, txFee: string): void {
-  const fee = new BigNumber(txFee);
-  const maxFeeDrops = xrpToDrops(client.maxFeeXRP);
+  const fee = new BigNumber(txFee)
+  const maxFeeDrops = xrpToDrops(client.maxFeeXRP)
   if (fee.isGreaterThan(maxFeeDrops)) {
     throw new ValidationError(
       `"Fee" should not exceed "${maxFeeDrops}". ` +
-        "To use a higher fee, set `maxFeeXRP` in the Client constructor."
-    );
+        'To use a higher fee, set `maxFeeXRP` in the Client constructor.',
+    )
   }
 }
 
@@ -220,33 +220,33 @@ function sign(
   txJSON: string,
   secret?: any,
   options?: SignOptions,
-  keypair?: KeyPair
+  keypair?: KeyPair,
 ): { signedTransaction: string; id: string } {
-  if (typeof secret === "string") {
+  if (typeof secret === 'string') {
     // we can't validate that the secret matches the account because
     // the secret could correspond to the regular key
     return signWithKeypair(
       this,
       txJSON,
       keypairs.deriveKeypair(secret),
-      options
-    );
+      options,
+    )
   }
   if (!keypair && !secret) {
     // Clearer message than 'ValidationError: instance is not exactly one from [subschema 0],[subschema 1]'
-    throw new ValidationError("sign: Missing secret or keypair.");
+    throw new ValidationError('sign: Missing secret or keypair.')
   }
-  return signWithKeypair(this, txJSON, keypair || secret, options);
+  return signWithKeypair(this, txJSON, keypair || secret, options)
 }
 
 // TODO: move this to Wallet class
 function signOffline(
   wallet: Wallet,
   txJSON: string,
-  options?: SignOptions
+  options?: SignOptions,
 ): { signedTransaction: string; id: string } {
-  const { publicKey, privateKey } = wallet;
-  return signWithKeypair(null, txJSON, { publicKey, privateKey }, options);
+  const { publicKey, privateKey } = wallet
+  return signWithKeypair(null, txJSON, { publicKey, privateKey }, options)
 }
 
-export { sign, signOffline };
+export { sign, signOffline }
