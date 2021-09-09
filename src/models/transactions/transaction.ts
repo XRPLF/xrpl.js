8d7dcf22611d6334d657e2255f3193750b686cb0
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-09-09 19:22:21 -0400

    refactor: fix '{' missing

--- src/models/transactions/transaction.ts
+++ src/models/transactions/transaction.ts
@@ -2,40 +2,40 @@
 /* eslint-disable max-lines-per-function -- need to work with a lot of Tx verifications */
 /* eslint-disable import/max-dependencies -- need to test more than 5 TxTypes */
 
-import _ from "lodash";
-import { encode, decode } from "ripple-binary-codec";
+import _ from 'lodash'
+import { encode, decode } from 'ripple-binary-codec'
 
-import { ValidationError } from "../../common/errors";
-import TransactionMetadata from "./metadata";
+import { ValidationError } from '../../common/errors'
+import TransactionMetadata from './metadata'
 
-import { AccountDelete, verifyAccountDelete } from "./accountDelete";
-import { AccountSet, verifyAccountSet } from "./accountSet";
-import { CheckCancel, verifyCheckCancel } from "./checkCancel";
-import { CheckCash, verifyCheckCash } from "./checkCash";
-import { CheckCreate, verifyCheckCreate } from "./checkCreate";
-import { DepositPreauth, verifyDepositPreauth } from "./depositPreauth";
-import { EscrowCancel, verifyEscrowCancel } from "./escrowCancel";
-import { EscrowCreate, verifyEscrowCreate } from "./escrowCreate";
-import { EscrowFinish, verifyEscrowFinish } from "./escrowFinish";
-import { OfferCancel, verifyOfferCancel } from "./offerCancel";
-import { OfferCreate, verifyOfferCreate } from "./offerCreate";
+import { AccountDelete, verifyAccountDelete } from './accountDelete'
+import { AccountSet, verifyAccountSet } from './accountSet'
+import { CheckCancel, verifyCheckCancel } from './checkCancel'
+import { CheckCash, verifyCheckCash } from './checkCash'
+import { CheckCreate, verifyCheckCreate } from './checkCreate'
+import { DepositPreauth, verifyDepositPreauth } from './depositPreauth'
+import { EscrowCancel, verifyEscrowCancel } from './escrowCancel'
+import { EscrowCreate, verifyEscrowCreate } from './escrowCreate'
+import { EscrowFinish, verifyEscrowFinish } from './escrowFinish'
+import { OfferCancel, verifyOfferCancel } from './offerCancel'
+import { OfferCreate, verifyOfferCreate } from './offerCreate'
 import {
   PaymentChannelClaim,
   verifyPaymentChannelClaim,
-} from "./paymentChannelClaim";
+} from './paymentChannelClaim'
 import {
   PaymentChannelCreate,
   verifyPaymentChannelCreate,
-} from "./paymentChannelCreate";
+} from './paymentChannelCreate'
 import {
   PaymentChannelFund,
   verifyPaymentChannelFund,
-} from "./paymentChannelFund";
-import { Payment, verifyPayment } from "./payment";
-import { SetRegularKey, verifySetRegularKey } from "./setRegularKey";
-import { SignerListSet, verifySignerListSet } from "./signerListSet";
-import { TicketCreate, verifyTicketCreate } from "./ticketCreate";
-import { TrustSet, verifyTrustSet } from "./trustSet";
+} from './paymentChannelFund'
+import { Payment, verifyPayment } from './payment'
+import { SetRegularKey, verifySetRegularKey } from './setRegularKey'
+import { SignerListSet, verifySignerListSet } from './signerListSet'
+import { TicketCreate, verifyTicketCreate } from './ticketCreate'
+import { TrustSet, verifyTrustSet } from './trustSet'
 
 export type Transaction =
   | AccountDelete
@@ -59,8 +59,8 @@ export type Transaction =
   | TrustSet
 
 export interface TransactionAndMetadata {
-  transaction: Transaction;
-  metadata: TransactionMetadata;
+  transaction: Transaction
+  metadata: TransactionMetadata
 }
 
 /**
@@ -72,91 +72,92 @@ export interface TransactionAndMetadata {
  */
 export function verify(tx: Record<string, unknown>): void {
   switch (tx.TransactionType) {
-    case "AccountDelete":
-      verifyAccountDelete(tx);
-      break;
+    case 'AccountDelete':
+      verifyAccountDelete(tx)
+      break
 
-    case "AccountSet":
-      verifyAccountSet(tx);
-      break;
+    case 'AccountSet':
+      verifyAccountSet(tx)
+      break
 
-    case "CheckCancel":
-      verifyCheckCancel(tx);
-      break;
+    case 'CheckCancel':
+      verifyCheckCancel(tx)
+      break
 
-    case "CheckCash":
-      verifyCheckCash(tx);
-      break;
+    case 'CheckCash':
+      verifyCheckCash(tx)
+      break
 
-    case "CheckCreate":
-      verifyCheckCreate(tx);
-      break;
+    case 'CheckCreate':
+      verifyCheckCreate(tx)
+      break
 
-    case "DepositPreauth":
-      verifyDepositPreauth(tx);
-      break;
+    case 'DepositPreauth':
+      verifyDepositPreauth(tx)
+      break
 
-    case "EscrowCancel":
-      verifyEscrowCancel(tx);
-      break;
+    case 'EscrowCancel':
+      verifyEscrowCancel(tx)
+      break
 
-    case "EscrowCreate":
-      verifyEscrowCreate(tx);
-      break;
+    case 'EscrowCreate':
+      verifyEscrowCreate(tx)
+      break
 
-    case "EscrowFinish":
-      verifyEscrowFinish(tx);
-      break;
+    case 'EscrowFinish':
+      verifyEscrowFinish(tx)
+      break
 
-    case "OfferCancel":
-      verifyOfferCancel(tx);
-      break;
+    case 'OfferCancel':
+      verifyOfferCancel(tx)
+      break
 
-    case "OfferCreate":
-      verifyOfferCreate(tx);
-      break;
+    case 'OfferCreate':
+      verifyOfferCreate(tx)
+      break
 
-    case "Payment":
-      verifyPayment(tx);
-      break;
+    case 'Payment':
+      verifyPayment(tx)
+      break
 
-    case "PaymentChannelClaim":
-      verifyPaymentChannelClaim(tx);
-      break;
+    case 'PaymentChannelClaim':
+      verifyPaymentChannelClaim(tx)
+      break
 
-    case "PaymentChannelCreate":
-      verifyPaymentChannelCreate(tx);
-      break;
+    case 'PaymentChannelCreate':
+      verifyPaymentChannelCreate(tx)
+      break
 
-    case "PaymentChannelFund":
-      verifyPaymentChannelFund(tx);
-      break;
+    case 'PaymentChannelFund':
+      verifyPaymentChannelFund(tx)
+      break
 
-    case "SetRegularKey":
-      verifySetRegularKey(tx);
-      break;
+    case 'SetRegularKey':
+      verifySetRegularKey(tx)
+      break
 
-    case "SignerListSet":
-      verifySignerListSet(tx);
-      break;
+    case 'SignerListSet':
+      verifySignerListSet(tx)
+      break
 
-    case "TicketCreate":
-      verifyTicketCreate(tx);
-      break;
+    case 'TicketCreate':
+      verifyTicketCreate(tx)
+      break
 
-    case "TrustSet":
-      verifyTrustSet(tx);
-      break;
+    case 'TrustSet':
+      verifyTrustSet(tx)
+      break
 
     default:
-      throw new ValidationError(`Invalid field TransactionType`);
+      throw new ValidationError(`Invalid field TransactionType`)
   }
 
   if (
     !_.isEqual(
       decode(encode(tx)),
-      _.omitBy(tx, (value) => value == null)
+      _.omitBy(tx, (value) => value == null),
     )
   ) {
-    throw new ValidationError(`Invalid Transaction: ${tx.TransactionType}`);
+    throw new ValidationError(`Invalid Transaction: ${tx.TransactionType}`)
   }
+}
