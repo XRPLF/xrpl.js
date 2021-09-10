41c97a1d6c5d99b8621d60ba260f787231053a46
Nathan Nichols <natenichols@cox.net>
2021-09-10 12:32:38 -0700

    Lint utils directory (#1563)
    * Lint the utils directory
    
    * Modify computeLedgerHash to take new Ledger format.

---
+++ src/utils/hashes/shaMap/node.ts
@@ -0,0 +1,14 @@
+export enum NodeType {
+  INNER = 1,
+  TRANSACTION_NO_METADATA = 2,
+  TRANSACTION_METADATA = 3,
+  ACCOUNT_STATE = 4,
+}
+
+/**
+ * Abstract base class for SHAMapNode.
+ */
+export abstract class Node {
+  public abstract addItem(_tag: string, _node: Node): void
+  public abstract get hash(): string
+}
