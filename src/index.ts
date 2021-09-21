94a8c652002278d35f20233d35d16f6dd669c9bc
Jackson Mills <jmills@ripple.com>
2021-09-21 09:08:56 -0700

    Remove old types (#1630)
    * Remove transaction/types
    
    * Delete common/types/commands
    
    * Delete common/types

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,6 @@ export { default as BroadcastClient } from './client/broadcastClient'
 
 export { Client } from './client'
 
-export * from './transaction/types'
-
-export * from './common/types/objects/ledger'
-
 export * from './models/methods'
 
 export * from './models/transactions'
