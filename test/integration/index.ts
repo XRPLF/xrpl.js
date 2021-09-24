533a01bd5f5856188d564764455e80b4f82af4df
Nathan Nichols <natenichols@cox.net>
2021-09-24 07:57:52 -0700

    fix: speed up webpacking

---
+++ test/integration/index.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/export -- Tells webpack which files exist. */
+export * from './integration'
+export * from './transactions/signerListSet'
+export * from './transactions/payment'
+export * from './transactions/offerCreate'
+export * from './transactions/offerCancel'
+export * from './transactions/signerListSet'
+export * from './requests/utility'
