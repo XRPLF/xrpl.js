489277a10348f06a713880b8fe77f280e67f2536
Nathan Nichols <natenichols@cox.net>
2021-09-27 10:47:56 -0700

    spacing

--- test/integration/faucet.ts
+++ test/integration/faucet.ts
@@ -10,42 +10,54 @@ describe('generateFaucetWallet', function () {
   this.timeout(TIMEOUT)
   it('submit generates a testnet wallet', async function () {
     const api = new Client('wss://s.altnet.rippletest.net:51233')
+
     await api.connect()
     const wallet = await api.generateFaucetWallet()
+
     assert.notEqual(wallet, undefined)
     assert(isValidClassicAddress(wallet?.classicAddress ?? ''))
     assert(isValidXAddress(wallet?.getXAddress() ?? ''))
+
     const info = await api.request({
       command: 'account_info',
       account: wallet?.classicAddress ?? '',
     })
     assert.equal(info.result.account_data.Balance, '1000000000')
+
     await api.generateFaucetWallet(wallet)
+
     const afterSent = await api.request({
       command: 'account_info',
       account: wallet?.classicAddress ?? '',
     })
     assert.equal(afterSent.result.account_data.Balance, '2000000000')
+
     await api.disconnect()
   })
   it('submit generates a devnet wallet', async function () {
     const api = new Client('wss://s.devnet.rippletest.net:51233')
+
     await api.connect()
     const wallet = await api.generateFaucetWallet()
+
     assert.notEqual(wallet, undefined)
     assert(isValidClassicAddress(wallet?.classicAddress ?? ''))
     assert(isValidXAddress(wallet?.getXAddress() ?? ''))
+
     const info = await api.request({
       command: 'account_info',
       account: wallet?.classicAddress ?? '',
     })
     assert.equal(info.result.account_data.Balance, '1000000000')
+
     await api.generateFaucetWallet(wallet)
+
     const afterSent = await api.request({
       command: 'account_info',
       account: wallet?.classicAddress ?? '',
     })
     assert.equal(afterSent.result.account_data.Balance, '2000000000')
+
     await api.disconnect()
   })
 })
