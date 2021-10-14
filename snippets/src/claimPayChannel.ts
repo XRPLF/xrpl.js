0215661aeaf8bf3893fe97c137ee7d18a819efd5
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-10-14 12:20:22 -0400

    refactor: add snippets for sendEscrow, claimPayChannel, reliableSub

---
+++ snippets/src/claimPayChannel.ts
@@ -0,0 +1,105 @@
+/* eslint-disable no-await-in-loop -- waiting required. */
+/* eslint-disable max-lines-per-function -- Snippet for sending escrow flow. */
+/* eslint-disable no-console -- logs are helpful to understand snippets */
+import {
+  AccountInfoRequest,
+  AccountObjectsRequest,
+  Client,
+  PaymentChannelCreate,
+  PaymentChannelClaim,
+  hashes,
+  LedgerRequest,
+  ISOTimeToRippleTime,
+} from '../../dist/npm'
+
+const client = new Client('wss://s.altnet.rippletest.net:51233')
+
+async function getXRPBalance(account: string): Promise<string> {
+  const request: AccountInfoRequest = {
+    command: 'account_info',
+    account,
+  }
+  return (await client.request(request)).result.account_data.Balance
+}
+
+async function sleep(ms): Promise<void> {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms)
+  })
+}
+
+void claimPayChannel()
+
+async function claimPayChannel(): Promise<void> {
+  await client.connect()
+
+  const { wallet: wallet1 } = await client.fundWallet()
+  const { wallet: wallet2 } = await client.fundWallet()
+
+  console.log('Balances of wallets before Payment Channel is claimed')
+  console.log(
+    await getXRPBalance(wallet1.classicAddress),
+    await getXRPBalance(wallet2.classicAddress),
+  )
+  // eslint-disable-next-line new-cap -- function defined as that.
+  const finishAfter = ISOTimeToRippleTime(Date()) + 2
+
+  const paymentChannelCreate: PaymentChannelCreate = {
+    TransactionType: 'PaymentChannelCreate',
+    Account: wallet1.getClassicAddress(),
+    Amount: '100',
+    Destination: wallet2.getClassicAddress(),
+    SettleDelay: 86400,
+    PublicKey: wallet1.publicKey,
+  }
+
+  const paymentChannelResponse = await client.submit(
+    wallet1,
+    paymentChannelCreate,
+  )
+
+  // check that the object was actually created
+  const accountObjectsRequest: AccountObjectsRequest = {
+    command: 'account_objects',
+    account: wallet1.getClassicAddress(),
+  }
+
+  const accountObjects = (await client.request(accountObjectsRequest)).result
+    .account_objects
+
+  console.log(accountObjects)
+
+  // wait till we get a ledger with close time > finish time
+  const ledgerRequest: LedgerRequest = {
+    command: 'ledger',
+    ledger_index: 'validated',
+  }
+  let updatedCloseTime
+  do {
+    await sleep(1000)
+    updatedCloseTime = (await client.request(ledgerRequest)).result.ledger
+      .close_time
+  } while (updatedCloseTime <= finishAfter)
+
+  const paymentChannelClaim: PaymentChannelClaim = {
+    Account: wallet1.getClassicAddress(),
+    TransactionType: 'PaymentChannelClaim',
+    Channel: hashes.hashPaymentChannel(
+      wallet1.getClassicAddress(),
+      wallet2.getClassicAddress(),
+      paymentChannelResponse.result.tx_json.Sequence ?? 0,
+    ),
+    Amount: '100',
+  }
+
+  const channelClaimResponse = await client.submit(wallet1, paymentChannelClaim)
+  console.log(channelClaimResponse)
+
+  console.log('Balances of wallets after Payment Channel is claimed')
+  console.log(
+    await getXRPBalance(wallet1.classicAddress),
+    await getXRPBalance(wallet2.classicAddress),
+  )
+
+  void client.disconnect()
+}
