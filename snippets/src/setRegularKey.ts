e061fae8232f142b19b99156a67480eeaa49a3d1
Mukul Jangid <49061120+mukulljangid@users.noreply.github.com>
2021-10-15 15:03:27 -0400

    refactor: add RegularKey

---
+++ snippets/src/setRegularKey.ts
@@ -0,0 +1,42 @@
+/* eslint-disable no-console -- logs are helpful to understand snippets */
+import { Client, Payment, SetRegularKey } from '../../dist/npm'
+
+const client = new Client('wss://s.altnet.rippletest.net:51233')
+
+async function main(): Promise<void> {
+  await client.connect()
+  const { wallet: wallet1 } = await client.fundWallet()
+  const { wallet: wallet2 } = await client.fundWallet()
+  const { wallet: regularKeyWallet } = await client.fundWallet()
+
+  console.log('Balances before payment')
+  console.log(client.getXrpBalance(wallet1.classicAddress))
+  console.log(client.getXrpBalance(wallet2.classicAddress))
+
+  const tx: SetRegularKey = {
+    TransactionType: 'SetRegularKey',
+    Account: wallet1.classicAddress,
+    RegularKey: regularKeyWallet.classicAddress,
+  }
+  const response = await client.submitReliable(wallet1, tx)
+
+  console.log('Response for successful SetRegularKey tx')
+  console.log(response)
+
+  const payment: Payment = {
+    TransactionType: 'Payment',
+    Account: wallet1.classicAddress,
+    Destination: wallet2.classicAddress,
+    Amount: '1000',
+  }
+
+  const submitTx = await client.submit(regularKeyWallet, payment)
+  console.log('Blob for tx signed using Regular Key')
+  console.log(submitTx)
+  console.log('Balances before payment')
+  console.log(client.getXrpBalance(wallet1.classicAddress))
+  console.log(client.getXrpBalance(wallet2.classicAddress))
+
+  await client.disconnect()
+}
+void main()
