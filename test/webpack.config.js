e2e6b7a7014682d096df99acda5e1f50f50fe267
Mayukha Vadari <mvadari@ripple.com>
2021-10-18 16:42:45 -0400

    add to test webpacking

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -64,6 +64,7 @@ function webpackForTest(testFileName) {
       alias: {
         ws: './dist/npm/client/wsWrapper.js',
         'https-proxy-agent': false,
+        'bn.js': path.join(__dirname, 'node_modules/bn.js/lib/bn.js'),
       },
       extensions: ['.ts', '.js', '.json'],
       fallback: {
