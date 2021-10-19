1f15eadbdd14fd81fa41c9896b2f767687e37ef7
Jackson Mills <jmills@ripple.com>
2021-10-19 08:50:48 -0700

    Add export for setTransactionFlagsToNumber

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,10 @@
 import * as LedgerEntry from './ledger'
-import { parseAccountRootFlags } from './utils/flags'
+import setTransactionFlagsToNumber, {
+  parseAccountRootFlags,
+} from './utils/flags'
 
 export * from './methods'
 
 export * from './transactions'
 
-export { LedgerEntry, parseAccountRootFlags }
+export { LedgerEntry, parseAccountRootFlags, setTransactionFlagsToNumber }
