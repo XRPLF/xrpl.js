47b558c5a18185fe34dffea81256b3273006c716
Mayukha Vadari <mvadari@ripple.com>
2022-03-28 18:03:07 -0400

    fix docstrings

--- packages/xrpl/src/utils/createCrossChainPayment.ts
+++ packages/xrpl/src/utils/createCrossChainPayment.ts
@@ -5,12 +5,12 @@ import { Memo } from '../models/common'
 import { convertStringToHex } from './stringConversion'
 
 /**
- * Computes the complete list of every balance that changed in the ledger
- * as a result of the given transaction.
+ * Creates a cross-chain payment transaction.
  *
  * @param payment - The initial payment transaction. If the transaction is
  * signed, then it will need to be re-signed. There must be no more than 2
- * memos, since one memo is used for the sidechain destination account.
+ * memos, since one memo is used for the sidechain destination account. The
+ * destination must be the sidechain's door account.
  * @param destAccount - the destination account on the sidechain.
  * @returns A cross-chain payment transaction, where the mainchain door account
  * is the `Destination` and the destination account on the sidechain is encoded
