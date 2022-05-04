b9e9923a3cdb8c3fda4a4758e9d8f32dc63fba00
Jackson Mills <jmills@ripple.com>
2022-05-04 13:27:35 -0700

    Fix linter error

--- packages/xrpl/src/Wallet/rfc1751.ts
+++ packages/xrpl/src/Wallet/rfc1751.ts
@@ -14,7 +14,8 @@ import rfc1751Words from './rfc1751Words.json'
 
 const rfc1751WordList: string[] = rfc1751Words
 
-// Added to allow _BINARY to be on two lines, instead of one entry per line.
+// Added prettier-ignore to allow _BINARY to be on two lines, instead of one entry per line.
+
 // prettier-ignore
 const _BINARY = ['0000', '0001', '0010', '0011', '0100', '0101', '0110', '0111',
                 '1000', '1001', '1010', '1011', '1100', '1101', '1110', '1111'];
