e0233a620ca0caa4694b613b389749db5c2008c6
Mayukha Vadari <mvadari@ripple.com>
2022-07-12 11:04:45 -0400

    add XChainCommit tx

---
+++ packages/xrpl/src/models/transactions/XChainCommit.ts
@@ -0,0 +1,40 @@
+import { ValidationError } from '../../errors'
+import { Amount, Bridge } from '../common'
+
+import { BaseTransaction, validateBaseTransaction } from './common'
+
+/**
+ *
+ * @category Transaction Models
+ */
+export interface XChainCommit extends BaseTransaction {
+  TransactionType: 'XChainCommit'
+
+  Bridge: Bridge
+
+  XChainClaimID: number | string
+
+  Amount: Amount
+}
+
+/**
+ * Verify the form and type of a XChainCommit at runtime.
+ *
+ * @param tx - A XChainCommit Transaction.
+ * @throws When the XChainCommit is malformed.
+ */
+export function validateXChainCommit(tx: Record<string, unknown>): void {
+  validateBaseTransaction(tx)
+
+  if (tx.Bridge == null) {
+    throw new ValidationError('XChainCommit: missing field Bridge')
+  }
+
+  if (tx.XChainCommitID == null) {
+    throw new ValidationError('XChainCommit: missing field XChainCommitID')
+  }
+
+  if (tx.Amount == null) {
+    throw new ValidationError('XChainCommit: missing field Amount')
+  }
+}
