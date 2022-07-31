4b91908ca1addee59a9d0b01c90613f7128d551d
Mayukha Vadari <mvadari@ripple.com>
2022-07-31 17:13:28 +0200

    remove util

--- packages/xrpl/src/utils/index.ts
+++ packages/xrpl/src/utils/index.ts
@@ -22,7 +22,6 @@ import { Response } from '../models/methods'
 import { PaymentChannelClaim } from '../models/transactions/paymentChannelClaim'
 import { Transaction } from '../models/transactions/transaction'
 
-import createCrossChainPayment from './createCrossChainPayment'
 import { deriveKeypair, deriveAddress, deriveXAddress } from './derive'
 import getBalanceChanges from './getBalanceChanges'
 import {
@@ -218,6 +217,5 @@ export {
   encodeForMultiSigning,
   encodeForSigning,
   encodeForSigningClaim,
-  createCrossChainPayment,
   parseNFTokenID,
 }
