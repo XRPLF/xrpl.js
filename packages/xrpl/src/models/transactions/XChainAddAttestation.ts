709ab7f6404520452728ace33f3a9348c3860669
Mayukha Vadari <mvadari@ripple.com>
2022-08-26 18:41:39 +0200

    more fixes

--- packages/xrpl/src/models/transactions/XChainAddAttestation.ts
+++ packages/xrpl/src/models/transactions/XChainAddAttestation.ts
@@ -19,7 +19,7 @@ export interface XChainAddAttestation extends BaseTransaction {
 
         Amount: Amount
 
-        AttestationRewardAccount: string
+        AttestationSignerAccount: string
 
         Destination: string
 
@@ -39,7 +39,7 @@ export interface XChainAddAttestation extends BaseTransaction {
 
         Amount: Amount
 
-        AttestationRewardAccount: string
+        AttestationSignerAccount: string
 
         Destination: string
 
