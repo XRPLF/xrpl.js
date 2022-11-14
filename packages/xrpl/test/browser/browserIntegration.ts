78d8bc7a7aa044626255a093554e753ee8910582
Connor Chen <67431889+connorjchen@users.noreply.github.com>
2022-11-14 11:49:19 -0500

    Increase intergration runtime allowed

--- packages/xrpl/test/browser/browserIntegration.ts
+++ packages/xrpl/test/browser/browserIntegration.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { expect, assert } from 'chai'
 import puppeteer from 'puppeteer'
 
-const TIMEOUT = 120000
+const TIMEOUT = 150000
 interface TestCaseInfo {
   name: string
   span: string
