ff0c9c53bbca63a0b81de043d0a6b25d2cecb095
Jackson Mills <jmills@ripple.com>
2022-12-13 15:31:53 -0700

    Factor objects out of xrpl-definitions.ts

--- packages/ripple-binary-codec/src/enums/xrpl-definitions.ts
+++ packages/ripple-binary-codec/src/enums/xrpl-definitions.ts
@@ -1,11 +1,12 @@
 import { SerializedType } from '../types/serialized-type'
-import { Buffer } from 'buffer/'
-import { BytesList, BinaryParser } from '../binary'
-
-const TYPE_WIDTH = 2
-const LEDGER_ENTRY_WIDTH = 2
-const TRANSACTION_TYPE_WIDTH = 2
-const TRANSACTION_RESULT_WIDTH = 1
+import { Bytes, BytesLookup } from './bytes'
+import { FieldInfo, FieldLookup, FieldInstance } from './field'
+import {
+  TYPE_WIDTH,
+  LEDGER_ENTRY_WIDTH,
+  TRANSACTION_TYPE_WIDTH,
+  TRANSACTION_RESULT_WIDTH,
+} from './constants'
 
 interface DefinitionsData {
   TYPES: Record<string, number>
@@ -13,163 +14,6 @@ interface DefinitionsData {
   FIELDS: (string | FieldInfo)[][]
   TRANSACTION_RESULTS: Record<string, number>
   TRANSACTION_TYPES: Record<string, number>
-}
-
-/*
- * @brief: Serialize a field based on type_code and Field.nth
- */
-function fieldHeader(type: number, nth: number): Buffer {
-  const header: Array<number> = []
-  if (type < 16) {
-    if (nth < 16) {
-      header.push((type << 4) | nth)
-    } else {
-      header.push(type << 4, nth)
-    }
-  } else if (nth < 16) {
-    header.push(nth, type)
-  } else {
-    header.push(0, type, nth)
-  }
-  return Buffer.from(header)
-}
-
-/*
- * @brief: Bytes, name, and ordinal representing one type, ledger_type, transaction type, or result
- */
-class Bytes {
-  readonly bytes: Buffer
-
-  constructor(
-    readonly name: string,
-    readonly ordinal: number,
-    readonly ordinalWidth: number,
-  ) {
-    this.bytes = Buffer.alloc(ordinalWidth)
-    for (let i = 0; i < ordinalWidth; i++) {
-      this.bytes[ordinalWidth - i - 1] = (ordinal >>> (i * 8)) & 0xff
-    }
-  }
-
-  toJSON(): string {
-    return this.name
-  }
-
-  toBytesSink(sink: BytesList): void {
-    sink.put(this.bytes)
-  }
-
-  toBytes(): Uint8Array {
-    return this.bytes
-  }
-}
-
-/*
- * @brief: Collection of Bytes objects, mapping bidirectionally
- */
-class BytesLookup {
-  constructor(types: Record<string, number>, readonly ordinalWidth: number) {
-    Object.entries(types).forEach(([k, v]) => {
-      this.add(k, v)
-    })
-  }
-
-  /**
-   * Add a new name value pair to the BytesLookup.
-   *
-   * @param name - A human readable name for the field.
-   * @param value - The numeric value for the field.
-   * @throws if the name or value already exist in the lookup because it's unclear how to decode.
-   */
-  add(name: string, value: number): void {
-    if (this[name]) {
-      throw new SyntaxError(
-        `Attempted to add a value with a duplicate name "${name}". This is not allowed because it is unclear how to decode.`,
-      )
-    }
-    if (this[value.toString()]) {
-      throw new SyntaxError(
-        `Attempted to add a duplicate value under a different name (Given name: "${name}" and previous name: "${
-          this[value.toString()]
-        }. This is not allowed because it is unclear how to decode.\nGiven value: ${value.toString()}`,
-      )
-    }
-    this[name] = new Bytes(name, value, this.ordinalWidth)
-    this[value.toString()] = this[name]
-  }
-
-  from(value: Bytes | string): Bytes {
-    return value instanceof Bytes ? value : (this[value] as Bytes)
-  }
-
-  fromParser(parser: BinaryParser): Bytes {
-    return this.from(parser.readUIntN(this.ordinalWidth).toString())
-  }
-}
-
-/**
- * Encoding information for a rippled field, often used in transactions.
- * See the enums [README.md](https://github.com/XRPLF/xrpl.js/tree/main/packages/ripple-binary-codec/src/enums) for more details on what each means.
- */
-interface FieldInfo {
-  nth: number
-  isVLEncoded: boolean
-  isSerialized: boolean
-  isSigningField: boolean
-  type: string
-}
-
-interface FieldInstance {
-  readonly nth: number
-  readonly isVariableLengthEncoded: boolean
-  readonly isSerialized: boolean
-  readonly isSigningField: boolean
-  readonly type: Bytes
-  readonly ordinal: number
-  readonly name: string
-  readonly header: Buffer
-  readonly associatedType: typeof SerializedType
-}
-
-function buildField(
-  [name, info]: [string, FieldInfo],
-  typeOrdinal: number,
-): FieldInstance {
-  const field = fieldHeader(typeOrdinal, info.nth)
-  return {
-    name: name,
-    nth: info.nth,
-    isVariableLengthEncoded: info.isVLEncoded,
-    isSerialized: info.isSerialized,
-    isSigningField: info.isSigningField,
-    ordinal: (typeOrdinal << 16) | info.nth,
-    type: new Bytes(info.type, typeOrdinal, TYPE_WIDTH),
-    header: field,
-    associatedType: SerializedType, // For later assignment in ./types/index.js or Definitions.updateAll(...)
-  }
-}
-
-/*
- * @brief: The collection of all fields as defined in definitions.json
- */
-class FieldLookup {
-  constructor(
-    fields: Array<[string, FieldInfo]>,
-    types: Record<string, number>,
-  ) {
-    fields.forEach(([k, v]) => {
-      this.add(k, v, types[v.type])
-    })
-  }
-
-  public add(name: string, field_info: FieldInfo, typeOrdinal: number): void {
-    this[name] = buildField([name, field_info], typeOrdinal)
-    this[this[name].ordinal.toString()] = this[name]
-  }
-
-  fromString(value: string): FieldInstance {
-    return this[value] as FieldInstance
-  }
 }
 
 /**
