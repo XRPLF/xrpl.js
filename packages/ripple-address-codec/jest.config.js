6c91ee5ddb072218d8c55aa716c2b06377a52e14
Justin Reynolds <justinr1234@gmail.com>
2022-12-16 20:32:57 -0600

    re-enable all tests

--- packages/ripple-address-codec/jest.config.js
+++ packages/ripple-address-codec/jest.config.js
@@ -3,6 +3,5 @@ const base = require('../../jest.config.base.js')
 
 module.exports = {
   ...base,
-  name: 'ripple-address-codec',
   displayName: 'ripple-address-codec',
 }
--- packages/ripple-keypairs/jest.config.js
+++ packages/ripple-keypairs/jest.config.js
@@ -4,6 +4,5 @@ const base = require('../../jest.config.base.js')
 module.exports = {
   ...base,
   roots: [...base.roots, '<rootDir>/test'],
-  name: 'ripple-keypairs',
   displayName: 'ripple-keypairs',
 }
