8abcfe4640000da86261dd9792f841c124c55392
justinr1234 <justinr1234@gmail.com>
2023-01-06 14:04:36 -0600

    chore: rename test files (#2181)

--- packages/xrpl/test/integration/wallet.test.ts
+++ packages/xrpl/test/integration/wallet.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-default-export-name/default-export-match-filename -- This is a test file. */
 import { Wallet } from 'xrpl-local'
 
 const walletSecret = 'shK6YXzwYfnFVn3YZSaMh5zuAddKx'
