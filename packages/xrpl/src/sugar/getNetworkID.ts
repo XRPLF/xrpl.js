37c03460af4323da7ef27cf8c89ada86b19c8a5d
Denis Angell <dangell@transia.co>
2023-02-24 16:23:02 -0500

    update getNetworkID

--- packages/xrpl/src/sugar/getNetworkID.ts
+++ packages/xrpl/src/sugar/getNetworkID.ts
@@ -1,5 +1,5 @@
 import type { Client } from '..'
-import { XrplError } from '../errors'
+// import { XrplError } from '../errors'
 
 /**
  * Returns the network ID of the rippled server.
@@ -11,11 +11,5 @@ export default async function getNetworkID(this: Client): Promise<number> {
   const response = await this.request({
     command: 'server_info',
   })
-  const networkID = response.result.info.network_id
-  if (networkID == null) {
-    throw new XrplError(
-      'getNetworkID: Could not get network_id from server_info',
-    )
-  }
-  return networkID
+  return response.result.info.network_id ?? 1
 }
