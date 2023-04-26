68454be1b18e771cd8d7d4ee3b7ae0c459e86f46
Nicholas Dudfield <ndudfield@gmail.com>
2023-04-26 09:34:57 +0700

    feat: EOL node 14

--- packages/ripple-keypairs/src/random.ts
+++ packages/ripple-keypairs/src/random.ts
@@ -1,5 +1,5 @@
-// TODO: import { randomBytes as rand } from '@noble/hashes/utils'
-import rand from 'brorand'
+import { randomBytes as rand } from '@noble/hashes/utils'
+// import rand from 'brorand' // TODO
 
 export default function randomBytes(n: number) {
   return rand(n)
