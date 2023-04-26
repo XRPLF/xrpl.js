95bcbbd71c507177b1f83af563d22f98956a7b7a
Nicholas Dudfield <ndudfield@gmail.com>
2023-04-26 09:17:14 +0700

    fix: revert brorand replacement experiment

--- packages/ripple-keypairs/src/random.ts
+++ packages/ripple-keypairs/src/random.ts
@@ -1,4 +1,5 @@
-import { randomBytes as rand } from '@noble/hashes/utils'
+// TODO: import { randomBytes as rand } from '@noble/hashes/utils'
+import rand from 'brorand'
 
 export default function randomBytes(n: number) {
   return rand(n)
