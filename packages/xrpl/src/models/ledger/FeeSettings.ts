0ef72f67a6eb4e8d386fb34544014b07280e88e8
Caleb Kniffen <ckniffen@ripple.com>
2023-06-29 15:49:14 -0500

    feat(types): Update `FeeSettings` and add id
    Adds the new fields for `XRPFees` amendment and id for the `FeeSettings`
    singleton ledger entry.
    
    Closes #2219

--- packages/xrpl/src/models/ledger/FeeSettings.ts
+++ packages/xrpl/src/models/ledger/FeeSettings.ts
@@ -1,17 +1,10 @@
 import BaseLedgerEntry from './BaseLedgerEntry'
 
-/**
- * The FeeSettings object type contains the current base transaction cost and
- * reserve amounts as determined by fee voting.
- *
- * @category Ledger Entries
- */
-export default interface FeeSettings extends BaseLedgerEntry {
-  LedgerEntryType: 'FeeSettings'
-  /**
-   * The transaction cost of the "reference transaction" in drops of XRP as
-   * hexadecimal.
-   */
+export const FEE_SETTINGS_ID =
+  '4BC50C9B0D8515D3EAAE1E74B29A95804346C491EE1A95BF25E4AAB854A6A651'
+
+export interface FeeSettingsPreAmendmentFields {
+  /** The transaction cost of the "reference transaction" in drops of XRP as hexadecimal. */
   BaseFee: string
   /** The BaseFee translated into "fee units". */
   ReferenceFeeUnits: number
@@ -19,9 +12,38 @@ export default interface FeeSettings extends BaseLedgerEntry {
   ReserveBase: number
   /** The incremental owner reserve for owning objects, as drops of XRP. */
   ReserveIncrement: number
-  /**
-   * A bit-map of boolean flags for this object. No flags are defined for this
-   * type.
-   */
-  Flags: number
 }
+
+export interface FeeSettingsPostAmendmentFields {
+  /** The transaction cost of the "reference transaction" in drops of XRP as hexadecimal. */
+  BaseFeeDrops: string
+  /** The base reserve for an account in the XRP Ledger, as drops of XRP. */
+  ReserveBaseDrops: string
+  /** The incremental owner reserve for owning objects, as drops of XRP. */
+  ReserveIncrementDrops: string
+}
+
+export interface FeeSettingsBase extends BaseLedgerEntry {
+  LedgerEntryType: 'FeeSettings'
+  /**
+   * A bit-map of boolean flags for this object. No flags are defined for this type.
+   */
+  Flags: 0
+}
+
+/**
+ * The FeeSettings object type contains the current base transaction cost and
+ * reserve amounts as determined by fee voting.
+ *
+ * The fields will be based on the status of the `XRPFees` amendment.
+ * - Before: {@link FeeSettingsPreAmendmentFields}
+ * - After: {@link FeeSettingsPostAmendmentFields}
+ *
+ * @interface
+ *
+ * @category Ledger Entries
+ */
+type FeeSettings = FeeSettingsBase &
+  (FeeSettingsPreAmendmentFields | FeeSettingsPostAmendmentFields)
+
+export default FeeSettings
