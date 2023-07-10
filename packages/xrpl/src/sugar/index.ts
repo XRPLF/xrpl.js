10112b93c21fdb0c27d9f172d54ef505f6502291
Caleb Kniffen <ckniffen@ripple.com>
2023-07-10 16:37:48 -0500

    feat: export `Balance` type
    Closes #1823

--- packages/xrpl/src/sugar/index.ts
+++ packages/xrpl/src/sugar/index.ts
@@ -1,6 +1,6 @@
 export { default as autofill } from './autofill'
 
-export { getBalances, getXrpBalance } from './balances'
+export { getBalances, getXrpBalance, Balance } from './balances'
 
 export { default as getLedgerIndex } from './getLedgerIndex'
 
