5c759f4129ed2cf4308454b6196b75ac193b1212
Caleb Kniffen <ckniffen@ripple.com>
2023-08-09 20:58:18 -0500

    remove bn.js dedupe code, more polyfills, and fix crypto/utils browser path
    Turns out `stream-browserify` cant be removed till #2428 goes into effect.

--- packages/xrpl/webpack.config.js
+++ packages/xrpl/webpack.config.js
@@ -3,15 +3,6 @@ const path = require('path')
 const webpack = require('webpack')
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer')
 
-const bnJsReplaces = [
-  'tiny-secp256k1',
-  'asn1.js',
-  'create-ecdh',
-  'miller-rabin',
-  'public-encrypt',
-  'elliptic',
-]
-
 function getDefaultConfiguration() {
   return {
     cache: true,
@@ -28,22 +19,6 @@ function getDefaultConfiguration() {
       new webpack.NormalModuleReplacementPlugin(/^ws$/, './WSWrapper'),
       new webpack.ProvidePlugin({ process: 'process/browser' }),
       new webpack.ProvidePlugin({ Buffer: ['buffer', 'Buffer'] }),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^\.\/wordlists\/(?!english)/,
-        contextRegExp: /bip39\/src$/,
-      }),
-      // this is a bit of a hack to prevent 'bn.js' from being installed 6 times
-      // TODO: any package that is updated to use bn.js 5.x needs to be removed from `bnJsReplaces` above
-      // https://github.com/webpack/webpack/issues/5593#issuecomment-390356276
-      new webpack.NormalModuleReplacementPlugin(/^bn.js$/, (resource) => {
-        if (
-          bnJsReplaces.some((pkg) =>
-            resource.context.includes(`node_modules/${pkg}`),
-          )
-        ) {
-          resource.request = 'diffie-hellman/node_modules/bn.js'
-        }
-      }),
     ],
     module: {
       rules: [
@@ -55,7 +30,6 @@ function getDefaultConfiguration() {
     },
     resolve: {
       alias: {
-        crypto: false,
         ws: './dist/npm/client/WSWrapper.js',
       },
       extensions: ['.js', '.json'],
@@ -65,7 +39,6 @@ function getDefaultConfiguration() {
       symlinks: false,
       fallback: {
         buffer: require.resolve('buffer/'),
-        stream: require.resolve('stream-browserify')
       },
     },
   }
