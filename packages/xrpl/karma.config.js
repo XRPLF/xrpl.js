0f87baf4237b688b9ca6b2cb7fe3c646fca898b3
Caleb Kniffen <ckniffen@ripple.com>
2023-08-11 20:22:37 -0500

    test: remove extra polyfills only for testing (#2428)
    Removes the testing polyfills by using jasmine and not jest in the
    browser. We were already using jasmine but where overriding those
    methods with jest versions which was not needed.
    
    The previous karma setup also used a single entrypoint which meant that
    not all integration tests were running because all files were not
    imported in integrations/index.ts.
    
    This also eliminates 79 dev dependencies.

--- packages/xrpl/karma.config.js
+++ packages/xrpl/karma.config.js
@@ -15,18 +15,13 @@ module.exports = function (config) {
     frameworks: ['jasmine'],
 
     // list of files / patterns to load in the browser
-    files: [
-      'build/xrpl-latest.js',
-      'test/integration/index.ts',
-      'karma-setup.js',
-    ],
+    files: ['build/xrpl-latest.js', 'test/integration/**/*.test.ts'],
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'karma-setup.js': ['webpack'],
       // Use webpack to bundle our test files
-      'test/integration/index.ts': ['webpack'],
+      'test/integration/**/*.test.ts': ['webpack'],
     },
 
     browsers: ['ChromeHeadless'],
