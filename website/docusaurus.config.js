e48dd0b21c4474c721a561f127ab62f6a2959bbf
jonathanlei <jonathanlei98@gmail.com>
2023-08-17 12:03:27 -0700

    add docsearch

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -211,7 +211,7 @@ const config = {
               },
               {
                 label: "Subscribe to ripple-server",
-                href: "https://groups.google.com/g/ripple-server",
+                href: "https://groups.google.com/g/xrpl-announce",
               },
             ],
           },
@@ -221,6 +221,35 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+      },
+      algolia: {
+        // The application ID provided by Algolia
+        appId: "UWLAFN5MGH",
+
+        // Public API key: it is safe to commit it
+        apiKey: "f7b1b6b53eda9a560bf206a5148207d8",
+
+        indexName: "js-xrpl",
+
+        // Optional: see doc section below
+        contextualSearch: true,
+
+        // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+        externalUrlRegex: "external\\.com|domain\\.com",
+
+        // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+        replaceSearchResultPathname: {
+          from: "/docs/", // or as RegExp: /\/docs\//
+          to: "/",
+        },
+
+        // Optional: Algolia search parameters
+        searchParameters: {},
+
+        // Optional: path for search page that enabled by default (`false` to disable it)
+        searchPagePath: "search",
+
+        //... other Algolia params
       },
     }),
 };
