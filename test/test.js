ab6b8a62536b6ae23a8ba9cd7a2106203ea4572f
JST5000 <aim4math@gmail.com>
2023-08-23 16:35:09 -0700

    Add demo code to modify amm

---
+++ test/test.js
@@ -0,0 +1,48 @@
+const { Client } = require("xrpl");
+
+// The snippet walks us through creating and finishing escrows.
+async function sendTx() {
+  const client = new Client("wss://amm.devnet.rippletest.net:51233/");
+  await client.connect();
+
+  const { wallet: wallet1 } = await client.fundWallet();
+  const { wallet: wallet2 } = await client.fundWallet();
+
+  const currencyAmount = {
+    currency: "FOO",
+    issuer: wallet1.address,
+    value: "10000",
+  };
+
+  console.log(currencyAmount);
+
+  const trustset = {
+    TransactionType: "TrustSet",
+    Account: wallet2.address,
+    LimitAmount: currencyAmount,
+  };
+
+  const trustset_response = await client.submitAndWait(trustset, {
+    wallet: wallet2,
+  });
+
+  console.log("TrustSet Response:\n", trustset_response);
+
+  const ammCreate = {
+    TransactionType: "AMMCreate",
+    Account: wallet1.address,
+    Amount: "1000", // XRP
+    Amount2: { ...currencyAmount, value: "1000" },
+    TradingFee: 0,
+  };
+
+  const ammCreateResponse = await client.submitAndWait(ammCreate, {
+    wallet: wallet1,
+  });
+
+  console.log("AMMCreate Response:\n", ammCreateResponse);
+
+  await client.disconnect();
+}
+
+void sendTx();
