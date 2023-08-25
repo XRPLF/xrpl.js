011153272be2c54b1fed050ffa548cff5cea56a3
Caleb Kniffen <ckniffen@ripple.com>
2023-08-25 14:26:26 -0500

    Update packages/ripple-address-codec/jest.config.js
    fix copy pasta error
    
    Co-authored-by: Mayukha Vadari <mvadari@ripple.com>

--- packages/ripple-address-codec/jest.config.js
+++ packages/ripple-address-codec/jest.config.js
@@ -4,5 +4,5 @@ const base = require('../../jest.config.base.js')
 module.exports = {
   ...base,
   roots: [...base.roots, '<rootDir>/test'],
-  displayName: 'ripple-binary-codec',
+  displayName: 'ripple-address-codec',
 }
