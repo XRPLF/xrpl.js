091cc8f9db651ff9bc7b73d7abdfb10bcad29345
Caleb Kniffen <ckniffen@ripple.com>
2023-08-31 19:48:39 -0500

    feat: add `xrpl-secret-numbers` to the monorepo (#2445)
    - Add `xrpl-secret-numbers` by @WietseWind  to the mono repo.
    - `unpkg` and `jsdelivr` support was simplified by adding fields to package.json.
    - Unit tests run in a browser and node.
    
    BREAKING CHANGES:
    - `xrpl-secret-numbers` is now `@xrplf/secret-numbers`.
    - Changed the bundled file produced changed from  `dist/browerified.js` to `build/xrplf-secret-numbers-latest.js`.
    - Bundle variable is `xrplf_secret_numbers` instead of using browserify's loader.
    - If using CDN instructions in README will need to update to `https://cdn.jsdelivr.net/npm/@xrplf/secret-numbers`
    
    Making this library part of the monorepo was discussed in #1788.  This solves several problems with the upcoming `xrpl.js@3.0`.
    
    `xrpl-secret-numbers` has a dependency of `ripple-keypairs` and `xrpl` has a depenendecy on `xrpl-secret-numbers`.  This means that any change would require a separate release of `ripple-keypairs`, then a release of `xrpl-secret-numbers`, followed by `xrpl`.  Now they can be released all at once
    
    In 3.0 we eliminated the need for many polyfills like `util`, `assert`, and soon to be `buffer` (after noble libs PR is merged).  `xrpl-secret-numbers` still needs those.  This will also eliminate them and anytime similar changes in the future will be much easier.  This further reduces the bundle size of 3.0 branch by over 10% as well as reducing bundler setup.

---
+++ packages/secret-numbers/test/api.test.ts
@@ -0,0 +1,99 @@
+import * as keypairs from "ripple-keypairs";
+
+import { Account, Utils } from "../src";
+
+describe("API: XRPL Secret Numbers", () => {
+  describe("Generate new account", () => {
+    const account = new Account();
+    it("Output sanity checks", () => {
+      expect(account.getAddress()).toMatch(/^r[a-zA-Z0-9]{19,}$/u);
+      const entropy = Utils.secretToEntropy(`${account.toString()}`.split(" "));
+      const familySeed = keypairs.generateSeed({ entropy });
+      const keypair = keypairs.deriveKeypair(familySeed);
+      const address = keypairs.deriveAddress(keypair.publicKey);
+      expect(address).toEqual(account.getAddress());
+      expect(familySeed).toEqual(account.getFamilySeed());
+    });
+  });
+
+  describe("Account based on entropy", () => {
+    const entropy = Buffer.from("0123456789ABCDEF0123456789ABCDEF", "hex");
+    const account = new Account(entropy);
+
+    it("familySeed as expected", () => {
+      expect(account.getFamilySeed()).toEqual("sp5DmDCut79BpgumfHhvRzdxXYQyU");
+    });
+    it("address as expected", () => {
+      expect(account.getAddress()).toEqual(
+        "rMCcybKHfwCSkDHd3M36PAeUniEoygwjR3"
+      );
+    });
+    it("Account object to string as expected", () => {
+      const accountAsStr =
+        "002913 177673 352434 527196 002910 177672 352435 527190";
+      expect(`${account.toString()}`).toEqual(accountAsStr);
+    });
+  });
+
+  describe("Account based on existing secret", () => {
+    const secret = [
+      "084677",
+      "005323",
+      "580272",
+      "282388",
+      "626800",
+      "105300",
+      "560913",
+      "071783",
+    ];
+
+    const account = new Account(secret);
+
+    it("familySeed as expected", () => {
+      expect(account.getFamilySeed()).toEqual("sswpWwri7Y11dNCSmXdphgcoPZk3y");
+    });
+    it("publicKey as expected", () => {
+      const pubkey =
+        "020526A0EDC9123F7FBB7588402518B80FCD2C8D8AB4C45F5A68A2F220098EA06F";
+      expect(account.getKeypair().publicKey).toEqual(pubkey);
+    });
+    it("privateKey as expected", () => {
+      const privkey =
+        "005122B2127B4635FEE7D242FA6EC9B02B611C04494D0D7D49764374D90C8BC8D3";
+      expect(account.getKeypair().privateKey).toEqual(privkey);
+    });
+    it("address as expected", () => {
+      expect(account.getAddress()).toEqual(
+        "rfqJsRLLmr7wdWnEzW1mP6AVaPSdzmso9Z"
+      );
+    });
+    it("Account object to string as expected", () => {
+      const accountAsStr =
+        "084677 005323 580272 282388 626800 105300 560913 071783";
+      expect(`${account.toString()}`).toEqual(accountAsStr);
+    });
+  });
+
+  describe("Checksum error", () => {
+    const secret = [
+      "084677",
+      "005324",
+      "580272",
+      "626800",
+      "282388",
+      "105300",
+      "560913",
+      "071783",
+    ];
+    it("Should throw an Checksum Error", () => {
+      expect(() => {
+        // eslint-disable-next-line no-new -- Don't want unused variable
+        new Account(secret);
+      })
+        // TODO: Remove if jest is removed.
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- Jest and Jasmine have two different signatures.
+        // @ts-expect-error
+        .toThrowError(Error, "Invalid secret part: checksum invalid");
+    });
+  });
+});
