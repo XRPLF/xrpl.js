68269bf381c9ae53884b5ac1cc52e201879003f9
Caleb Kniffen <ckniffen@ripple.com>
2023-08-31 19:48:39 -0500

    feat: add `xrpl-secret-numbers` to the monorepo (#2445)
    - Add `xrpl-secret-numbers` by @WietseWind  to the mono repo.
    - `unpkg` and `jsdelivr` support was simplified by adding fields to package.json.
    - Unit tests run in a browser and node.
    
    BREAKING CHANGES:
    - `xrpl-secret-numbers` is now `@xrplf/secret-numbers`.
    - Changed the bundled file produced changed from  `dist/browerified.js` to `build/xrplf-secret-numbers-latest.js`.
    - Bundle variable is `xrplf_secret_numbers` instead of using browserify's loader.
    - If using CDN instructions in README will need to update to `https://cdn.jsdelivr.net/npm/@xrplf/secret-numbers`
    
    ### Context of Change
    
    Making this library part of the monorepo was discussed in #1788.  This solves several problems with the upcoming `xrpl.js@3.0`.
    
    #### Release Process
    `xrpl-secret-numbers` has a dependency of `ripple-keypairs` and `xrpl` has a depenendecy on `xrpl-secret-numbers`.  This means that any change would require a separate release of `ripple-keypairs`, then a release of `xrpl-secret-numbers`, followed by `xrpl`.  Now they can be released all at once
    
    #### Fewer Frontend Runtime Dependencies
    In 3.0 we eliminated the need for many polyfills like `util`, `assert`, and soon to be `buffer` (after noble libs PR is merged).  `xrpl-secret-numbers` still needs those.  This will also eliminate them and anytime similar changes in the future will be much easier.  This further reduces the bundle size of 3.0 branch by over 10% as well as reducing bundler setup.

---
+++ packages/xrpl/webpack.base.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const webpack = require('webpack')
+const { merge } = require('webpack-merge')
+const { getDefaultConfiguration } = require('../../webpack.config')
+
+module.exports = merge(getDefaultConfiguration(), {
+  entry: './dist/npm/index.js',
+  // overriding the output path and filename
+  output: {
+    library: 'xrpl',
+    path: path.join(__dirname, 'build/'),
+    filename: `xrpl.default.js`,
+  },
+  plugins: [
+    new webpack.NormalModuleReplacementPlugin(/^ws$/, './WSWrapper'),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^\.\/wordlists\/(?!english)/,
+      contextRegExp: /bip39\/src$/,
+    }),
+  ],
+  resolve: {
+    alias: {
+      ws: './dist/npm/client/WSWrapper.js',
+    },
+  },
+})
