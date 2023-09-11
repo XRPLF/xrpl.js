52a82931b88d434ff726bfbfd9ac482cccd12feb
Omar Khan <khancodegt@gmail.com>
2023-09-11 15:02:17 -0400

    add AMMVote test

---
+++ packages/xrpl/test/integration/transactions/ammVote.test.ts
@@ -0,0 +1,124 @@
+import { assert } from 'chai'
+import { AMMDeposit, AMMDepositFlags, AMMVote } from 'xrpl'
+
+import { AMMInfoResponse, Wallet } from '../../../src'
+import serverUrl from '../serverUrl'
+import {
+  setupClient,
+  teardownClient,
+  type XrplIntegrationTestContext,
+} from '../setup'
+import { generateFundedWallet, setupAMMPool, testTransaction } from '../utils'
+
+// how long before each test case times out
+const TIMEOUT = 20000
+
+describe('AMMVote', function () {
+  let testContext: XrplIntegrationTestContext
+  let wallet: Wallet
+  let wallet2: Wallet
+  let wallet3: Wallet
+  let currencyCode: string
+
+  beforeAll(async () => {
+    testContext = await setupClient(serverUrl)
+    wallet = testContext.wallet
+    wallet2 = await generateFundedWallet(testContext.client)
+    wallet3 = await generateFundedWallet(testContext.client)
+    currencyCode = 'USD'
+
+    await setupAMMPool(testContext.client, wallet, wallet2, currencyCode)
+  })
+  afterAll(async () => teardownClient(testContext))
+
+  it(
+    'vote',
+    async function () {
+      const ammDepositTx: AMMDeposit = {
+        TransactionType: 'AMMDeposit',
+        Account: wallet3.classicAddress,
+        Asset: {
+          currency: 'XRP',
+        },
+        Asset2: {
+          currency: currencyCode,
+          issuer: wallet2.classicAddress,
+        },
+        Amount: '1000',
+        Flags: AMMDepositFlags.tfSingleAsset,
+      }
+
+      // @ts-expect-error: ammDepositTx is valid
+      await testTransaction(testContext.client, ammDepositTx, wallet3)
+
+      const preAmmInfoRes: AMMInfoResponse = await testContext.client.request({
+        command: 'amm_info',
+        asset: {
+          currency: 'XRP',
+        },
+        asset2: {
+          currency: currencyCode,
+          issuer: wallet2.classicAddress,
+        },
+      })
+
+      const { amm: preAmm } = preAmmInfoRes.result
+      const {
+        auction_slot: preAuctionSlot,
+        trading_fee: preTradingFee,
+        vote_slots: preVoteSlots,
+      } = preAmm
+      if (preAuctionSlot === undefined) {
+        throw new Error('preAuctionSlot should not be undefined')
+      }
+      const { discounted_fee: preDiscountedFee } = preAuctionSlot
+      if (preVoteSlots === undefined) {
+        throw new Error('preVoteSlots should not be undefined')
+      }
+
+      const ammVoteTx: AMMVote = {
+        TransactionType: 'AMMVote',
+        Account: wallet3.classicAddress,
+        Asset: {
+          currency: 'XRP',
+        },
+        Asset2: {
+          currency: currencyCode,
+          issuer: wallet2.classicAddress,
+        },
+        TradingFee: 150,
+      }
+
+      // @ts-expect-error: ammVoteTx is valid
+      await testTransaction(testContext.client, ammVoteTx, wallet3)
+
+      const ammInfoRes: AMMInfoResponse = await testContext.client.request({
+        command: 'amm_info',
+        asset: {
+          currency: 'XRP',
+        },
+        asset2: {
+          currency: currencyCode,
+          issuer: wallet2.classicAddress,
+        },
+      })
+
+      const { amm } = ammInfoRes.result
+      const { auction_slot, trading_fee, vote_slots } = amm
+
+      assert.equal(trading_fee > preTradingFee, true)
+
+      if (auction_slot === undefined) {
+        throw new Error('auction_slot should not be undefined')
+      }
+      const { discounted_fee } = auction_slot
+      assert.equal(discounted_fee > preDiscountedFee, true)
+
+      if (vote_slots === undefined) {
+        throw new Error('vote_slots should not be undefined')
+      }
+      assert.equal(vote_slots.length - preVoteSlots.length, 1)
+    },
+    TIMEOUT,
+  )
+})
