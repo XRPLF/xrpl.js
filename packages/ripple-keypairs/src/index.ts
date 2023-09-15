226d67a78c59369072b592579e804c64184732a1
Nicholas Dudfield <ndudfield@gmail.com>
2023-09-15 09:04:07 +0700

    feat: tighten up/document getAlgorithmFromKey

--- packages/ripple-keypairs/src/index.ts
+++ packages/ripple-keypairs/src/index.ts
@@ -149,11 +149,43 @@ function deriveKeypair(
   return keypair
 }
 
+/**
+ * Determines the algorithm associated with a given key (public/private).
+ *
+ * | Curve     | Type        | Prefix | Length | Description                                           | Algorithm       |
+ * |-----------|-------------|:------:|:------:|-------------------------------------------------------|----------------:|
+ * | ed25519   | Private     |  0xED  |   33   | prefix + Uint256LE (0 < n < order )                   |         ed25519 |
+ * | ed25519   | Public      |  0xED  |   33   | prefix + 32 y-bytes                                   |         ed25519 |
+ * | secp256k1 | Public (1)  |  0x02  |   33   | prefix + 32 x-bytes                                   | ecdsa-secp256k1 |
+ * | secp256k1 | Public (2)  |  0x03  |   33   | prefix + 32 x-bytes (y is odd)                        | ecdsa-secp256k1 |
+ * | secp256k1 | Public (3)  |  0x04  |   65   | prefix + 32 x-bytes + 32 y-bytes                      | ecdsa-secp256k1 |
+ * | secp256k1 | Private (1) |  None  |   32   | Uint256BE (0 < n < order)                             | ecdsa-secp256k1 |
+ * | secp256k1 | Private (2) |  0x00  |   33   | prefix + Uint256BE (0 < n < order)                    | ecdsa-secp256k1 |
+ *
+ * Note: The 0x00 prefix for secpk256k1 Private (2) essentially 0 pads the number
+ *       and the interpreted number is the same as 32 bytes.
+ *
+ * @param key - hexadecimal string representation of the key.
+ * @returns {'ed25519' | 'ecdsa-secp256k1'} algorithm for signing/verifying
+ * @throws Error when key is invalid
+ */
 function getAlgorithmFromKey(key: HexString): 'ed25519' | 'ecdsa-secp256k1' {
   const bytes = hexToBytes(key)
-  return bytes.length === 33 && bytes[0] === 0xed
-    ? 'ed25519'
-    : 'ecdsa-secp256k1'
+  const tag = bytes[0]
+  const len = bytes.length
+
+  switch (true) {
+    case len === 32:
+      return 'ecdsa-secp256k1'
+    case len === 33 && tag === 0xed:
+      return 'ed25519'
+    case len === 33 && (tag === 0x02 || tag === 0x03 || tag === 0x00):
+      return 'ecdsa-secp256k1'
+    case len === 65 && tag === 0x04:
+      return 'ecdsa-secp256k1'
+    default:
+      throw new Error('invalid key format')
+  }
 }
 
 function sign(messageHex: HexString, privateKey: HexString): string {
