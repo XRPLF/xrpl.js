a832d14471a1b4e4f16d0e4c4b7650d4c9687fa2
Caleb Kniffen <ckniffen@ripple.com>
2023-09-19 14:57:34 -0500

    test: run address-codec tests in the browser (#2466)
    Update tests to use jasmine compatible functions.
    
    This means changing `test` to `it`, `toStrictEqual` to `toEqual` (which
    is still strict), `toThrowError` to `toError`, and updating the param
    for toError to pass an `Error` object.
    
    Remove the need to specify --single-run.

--- packages/secret-numbers/karma.config.js
+++ packages/secret-numbers/karma.config.js
@@ -1,39 +1,14 @@
 const webpackConfig = require("./test/webpack.config");
+const baseKarmaConfig = require("../../karma.config");
 delete webpackConfig.entry;
 
 module.exports = function (config) {
   config.set({
-    plugins: ["karma-webpack", "karma-jasmine", "karma-chrome-launcher"],
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: "",
-
     webpack: webpackConfig,
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["jasmine"],
 
     // list of files / patterns to load in the browser
     files: ["build/xrplf-secret-numbers-latest.js", "test/*.test.ts"],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      // Use webpack to bundle our test files
-      "test/*.test.ts": ["webpack"],
-    },
-
-    browsers: ["ChromeHeadless"],
-    // runs only one browser at a time
-    concurrency: 1,
-    // CI mode
-    singleRun: true,
-    client: {
-      jasmine: {
-        // ensures that tests are run in order instead of a random order
-        random: false,
-      },
-    },
   });
+
+  baseKarmaConfig(config);
 };
