5ff5e60d600b498da352fc5cd0c1ffbab5794a1f
Caleb Kniffen <ckniffen@ripple.com>
2023-09-20 10:49:33 -0500

    incorporate cleanup from #2466
    - Remove `--force`
    - Normalize `clean` command
    - Simplify package level karma and webpack configs

--- packages/isomorphic/karma.config.js
+++ packages/isomorphic/karma.config.js
@@ -1,38 +1,15 @@
-const webpackConfig = require("./test/webpack.config");
-delete webpackConfig.entry;
+const baseKarmaConfig = require('../../karma.config')
+const webpackConfig = require('./test/webpack.config')
+delete webpackConfig.entry
 
 module.exports = function (config) {
+  baseKarmaConfig(config)
+
   config.set({
-    plugins: ["karma-webpack", "karma-jasmine", "karma-chrome-launcher"],
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: "",
-
+    base: '',
     webpack: webpackConfig,
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ["jasmine"],
-
     // list of files / patterns to load in the browser
-    files: ["test/*.test.ts"],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      // Use webpack to bundle our test files
-      "test/*.test.ts": ["webpack"],
-    },
-    browsers: ["ChromeHeadless"],
-    // runs only one browser at a time
-    concurrency: 1,
-    // CI mode
-    singleRun: true,
-    client: {
-      jasmine: {
-        // ensures that tests are run in order instead of a random order
-        random: false,
-      },
-    },
-  });
-};
+    files: ['test/**/*.test.ts'],
+  })
+}
--- packages/isomorphic/test/webpack.config.js
+++ packages/isomorphic/test/webpack.config.js
@@ -1,65 +1,9 @@
-"use strict";
-const assert = require("assert");
-const path = require("path");
-const webpack = require("webpack");
+'use strict'
+const { merge } = require('webpack-merge')
+const { webpackForTest } = require('../../../weback.test.config')
+const { getDefaultConfiguration } = require('../../../webpack.config')
 
-function webpackForTest(testFileName) {
-  const match = testFileName.match(/\/?([^\/]*)\.ts$/);
-  if (!match) {
-    assert(false, "wrong filename:" + testFileName);
-  }
-
-  const test = {
-    mode: "production",
-    cache: true,
-    externals: [
-      {
-        net: "null", // net is used in one spot in only the tests
-      },
-    ],
-    entry: testFileName,
-    output: {
-      library: match[1].replace(/-/g, "_"),
-      path: path.join(__dirname, "./testCompiledForWeb/"),
-      filename: match[1] + ".js",
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        "process.stdout": {},
-      }),
-    ],
-    module: {
-      rules: [
-        // Compile compile the tests
-        {
-          test: /\.ts$/,
-          use: [
-            {
-              loader: "ts-loader",
-              options: {
-                compilerOptions: {
-                  lib: ["esnext", "dom"],
-                  composite: false,
-                  declaration: false,
-                  declarationMap: false,
-                },
-              },
-            },
-          ],
-        },
-      ],
-    },
-    node: {
-      global: true,
-      __filename: false,
-      __dirname: true,
-    },
-    resolve: {
-      extensions: [".ts", ".js", ".json"],
-    },
-  };
-
-  return test;
-}
-
-module.exports = webpackForTest("./test/index.ts");
+module.exports = merge(
+  getDefaultConfiguration(),
+  webpackForTest('./test/index.ts', __dirname),
+)
