d112e9a3d36c6f1e0260aa28b8766ad00b8f0923
Nicholas Dudfield <ndudfield@gmail.com>
2023-09-21 11:37:48 +0700

    feat(isomorphic): add TODO

--- packages/isomorphic/src/utils/index.ts
+++ packages/isomorphic/src/utils/index.ts
@@ -2,17 +2,34 @@ import { randomBytes as cryptoRandomBytes } from 'crypto'
 import { Utils } from './types'
 
 /**
- * Converts a Node.js Buffer to a Uint8Array for strict uniformity with browser implementations.
+ * Converts a Node.js Buffer to a Uint8Array for uniform behavior with browser implementations.
  *
- * Although a Buffer is a subclass of Uint8Array in Node.js, this function ensures an explicit
- * representation as a Uint8Array without copying underlying bytes, offering an efficient conversion.
+ * Choices:
+ * 1. Using `new Uint8Array(buffer)` or `Uint8Array.from(buffer)` (Chosen Method):
+ *    - Operation: Copies the buffer's data into a new Uint8Array.
+ *    - Pros: Ensures data isolation; memory-safe.
+ *    - Cons: Less memory-efficient due to data duplication.
+ *
+ * 2. Using `buf.buffer.slice(buf.byteOffset, buf.byteOffset + buf.byteLength)`:
+ *    - Operation: Shares memory between Buffer and Uint8Array.
+ *    - Pros: Memory-efficient.
+ *    - Cons: Risks with shared memory and potential for invalid references.
+ *
+ * 3. Directly returning the Buffer:
+ *    - Operation: Return Buffer as is (a Buffer *IS* an instanceof Uint8Array).
+ *    - Pros: Most memory and performance efficient.
+ *    - Cons: Violates strict Uint8Array typing and may introduce bugs if Buffer-specific features are abused.
+ *
+ * The chosen method (1) prioritizes safety and predictability by copying the buffer's data.
  *
  * @param {Buffer} buffer - The Node.js Buffer to convert.
- * @returns {Uint8Array} Resulting Uint8Array sharing the same memory as the Buffer.
+ * @returns {Uint8Array} Resulting Uint8Array with data copied from the Buffer.
  */
-function normalize(buffer: Buffer): Uint8Array {
-  // different copying semantics to Uint8Array.from
-  return new Uint8Array(buffer)
+function toUint8Array(buffer: Buffer): Uint8Array {
+  // TODO: are there /actually/ risks with option 2?
+  //       why not just option 3? make sure non TS users
+  //       don't shoot themselves in the foot ?
+  return Uint8Array.from(buffer)
 }
 
 const utils: Utils = {
@@ -21,10 +38,10 @@ const utils: Utils = {
     return buf.toString('hex').toUpperCase()
   },
   hexToBytes(hex) {
-    return normalize(Buffer.from(hex, 'hex'))
+    return toUint8Array(Buffer.from(hex, 'hex'))
   },
   randomBytes(size) {
-    return normalize(cryptoRandomBytes(size))
+    return toUint8Array(cryptoRandomBytes(size))
   },
 }
 
