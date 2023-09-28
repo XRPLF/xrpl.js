28cb6598b361c317ceaeaf44aebd40a49090fef1
Mayukha Vadari <mvadari@gmail.com>
2023-09-28 14:51:12 -0400

    write generateModels script

--- packages/xrpl/tools/createValidateTests.js
+++ packages/xrpl/tools/createValidateTests.js
@@ -1,120 +1,126 @@
-const fs = require("fs");
-const fixtures = require("../packages/ripple-binary-codec/test/fixtures/codec-fixtures.json");
+const fs = require('fs')
+const path = require('path')
+const fixtures = require('../../ripple-binary-codec/test/fixtures/codec-fixtures.json')
 
-const NORMAL_TYPES = ["number", "string"];
-const NUMBERS = ["0", "1"];
+const NORMAL_TYPES = ['number', 'string']
+const NUMBERS = ['0', '1']
 
 function getTx(txName) {
-  transactions = fixtures.transactions;
+  transactions = fixtures.transactions
   const validTxs = fixtures.transactions
     .filter((tx) => tx.json.TransactionType === txName)
-    .map((tx) => tx.json);
-  const validTx = validTxs[0];
-  delete validTx.TxnSignature;
-  delete validTx.SigningPubKey;
-  return JSON.stringify(validTx, null, 2);
+    .map((tx) => tx.json)
+  if (validTxs.length == 0) {
+    throw new Error('Must have ripple-binary-codec fixture')
+  }
+  const validTx = validTxs[0]
+  delete validTx.TxnSignature
+  delete validTx.SigningPubKey
+  return JSON.stringify(validTx, null, 2)
 }
 
-function main() {
-  const modelName = process.argv[2];
-  const filename = `./packages/xrpl/src/models/transactions/${modelName}.ts`;
-  const [model, txName] = getModel(filename);
-  return processModel(model, txName);
+function main(modelName) {
+  const filename = path.join(
+    path.dirname(__filename),
+    `../src/models/transactions/${modelName}.ts`,
+  )
+  const [model, txName] = getModel(filename)
+  return processModel(model, txName)
 }
 
 // Extract just the model from the file
 function getModel(filename) {
-  let model = "";
-  let started = false;
-  let ended = false;
-  const data = fs.readFileSync(filename, "utf8");
-  const lines = data.split("\n");
+  let model = ''
+  let started = false
+  let ended = false
+  const data = fs.readFileSync(filename, 'utf8')
+  const lines = data.split('\n')
   for (let line of lines) {
     if (ended) {
-      continue;
+      continue
     }
-    if (!started && !line.startsWith("export")) {
-      continue;
+    if (!started && !line.startsWith('export')) {
+      continue
     }
-    if (!started && line.includes("Flags")) {
-      continue;
+    if (!started && line.includes('Flags')) {
+      continue
     }
     if (!started) {
-      started = true;
+      started = true
     }
-    model += line + "\n";
-    if (line === "}") {
-      ended = true;
+    model += line + '\n'
+    if (line === '}') {
+      ended = true
     }
   }
-  const name_line = model.split("\n")[0].split(" ");
-  const txName = name_line[2];
-  return [model, txName];
+  const name_line = model.split('\n')[0].split(' ')
+  const txName = name_line[2]
+  return [model, txName]
 }
 
 function getInvalidValue(paramTypes) {
   if (paramTypes.length === 1) {
-    const paramType = paramTypes[0];
-    if (paramType == "number") {
-      return "'number'";
-    } else if (paramType == "string") {
-      return 123;
-    } else if (paramType == "IssuedCurrency") {
-      return JSON.stringify({ test: "test" });
-    } else if (paramType == "Amount") {
-      return JSON.stringify({ currency: "ETH" });
-    } else if (paramType == "XChainBridge") {
-      return JSON.stringify({ XChainDoor: "test" });
+    const paramType = paramTypes[0]
+    if (paramType == 'number') {
+      return "'number'"
+    } else if (paramType == 'string') {
+      return 123
+    } else if (paramType == 'IssuedCurrency') {
+      return JSON.stringify({ test: 'test' })
+    } else if (paramType == 'Amount') {
+      return JSON.stringify({ currency: 'ETH' })
+    } else if (paramType == 'XChainBridge') {
+      return JSON.stringify({ XChainDoor: 'test' })
     } else {
-      throw Error(`${paramType} not supported yet`);
+      throw Error(`${paramType} not supported yet`)
     }
   }
 
   const simplifiedParamTypes = paramTypes.filter(
-    (_paramType, index) => index % 2 == 0
-  );
+    (_paramType, index) => index % 2 == 0,
+  )
   if (JSON.stringify(simplifiedParamTypes) === '["0","1"]') {
-    return 2;
+    return 2
   } else if (JSON.stringify(simplifiedParamTypes) === '["number","string"]') {
-    return JSON.stringify({ currency: "ETH" });
+    return JSON.stringify({ currency: 'ETH' })
   } else {
-    throw Error(`${simplifiedParamTypes} not supported yet`);
+    throw Error(`${simplifiedParamTypes} not supported yet`)
   }
 }
 
 // Process the model and build the tests
 
 function processModel(model, txName) {
-  let output = "";
-  for (let line of model.split("\n")) {
-    if (line == "") {
-      continue;
+  let output = ''
+  for (let line of model.split('\n')) {
+    if (line == '') {
+      continue
     }
-    if (line.startsWith("export")) {
-      continue;
+    if (line.startsWith('export')) {
+      continue
     }
-    if (line == "}") {
-      continue;
+    if (line == '}') {
+      continue
     }
-    line = line.trim();
-    if (line.startsWith("TransactionType")) {
-      continue;
+    line = line.trim()
+    if (line.startsWith('TransactionType')) {
+      continue
     }
-    if (line.startsWith("Flags")) {
+    if (line.startsWith('Flags')) {
       // TODO: support flag checking
-      continue;
+      continue
     }
-    if (line.startsWith("/**")) {
-      continue;
+    if (line.startsWith('/**')) {
+      continue
     }
-    if (line.startsWith("*")) {
-      continue;
+    if (line.startsWith('*')) {
+      continue
     }
 
-    const split = line.split(" ");
-    const param = split[0].replace("?:", "").replace(":", "").trim();
-    const paramTypes = split.slice(1);
-    const optional = split[0].endsWith("?:");
+    const split = line.split(' ')
+    const param = split[0].replace('?:', '').replace(':', '').trim()
+    const paramTypes = split.slice(1)
+    const optional = split[0].endsWith('?:')
 
     if (!optional) {
       output += `  it("throws w/ missing ${param}", function () {
@@ -132,10 +138,10 @@ function processModel(model, txName) {
     )
   })
 
-`;
+`
     }
 
-    const fakeValue = getInvalidValue(paramTypes);
+    const fakeValue = getInvalidValue(paramTypes)
     output += `  it('throws w/ invalid ${param}', function () {
     tx.${param} = ${fakeValue}
 
@@ -151,11 +157,11 @@ function processModel(model, txName) {
     )
   })
 
-`;
+`
   }
 
-  output = output.substring(0, output.length - 2);
-  output += "\n})\n";
+  output = output.substring(0, output.length - 2)
+  output += '\n})\n'
   output =
     `import { assert } from 'chai'
 
@@ -179,9 +185,13 @@ describe('${txName}', function () {
     assert.doesNotThrow(() => validate(tx))
   })
 
-` + output;
+` + output
 
-  return output;
+  return output
 }
 
-console.log(main());
+if (require.main === module) {
+  console.log(main(process.argv[2]))
+}
+
+module.exports = main
---
+++ packages/xrpl/tools/generateModels.js
@@ -0,0 +1,190 @@
+const fs = require('fs')
+const path = require('path')
+const createValidate = require('./createValidate')
+const createValidateTests = require('./createValidateTests')
+
+const folder = '../rippled-all/did'
+
+function readFile(filename) {
+  return fs.readFileSync(filename, 'utf-8')
+}
+
+const sfieldCpp = readFile(
+  path.join(folder, 'src/ripple/protocol/impl/SField.cpp'),
+)
+const sfieldHits = sfieldCpp.match(
+  /^ *CONSTRUCT_[^\_]+_SFIELD *\( *[^,\n]*,[ \n]*"([^\"\n ]+)"[ \n]*,[ \n]*([^, \n]+)[ \n]*,[ \n]*([0-9]+)(,.*?(notSigning))?/gm,
+)
+const sfields = {}
+for (const hit of sfieldHits) {
+  const matches = hit.match(
+    /^ *CONSTRUCT_[^\_]+_SFIELD *\( *[^,\n]*,[ \n]*"([^\"\n ]+)"[ \n]*,[ \n]*([^, \n]+)[ \n]*,[ \n]*([0-9]+)(,.*?(notSigning))?/,
+  )
+  sfields[matches[1]] = matches.slice(2)
+}
+
+const txFormatsCpp = readFile(
+  path.join(folder, 'src/ripple/protocol/impl/TxFormats.cpp'),
+)
+const txFormatsHits = txFormatsCpp.match(
+  /^ *add\(jss::([^\"\n, ]+),[ \n]*tt[A-Z_]+,[ \n]*{[ \n]*(({sf[A-Za-z0-9]+, soe(OPTIONAL|REQUIRED|DEFAULT)},[ \n]+)*)},[ \n]*[pseudocC]+ommonFields\);/gm,
+)
+const txFormats = {}
+for (const hit of txFormatsHits) {
+  const matches = hit.match(
+    /^ *add\(jss::([^\"\n, ]+),[ \n]*tt[A-Z_]+,[ \n]*{[ \n]*(({sf[A-Za-z0-9]+, soe(OPTIONAL|REQUIRED|DEFAULT)},[ \n]+)*)},[ \n]*[pseudocC]+ommonFields\);/,
+  )
+  txFormats[matches[1]] = formatTxFormat(matches[2])
+}
+
+const jsTransactionFile = readFile(
+  path.join(
+    path.dirname(__filename),
+    '../src/models/transactions/transaction.ts',
+  ),
+)
+const transactionMatch = jsTransactionFile.match(
+  /export type Transaction =([| \nA-Za-z]+)/,
+)[0]
+const existingLibraryTxs = transactionMatch
+  .split('\n  | ')
+  .filter((value) => !value.includes('export type'))
+  .map((value) => value.trim())
+existingLibraryTxs.push('EnableAmendment', 'SetFee', 'UNLModify')
+
+function formatTxFormat(rawTxFormat) {
+  return rawTxFormat
+    .trim()
+    .split('\n')
+    .map((element) => element.trim().replace(/[{},]/g, '').split(' '))
+}
+
+const txsToAdd = []
+
+for (const tx in txFormats) {
+  if (!existingLibraryTxs.includes(tx)) {
+    txsToAdd.push(tx)
+  }
+}
+
+console.log(txsToAdd)
+
+// for (const [tx, name] of txsToAdd) {
+//   fs.appendFileSync(
+//     '../src/models/transactions/types/transaction_type.py',
+//     `    ${name.toUpperCase()} = "${tx}"\n`,
+//   )
+// }
+
+const typeMap = {
+  UINT8: 'number',
+  UINT16: 'number',
+  UINT32: 'number',
+  UINT64: 'number | string',
+  UINT128: 'string',
+  UINT160: 'string',
+  UINT256: 'string',
+  AMOUNT: 'Amount',
+  VL: 'string',
+  ACCOUNT: 'string',
+  VECTOR256: 'string[]',
+  PATHSET: 'Path[]',
+  ISSUE: 'Currency',
+  XCHAIN_BRIDGE: 'XChainBridge',
+  OBJECT: 'any',
+  ARRAY: 'any[]',
+}
+
+const allCommonImports = ['Amount', 'Currency', 'Path', 'XChainBridge']
+const additionalValidationImports = ['string', 'number']
+
+function generateParamLine(param, isRequired) {
+  const paramName = param.slice(2)
+  const paramType = sfields[paramName][0]
+  const paramTypeOutput = typeMap[paramType]
+  return `  ${paramName}${isRequired ? '' : '?'}: ${paramTypeOutput}\n`
+}
+
+async function main() {
+  txsToAdd.forEach(async (tx) => {
+    const txFormat = txFormats[tx]
+    const paramLines = txFormat
+      .filter((param) => param[0] !== '')
+      .sort((a, b) => a[0].localeCompare(b[0]))
+      .map((param) => generateParamLine(param[0], param[1] === 'soeREQUIRED'))
+    paramLines.sort((a, b) => !a.includes('REQUIRED'))
+    const params = paramLines.join('\n')
+    let model = `/**
+ * @category Transaction Models
+ */
+export interface ${tx} extends BaseTransaction {
+  TransactionType: '${tx}'
+
+${params}
+}`
+
+    const commonImports = []
+    const validationImports = ['BaseTransaction', 'validateBaseTransaction']
+    for (const item of allCommonImports) {
+      if (params.includes(item)) {
+        commonImports.push(item)
+        validationImports.push('is' + item)
+      }
+    }
+    for (const item of additionalValidationImports) {
+      if (params.includes(item)) {
+        validationImports.push(
+          'is' + item.substring(0, 1).toUpperCase() + item.substring(1),
+        )
+      }
+    }
+    if (params.includes('?')) {
+      validationImports.push('validateOptionalField')
+    }
+    if (/[A-Za-z0-9]+:/.test(params)) {
+      validationImports.push('validateRequiredField')
+    }
+    validationImports.sort()
+    const commonImportLine =
+      commonImports.length > 0
+        ? `import { ${commonImports.join(', ')} } from '../common'`
+        : ''
+    const validationImportLine = `import { ${validationImports.join(
+      ', ',
+    )} } from './common'`
+    let imported_models = `${commonImportLine}
+
+${validationImportLine}`
+    imported_models = imported_models.replace('\n\n\n\n', '\n\n')
+    imported_models = imported_models.replace('\n\n\n', '\n\n')
+    model = model.replace('\n\n\n\n', '\n\n')
+    fs.writeFileSync(
+      path.join(
+        path.dirname(__filename),
+        `../src/models/transactions/${tx}.ts`,
+      ),
+      imported_models + '\n\n' + model,
+    )
+
+    const validate = await createValidate(tx)
+    fs.appendFileSync(
+      path.join(
+        path.dirname(__filename),
+        `../src/models/transactions/${tx}.ts`,
+      ),
+      '\n\n' + validate,
+    )
+
+    const validateTests = createValidateTests(tx)
+    fs.writeFileSync(
+      path.join(path.dirname(__filename), `../test/models/${tx}.test.ts`),
+      validateTests,
+    )
+
+    console.log('Added ' + tx)
+  })
+}
+
+if (require.main === module) {
+  main()
+}
