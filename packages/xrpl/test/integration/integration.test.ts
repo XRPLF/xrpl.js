4c61f542547bc962c5a59d83419e09cc572fe29e
Caleb Kniffen <ckniffen@ripple.com>
2023-11-06 15:34:13 -0600

    feat: remove `assert` library as a testing dep
    Right now we have 5 testing library dependencies. This eliminates
    `assert`. `jest`, `jasmine`, `chai` and `karma` are the remaining ones.

--- packages/xrpl/test/integration/integration.test.ts
+++ packages/xrpl/test/integration/integration.test.ts
@@ -1,4 +1,4 @@
-import assert from 'assert'
+import { assert } from 'chai'
 
 import { Client, SubmitResponse } from '../../src'
 import { AccountSet, SignerListSet } from '../../src/models/transactions'
