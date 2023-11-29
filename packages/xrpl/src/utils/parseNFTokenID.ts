e65b448fd2a973960a4a8ae624eba4f7232eafd4
Ildar Gumirov <ildar.gumirov@gmail.com>
2023-11-29 15:11:00 +0100

    fix linter

--- packages/xrpl/src/utils/parseNFTokenID.ts
+++ packages/xrpl/src/utils/parseNFTokenID.ts
@@ -24,12 +24,17 @@ import { XrplError } from '../errors'
  */
 function unscrambleTaxon(taxon: number, tokenSeq: number): number {
   /* eslint-disable no-bitwise -- XOR is part of the encode/decode scheme. */
-  const m = 384160001;
-  const c = 2459;
-  const max = 4294967296;
+  const m = 384160001
+  const c = 2459
+  const max = 4294967296
 
-  const scramble = new BigNumber(m).multipliedBy(tokenSeq).modulo(max).plus(c).modulo(max).toNumber();
-  return (taxon ^ scramble) >>> 0;
+  const scramble = new BigNumber(m)
+    .multipliedBy(tokenSeq)
+    .modulo(max)
+    .plus(c)
+    .modulo(max)
+    .toNumber()
+  return (taxon ^ scramble) >>> 0
   /* eslint-enable no-bitwise */
 }
 
