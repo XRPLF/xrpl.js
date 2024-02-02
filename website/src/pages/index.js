64f7b5f9046437afe3142fb47e0da36c88e6507d
jonathanlei <jonathanlei98@gmail.com>
2024-02-02 11:41:00 -0800

    change header and migration title

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -35,7 +35,7 @@ export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
+      title={`xrpl.js Reference Docs`}
       description="Description will go into a meta tag in <head />"
     >
       <HomepageHeader />
