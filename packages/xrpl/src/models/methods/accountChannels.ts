b7ae6e933a3dbcd33170f7fa3c7ddb5212620496
JST5000 <aim4math@gmail.com>
2024-02-07 19:13:37 -0500

    Lint

--- packages/xrpl/src/models/methods/accountChannels.ts
+++ packages/xrpl/src/models/methods/accountChannels.ts
@@ -11,18 +11,28 @@ export interface Channel {
   amount: string
 
   /**
-   * The total amount of XRP, in drops, paid out from this channel, as of the ledger version used.
-   * (You can calculate the amount of XRP left in the channel by subtracting balance from amount.)
+   * The total amount of XRP, in drops, paid out from this channel,
+   * as of the ledger version used. (You can calculate the amount of
+   * XRP left in the channel by subtracting balance from amount.)
    */
   balance: string
 
-  /** A unique ID for this channel, as a 64-character hexadecimal string. This is also the ID of the channel object in the ledger's state data. */
+  /**
+   * A unique ID for this channel, as a 64-character hexadecimal string.
+   * This is also the ID of the channel object in the ledger's state data.
+   */
   channel_id: string
 
-  /** The destination account of the channel, as an Address. Only this account can receive the XRP in the channel while it is open. */
+  /**
+   * The destination account of the channel, as an Address.
+   * Only this account can receive the XRP in the channel while it is open.
+   */
   destination_account: string
 
-  /** The number of seconds the payment channel must stay open after the owner of the channel requests to close it. */
+  /**
+   * The number of seconds the payment channel must stay open after the owner
+   * of the channel requests to close it.
+   */
   settle_delay: number
 
   /**
@@ -32,33 +42,38 @@ export interface Channel {
   public_key?: string
 
   /**
-   * The public key for the payment channel in hexadecimal format, if one was specified at channel creation.
-   * Signed claims against this channel must be redeemed with the matching key pair.
+   * The public key for the payment channel in hexadecimal format, if one was
+   * specified at channel creation. Signed claims against this channel must be
+   * redeemed with the matching key pair.
    */
   public_key_hex?: string
 
   /**
    * Time, in seconds since the Ripple Epoch, when this channel is set to expire.
-   * This expiration date is mutable. If this is before the close time of the most recent validated ledger, the channel is expired.
+   * This expiration date is mutable. If this is before the close time of the most
+   * recent validated ledger, the channel is expired.
    */
   expiration?: number
 
   /**
-   * Time, in seconds since the Ripple Epoch, of this channel's immutable expiration, if one was specified at channel creation.
-   * If this is before the close time of the most recent validated ledger, the channel is expired.
+   * Time, in seconds since the Ripple Epoch, of this channel's immutable expiration,
+   * if one was specified at channel creation. If this is before the close time of the
+   * most recent validated ledger, the channel is expired.
    */
   cancel_after?: number
 
   /**
-   * A 32-bit unsigned integer to use as a source tag for payments through this payment channel, if one was specified at channel creation.
-   * This indicates the payment channel's originator or other purpose at the source account.
-   * Conventionally, if you bounce payments from this channel, you should specify this value in the DestinationTag of the return payment.
+   * A 32-bit unsigned integer to use as a source tag for payments through this payment channel,
+   * if one was specified at channel creation. This indicates the payment channel's originator or
+   * other purpose at the source account. Conventionally, if you bounce payments from this channel,
+   * you should specify this value in the DestinationTag of the return payment.
    */
   source_tag?: number
 
   /**
-   * A 32-bit unsigned integer to use as a destination tag for payments through this channel, if one was specified at channel creation.
-   * This indicates the payment channel's beneficiary or other purpose at the destination account.
+   * A 32-bit unsigned integer to use as a destination tag for payments through this channel,
+   * if one was specified at channel creation. This indicates the payment channel's beneficiary
+   * or other purpose at the destination account.
    */
   destination_tag?: number
 }
