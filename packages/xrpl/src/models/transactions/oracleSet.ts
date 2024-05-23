3337d2146403dd098e6eb56de26ffa669e71510c
Omar Khan <khancodegt@gmail.com>
2024-05-23 14:19:58 -0400

    add input check for AssetPrice and Scale being either both present or excluded

--- packages/xrpl/src/models/transactions/oracleSet.ts
+++ packages/xrpl/src/models/transactions/oracleSet.ts
@@ -130,6 +130,22 @@ export function validateOracleSet(tx: Record<string, unknown>): void {
         )
       }
 
+      // Either AssetPrice and Scale are both present or both excluded
+      if (
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- we are validating the type
+        ('AssetPrice' in priceData.PriceData &&
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- we are validating the type
+          !('Scale' in priceData.PriceData)) ||
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- we are validating the type
+        ('Scale' in priceData.PriceData &&
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- we are validating the type
+          !('AssetPrice' in priceData.PriceData))
+      ) {
+        throw new ValidationError(
+          'OracleSet: PriceDataSeries must have both `AssetPrice` and `Scale` if any are present',
+        )
+      }
+
       if (
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access -- we are validating the type
         'AssetPrice' in priceData.PriceData &&
