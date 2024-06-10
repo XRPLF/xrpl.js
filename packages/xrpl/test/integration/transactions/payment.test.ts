aedab815822bc0586512977d473458a81c4baa19
Chenna Keshava B S <ckeshavabs@gmail.com>
2024-06-10 15:16:00 -0700

    integration tests pertaining to DeliverMax

--- packages/xrpl/test/integration/transactions/payment.test.ts
+++ packages/xrpl/test/integration/transactions/payment.test.ts
@@ -1,4 +1,8 @@
+import { assert } from 'chai'
+
 import { Payment } from '../../../src'
+// import { autofill } from '../../../src/client/index'
+import { ValidationError } from '../../../src/errors'
 import serverUrl from '../serverUrl'
 import {
   setupClient,
@@ -12,9 +16,21 @@ const TIMEOUT = 20000
 
 describe('Payment', function () {
   let testContext: XrplIntegrationTestContext
+  let payment_txn_example
+  let amount_: string
+  // This wallet is used for DeliverMax related tests
+  let payment_txn_wallet
 
   beforeEach(async () => {
     testContext = await setupClient(serverUrl)
+    amount_ = '200000000'
+    payment_txn_wallet = await generateFundedWallet(testContext.client)
+    payment_txn_example = {
+      TransactionType: 'Payment',
+      Account: payment_txn_wallet.classicAddress,
+      Amount: amount_,
+      Destination: 'rfkE1aSy9G8Upk4JssnwBxhEv5p4mn2KTy',
+    }
   })
   afterEach(async () => teardownClient(testContext))
 
@@ -29,6 +45,81 @@ describe('Payment', function () {
         Amount: '1000',
       }
       await testTransaction(testContext.client, tx, testContext.wallet)
+    },
+    TIMEOUT,
+  )
+
+  it(
+    'Validate Payment transaction v2 API: Payment Transaction: Specify Only Amount field',
+    async () => {
+      const result = await testTransaction(
+        testContext.client,
+        payment_txn_example,
+        payment_txn_wallet,
+      )
+
+      assert.equal(result.result.engine_result_code, 0)
+      assert.equal((result.result.tx_json as Payment).Amount, amount_)
+    },
+    TIMEOUT,
+  )
+
+  it(
+    'Validate Payment transaction v2 API: Payment Transaction: Specify Only DeliverMax field',
+    async () => {
+      const payment_txn = payment_txn_example
+      payment_txn.DeliverMax = payment_txn.Amount
+      delete payment_txn.Amount
+
+      const result = await testTransaction(
+        testContext.client,
+        payment_txn,
+        payment_txn_wallet,
+      )
+
+      assert.equal(result.result.engine_result_code, 0)
+      assert.equal((result.result.tx_json as Payment).Amount, amount_)
+    },
+    TIMEOUT,
+  )
+
+  it(
+    'Validate Payment transaction v2 API: Payment Transaction: identical DeliverMax and Amount fields',
+    async () => {
+      const payment_txn = payment_txn_example
+      payment_txn.DeliverMax = payment_txn.Amount
+
+      const result = await testTransaction(
+        testContext.client,
+        payment_txn,
+        payment_txn_wallet,
+      )
+
+      assert.equal(result.result.engine_result_code, 0)
+      assert.equal((result.result.tx_json as Payment).Amount, amount_)
+    },
+    TIMEOUT,
+  )
+
+  it(
+    'Validate Payment transaction v2 API: Payment Transaction: differing DeliverMax and Amount fields',
+    async () => {
+      const payment_txn = payment_txn_example
+      // Different from the Amount field
+      payment_txn.DeliverMax = '9999'
+
+      try {
+        await testTransaction(
+          testContext.client,
+          payment_txn,
+          payment_txn_wallet,
+        )
+      } catch (err) {
+        assert.equal(
+          (err as ValidationError).message,
+          'PaymentTransaction: Amount and DeliverMax fields must be identical when both are provided',
+        )
+      }
     },
     TIMEOUT,
   )
