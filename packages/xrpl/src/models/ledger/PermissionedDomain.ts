7f0baad8d87fd8a4eeaf547cdb3f18fb7bf97a79
Chenna Keshava B S <ckeshavabs@gmail.com>
2025-02-03 11:39:32 -0800

    add descriptions for the PD ledger object

--- packages/xrpl/src/models/ledger/PermissionedDomain.ts
+++ packages/xrpl/src/models/ledger/PermissionedDomain.ts
@@ -5,15 +5,21 @@ import { BaseLedgerEntry, HasPreviousTxnID } from './BaseLedgerEntry'
 export default interface PermissionedDomain
   extends BaseLedgerEntry,
     HasPreviousTxnID {
+  /*The ledger object's type (PermissionedDomain). */
   LedgerEntryType: 'PermissionedDomain'
 
+  /* The account that controls the settings of the domain. */
   Owner: string
 
+  /*  Flag values associated with this object. */
   Flags: 0
 
+  /* Owner account's directory page containing the PermissionedDomain object. */
   OwnerNode: string
 
+  /* The Sequence value of the PermissionedDomainSet transaction that created this domain. Used in combination with the Account to identify this domain. */
   Sequence: number
 
+  /* The credentials that are accepted by the domain. Ownership of one of these credentials automatically makes you a member of the domain. */
   AcceptedCredentials: AuthorizeCredential[]
 }
