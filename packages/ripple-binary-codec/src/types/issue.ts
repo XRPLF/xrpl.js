ea4f606fc6979f3e56d06f73fac96948f1170c50
achowdhry-ripple <achowdhry@ripple.com>
2025-03-19 15:19:06 -0400

    fix: add MPTAmount support in Issue model (#2919)
    * initial issue changes
    
    * finalized issue file
    
    * history update
    
    * separate issue types
    
    * all tests
    
    * fix text
    
    * cleanup logs
    
    * revert value prop test
    
    * test in pipeline
    
    * Revert "test in pipeline"
    
    This reverts commit 64ab7b047eb0ee12a9cf00ab617344e959b99a22.
    
    * fix test
    
    * fix

--- packages/ripple-binary-codec/src/types/issue.ts
+++ packages/ripple-binary-codec/src/types/issue.ts
@@ -4,24 +4,35 @@ import { BinaryParser } from '../serdes/binary-parser'
 import { AccountID } from './account-id'
 import { Currency } from './currency'
 import { JsonObject, SerializedType } from './serialized-type'
+import { Hash192 } from './hash-192'
 
-/**
- * Interface for JSON objects that represent amounts
- */
-interface IssueObject extends JsonObject {
+interface XRPIssue extends JsonObject {
   currency: string
-  issuer?: string
 }
 
+interface IOUIssue extends JsonObject {
+  currency: string
+  issuer: string
+}
+interface MPTIssue extends JsonObject {
+  mpt_issuance_id: string
+}
 /**
- * Type guard for AmountObject
+ * Interface for JSON objects that represent issues
+ */
+type IssueObject = XRPIssue | IOUIssue | MPTIssue
+
+/**
+ * Type guard for Issue Object
  */
 function isIssueObject(arg): arg is IssueObject {
   const keys = Object.keys(arg).sort()
-  if (keys.length === 1) {
-    return keys[0] === 'currency'
-  }
-  return keys.length === 2 && keys[0] === 'currency' && keys[1] === 'issuer'
+  const isXRP = keys.length === 1 && keys[0] === 'currency'
+  const isIOU =
+    keys.length === 2 && keys[0] === 'currency' && keys[1] === 'issuer'
+  const isMPT = keys.length === 1 && keys[0] === 'mpt_issuance_id'
+
+  return isXRP || isIOU || isMPT
 }
 
 /**
@@ -37,9 +48,9 @@ class Issue extends SerializedType {
   /**
    * Construct an amount from an IOU or string amount
    *
-   * @param value An Amount, object representing an IOU, or a string
+   * @param value An Amount, object representing an IOU, MPTAmount, or a string
    *     representing an integer amount
-   * @returns An Amount object
+   * @returns An Issue object
    */
   static from<T extends Issue | IssueObject>(value: T): Issue {
     if (value instanceof Issue) {
@@ -47,12 +58,26 @@ class Issue extends SerializedType {
     }
 
     if (isIssueObject(value)) {
-      const currency = Currency.from(value.currency).toBytes()
-      if (value.issuer == null) {
+      if (value.currency) {
+        const currency = Currency.from(value.currency.toString()).toBytes()
+
+        //IOU case
+        if (value.issuer) {
+          const issuer = AccountID.from(value.issuer.toString()).toBytes()
+          return new Issue(concat([currency, issuer]))
+        }
+
+        //XRP case
         return new Issue(currency)
       }
-      const issuer = AccountID.from(value.issuer).toBytes()
-      return new Issue(concat([currency, issuer]))
+
+      // MPT case
+      if (value.mpt_issuance_id) {
+        const mptIssuanceIdBytes = Hash192.from(
+          value.mpt_issuance_id.toString(),
+        ).toBytes()
+        return new Issue(mptIssuanceIdBytes)
+      }
     }
 
     throw new Error('Invalid type to construct an Amount')
@@ -62,9 +87,16 @@ class Issue extends SerializedType {
    * Read an amount from a BinaryParser
    *
    * @param parser BinaryParser to read the Amount from
-   * @returns An Amount object
+   * @param hint The number of bytes to consume from the parser.
+   * For an MPT amount, pass 24 (the fixed length for Hash192).
+   *
+   * @returns An Issue object
    */
-  static fromParser(parser: BinaryParser): Issue {
+  static fromParser(parser: BinaryParser, hint?: number): Issue {
+    if (hint === Hash192.width) {
+      const mptBytes = parser.read(Hash192.width)
+      return new Issue(mptBytes)
+    }
     const currency = parser.read(20)
     if (new Currency(currency).toJSON() === 'XRP') {
       return new Issue(currency)
@@ -79,7 +111,15 @@ class Issue extends SerializedType {
    * @returns the JSON interpretation of this.bytes
    */
   toJSON(): IssueObject {
+    // If the buffer is exactly 24 bytes, treat it as an MPT amount.
+    if (this.toBytes().length === Hash192.width) {
+      return {
+        mpt_issuance_id: this.toHex().toUpperCase(),
+      }
+    }
+
     const parser = new BinaryParser(this.toString())
+
     const currency = Currency.fromParser(parser) as Currency
     if (currency.toJSON() === 'XRP') {
       return { currency: currency.toJSON() }
