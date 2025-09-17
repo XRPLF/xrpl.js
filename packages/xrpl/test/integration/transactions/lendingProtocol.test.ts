73e3381a109d621bce5edd14bc583451835c5492
Raj Patel <rajp@ripple.com>
2025-09-16 20:51:18 -0400

    WIP commit

--- packages/xrpl/test/integration/transactions/lendingProtocol.test.ts
+++ packages/xrpl/test/integration/transactions/lendingProtocol.test.ts
@@ -1,5 +1,10 @@
-import { MPTokenIssuanceCreate, TxResponse, Wallet } from '../../../src'
-import { MPTokenIssuanceCreateMetadata } from '../../../src/models/transactions/MPTokenIssuanceCreate'
+import {
+  type MPTokenIssuanceCreate,
+  type TxResponse,
+  type VaultCreate,
+  Wallet,
+} from '../../../src'
+import { type MPTokenIssuanceCreateMetadata } from '../../../src/models/transactions/MPTokenIssuanceCreate'
 import serverUrl from '../serverUrl'
 import {
   setupClient,
@@ -25,7 +30,8 @@ describe('Lending Protocol IT', () => {
   it(
     'Successful LoanBroker creation and loan payment',
     async () => {
-      await createSingleAssetVault(testContext)
+      const vaultOwnerWallet = await generateFundedWallet(testContext.client)
+      await createSingleAssetVault(testContext, vaultOwnerWallet)
     },
     TIMEOUT,
   )
@@ -33,11 +39,28 @@ describe('Lending Protocol IT', () => {
 
 async function createSingleAssetVault(
   testContext: XrplIntegrationTestContext,
+  vaultOwnerWallet: Wallet,
 ): Promise<string> {
   const mptIssuerWallet = await generateFundedWallet(testContext.client)
+  const depositorWallet = await generateFundedWallet(testContext.client)
+
   const mptIssuanceId = await createMPToken(testContext, mptIssuerWallet)
 
-  // const vaultCreateTx: VaultCreate = {}
+  const vaultCreateTx: VaultCreate = {
+    TransactionType: 'VaultCreate',
+    Asset: {
+      mpt_issuance_id: mptIssuanceId,
+    },
+    Account: vaultOwnerWallet.address,
+    Fee: '10000',
+  }
+
+  const vaultCreateResp = await testTransaction(
+    testContext.client,
+    vaultCreateTx,
+    vaultOwnerWallet,
+  )
+
   return ''
 }
 
