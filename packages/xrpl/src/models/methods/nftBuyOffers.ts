cbfa92b151dbbdea83047f4f1a54d762fe5ffd35
Michael Pinter <mpinter@ripple.com>
2026-02-25 15:46:10 -0700

    fix(types): Add limit, marker to NFTBuyOffersRequest and NFTBuyOffersResponse

--- packages/xrpl/src/models/methods/nftBuyOffers.ts
+++ packages/xrpl/src/models/methods/nftBuyOffers.ts
@@ -9,12 +9,23 @@ import { BaseRequest, BaseResponse, LookupByLedgerRequest } from './baseMethod'
  * @category Requests
  */
 export interface NFTBuyOffersRequest
-  extends BaseRequest, LookupByLedgerRequest {
+  extends BaseRequest,
+    LookupByLedgerRequest {
   command: 'nft_buy_offers'
   /**
    * The unique identifier of an NFToken. The request returns buy offers for this NFToken.
    */
   nft_id: string
+  /**
+   * Limit the number of NFT buy offers to retrieve. The server may return
+   * fewer results. Valid values are within 50-500. The default is 250.
+   */
+  limit?: number
+  /**
+   * Value from a previous paginated response. Resume retrieving data where
+   * that response left off.
+   */
+  marker?: unknown
 }
 
 /**
@@ -32,5 +43,14 @@ export interface NFTBuyOffersResponse extends BaseResponse {
      * The token ID of the NFToken to which these offers pertain.
      */
     nft_id: string
+    /**
+     * The limit value used in the request.
+     */
+    limit?: number
+    /**
+     * Server-defined value indicating the response is paginated. Pass this to
+     * the next call to resume where this call left off.
+     */
+    marker?: unknown
   }
 }
