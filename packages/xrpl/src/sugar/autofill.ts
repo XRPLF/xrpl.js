d028385f28cc7caa0b0755abeb7a855dde320c7e
Mayukha Vadari <mvadari@gmail.com>
2026-02-25 17:01:46 -0500

    fix autofill error type

--- packages/xrpl/src/sugar/autofill.ts
+++ packages/xrpl/src/sugar/autofill.ts
@@ -159,7 +159,7 @@ function validateAccountAddress(
   const val: unknown = tx[accountField]
 
   if (typeof val !== 'string') {
-    throw new Error(`${accountField} must be a string`)
+    throw new ValidationError(`${accountField} must be a string`)
   }
   // if X-address is given, convert it to classic address
   const { classicAccount, tag } = getClassicAccountAndTag(val)
